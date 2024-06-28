68db5dbe5b1854e0a6aef1bd80a98129d8e2bcec
stephengold <sgold@sonic.net>
2024-06-28 12:24:42 -0700

    Vec3:  add a double-precision constructor

--- src/main/java/com/github/stephengold/joltjni/Vec3.java
+++ src/main/java/com/github/stephengold/joltjni/Vec3.java
@@ -63,6 +63,19 @@ final public class Vec3 {
      * @param y the desired Y component
      * @param z the desired Z component
      */
+    public Vec3(double x, double y, double z) {
+        this.x = (float) x;
+        this.y = (float) y;
+        this.z = (float) z;
+    }
+
+    /**
+     * Instantiate a vector with specified components.
+     *
+     * @param x the desired X component
+     * @param y the desired Y component
+     * @param z the desired Z component
+     */
     public Vec3(float x, float y, float z) {
         this.x = x;
         this.y = y;
