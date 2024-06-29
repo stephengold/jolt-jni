3a03ad569e3d04f6cb1f532068484b9eed0853fd
stephengold <sgold@sonic.net>
2024-06-28 21:08:48 -0700

    add the SphereShapeSettings class

---
+++ src/main/java/com/github/stephengold/joltjni/SphereShapeSettings.java
@@ -0,0 +1,63 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Settings used to construct a {@code SphereShape}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class SphereShapeSettings extends ConvexShapeSettings {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate settings for the specified radius.
+     *
+     * @param radius the desired radius (&ge;0)
+     */
+    public SphereShapeSettings(float radius) {
+        long settingsVa = createSphereShapeSettings(radius);
+        setVirtualAddress(settingsVa, true);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Generate a shape from these settings.
+     *
+     * @return a new instance
+     */
+    public Shape createShape() {
+        long settingsVa = va();
+        long shapeVa = createSphereShape(settingsVa);
+        SphereShape result = new SphereShape(shapeVa);
+
+        return result;
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createSphereShape(long settingsVa);
+
+    native private static long createSphereShapeSettings(float radius);
+}
---
+++ src/main/native/glue/com_github_stephengold_joltjni_SphereShapeSettings.cpp
@@ -0,0 +1,58 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Collision/Shape/SphereShape.h>
+#include "auto/com_github_stephengold_joltjni_SphereShapeSettings.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_SphereShapeSettings
+ * Method:    createSphereShape
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_SphereShapeSettings_createSphereShape
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const SphereShapeSettings * const pSettings
+            = reinterpret_cast<SphereShapeSettings *> (settingsVa);
+    ShapeSettings::ShapeResult shapeResult = pSettings->Create();
+    SphereShape * const pShape = new SphereShape(*pSettings, shapeResult);
+    if (shapeResult.HasError()) {
+        // TODO
+    }
+    return reinterpret_cast<jlong> (pShape);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SphereShapeSettings
+ * Method:    createSphereShapeSettings
+ * Signature: (F)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_SphereShapeSettings_createSphereShapeSettings
+  (JNIEnv *, jclass, jfloat radius) {
+    SphereShapeSettings * const pResult = new SphereShapeSettings(radius);
+    return reinterpret_cast<jlong> (pResult);
+}
