8a36e88da201f588ece7e2f6b70d28ec89d5caa4
stephengold <sgold@sonic.net>
2024-07-02 12:06:00 -0700

    add the MeshShape and MeshShapeSettings classes

---
+++ src/main/java/com/github/stephengold/joltjni/MeshShape.java
@@ -0,0 +1,59 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+import java.nio.FloatBuffer;
+
+/**
+ * A {@code Shape} to represent a surface defined by a collection of triangles.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class MeshShape extends Shape {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a shape for the specified parameters.
+     *
+     * @param triangles the vertex locations of the defining triangles (not
+     * null, capacity a multiple of 9)
+     */
+    public MeshShape(FloatBuffer triangles) {
+        long shapeVa = createMeshShape(triangles);
+        setVirtualAddress(shapeVa, true);
+    }
+
+    /**
+     * Instantiate a shape with the specified native object assigned.
+     *
+     * @param virtualAddress the virtual address of the native object to assign
+     * (not zero)
+     */
+    MeshShape(long virtualAddress) {
+        super(virtualAddress);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createMeshShape(FloatBuffer triangles);
+}
---
+++ src/main/java/com/github/stephengold/joltjni/MeshShapeSettings.java
@@ -0,0 +1,87 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+import java.nio.FloatBuffer;
+
+/**
+ * Settings used to construct a {@code MeshShape}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class MeshShapeSettings extends ShapeSettings {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate settings for the specified vertices and indices.
+     *
+     * @param vertices list of vertex locations (not null)
+     * @param indices list of triangles that use those vertices (not null)
+     */
+    public MeshShapeSettings(VertexList vertices, IndexedTriangleList indices) {
+        int numVertices = vertices.size();
+        FloatBuffer buffer = vertices.toDirectBuffer();
+        long indicesVa = indices.va();
+        long settingsVa
+                = createMeshShapeSettings(numVertices, buffer, indicesVa);
+        setVirtualAddress(settingsVa, true);
+    }
+    // *************************************************************************
+    // ShapeSettings methods
+
+    /**
+     * Generate a shape from these settings.
+     *
+     * @return a new instance
+     */
+    @Override
+    public MeshShape createShape() {
+        long settingsVa = va();
+        long shapeVa = createMeshShape(settingsVa);
+        assert shapeVa != 0L;
+        MeshShape result = new MeshShape(shapeVa);
+        assert result.getSubType() == EShapeSubType.Mesh : result.getSubType();
+
+        return result;
+    }
+
+    /**
+     * Alter the maximum number of triangles per leaf.
+     *
+     * @param numTriangles the desired number (default=8)
+     */
+    public void setMaxTrianglesPerLeaf(int numTriangles) {
+        long settingsVa = va();
+        setMaxTrianglesPerLeaf(settingsVa, numTriangles);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createMeshShape(long settingsVa);
+
+    native private static long createMeshShapeSettings(
+            int numVertices, FloatBuffer vertices, long indicesVa);
+
+    native private static void setMaxTrianglesPerLeaf(
+            long settingsVa, int numTriangles);
+}
