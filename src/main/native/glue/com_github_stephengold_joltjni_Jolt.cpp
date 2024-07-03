645ffec61f66d2da030dd0489887b2a20d4d15c5
stephengold <sgold@sonic.net>
2024-07-02 23:38:31 -0700

    bump the library version to 0.1.1 and try again

--- src/main/native/glue/com_github_stephengold_joltjni_Jolt.cpp
+++ src/main/native/glue/com_github_stephengold_joltjni_Jolt.cpp
@@ -170,6 +170,6 @@ JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Jolt_unregisterTypes
  */
 JNIEXPORT jstring JNICALL Java_com_github_stephengold_joltjni_Jolt_versionString
   (JNIEnv *pEnv, jclass) {
-    jstring result = pEnv->NewStringUTF("0.1.0");
+    jstring result = pEnv->NewStringUTF("0.1.1");
     return result;
 }
