c0c3b76a08975b95b14463eda6d13cd137dc4c60
stephengold <sgold@sonic.net>
2024-07-08 08:15:40 -0700

    Revert "Test002:  add some clean-up functionality (seems worth testing)"
    This reverts commit 9e3167048d9ae456028b05b2c75742da7db2f9ce.

--- src/test/java/testjoltjni/junit/Test002.java
+++ src/test/java/testjoltjni/junit/Test002.java
@@ -146,17 +146,6 @@ public class Test002 {
         }
 
         Assert.assertEquals(1_765, physicsSystem.getNumBodies());
-
-        jobSystem.close();
-        tempAllocator.close();
-
-        physicsSystem.close();
-        objVsObjFilter.close();
-        objVsBpFilter.close();
-        mapObj2Bp.close();
-
-        Jolt.unregisterTypes();
-        Jolt.destroyFactory();
     }
     // *************************************************************************
     // private methods
