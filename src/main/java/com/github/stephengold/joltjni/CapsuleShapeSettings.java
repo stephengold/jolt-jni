8d0f0a4cc2d112d7830575c7151ac8a478af3c30
stephengold <sgold@sonic.net>
2024-07-14 17:48:13 -0700

    add the CapsuleShapeSettings class

---
+++ src/main/java/com/github/stephengold/joltjni/CapsuleShapeSettings.java
@@ -0,0 +1,48 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Settings used to construct a {@code CapsuleShape}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class CapsuleShapeSettings extends ConvexShapeSettings {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate settings for the specified dimensions.
+     *
+     * @param halfHeight half the desired height of the cylindrical portion
+     * @param radius the desired radius
+     */
+    public CapsuleShapeSettings(float halfHeight, float radius) {
+        long settingsVa = createCapsuleShapeSettings(halfHeight, radius);
+        setVirtualAddress(settingsVa, null); // not owner due to ref counting
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createCapsuleShapeSettings(
+            float halfHeight, float radius);
+}
