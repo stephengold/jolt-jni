84ccf3e480cd5c3fa21f125c49a48cd49a9f9d46
stephengold <sgold@sonic.net>
2024-07-15 22:35:25 -0700

    add the ShapeSettingsRef class

---
+++ src/main/java/com/github/stephengold/joltjni/ShapeSettingsRef.java
@@ -0,0 +1,66 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * A counted reference to a {@code ShapeSettings}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+final public class ShapeSettingsRef extends JoltPhysicsObject {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a reference with the specified native object assigned.
+     *
+     * @param refVa the virtual address of the native object to assign (not
+     * zero)
+     * @param owner true &rarr; make the current object the owner, false &rarr;
+     * the current object isn't the owner
+     */
+    ShapeSettingsRef(long refVa, boolean owner) {
+        Runnable freeingAction = owner ? () -> free(refVa) : null;
+        setVirtualAddress(refVa, freeingAction);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Temporarily access the referenced {@code ShapeSettings}.
+     *
+     * @return a new JVM object that refers to the pre-existing native object
+     */
+    public ShapeSettings getPtr() {
+        long refVa = va();
+        long settingsVa = getPtr(refVa);
+        ShapeSettings result = ShapeSettings.newShapeSettings(settingsVa);
+
+        return result;
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static void free(long refVa);
+
+    native private static long getPtr(long refVa);
+}
