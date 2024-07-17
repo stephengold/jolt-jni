3db8f756d747b0d8140ac7f50ff4092847100b62
stephengold <sgold@sonic.net>
2024-07-17 11:31:25 -0700

    add 3 interfaces used in the instantiation of a PhysicsSystem

---
+++ src/main/java/com/github/stephengold/joltjni/ConstBroadPhaseLayerInterface.java
@@ -0,0 +1,48 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * An immutable {@code BroadPhaseLayerInterface}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public interface ConstBroadPhaseLayerInterface extends ConstJoltPhysicsObject {
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the broadphase layer for the specified object layer.
+     *
+     * @param objectLayer the index of the object layer to query (&ge;0,
+     * &lt;numObjectLayers)
+     * @return the index of the corresponding broadphase layer
+     */
+    int getBroadPhaseLayer(int objectLayer);
+
+    /**
+     * Count how many broadphase layers there are.
+     *
+     * @return the count (&ge;0)
+     */
+    int getNumBroadPhaseLayers();
+}
