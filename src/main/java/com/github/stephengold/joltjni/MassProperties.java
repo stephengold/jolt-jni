229a779ea5f6337c0c4274cd4d1a8abd6632035b
stephengold <sgold@sonic.net>
2024-07-20 11:18:54 -0700

    MassProperties:  add constructor, interface, and 9 public methods

--- src/main/java/com/github/stephengold/joltjni/MassProperties.java
+++ src/main/java/com/github/stephengold/joltjni/MassProperties.java
@@ -27,12 +27,22 @@ package com.github.stephengold.joltjni;
  *
  * @author Stephen Gold sgold@sonic.net
  */
-final public class MassProperties extends JoltPhysicsObject {
+final public class MassProperties
+        extends JoltPhysicsObject
+        implements ConstMassProperties {
     // *************************************************************************
     // constructors
 
     /**
-     * Instantiate with the specified native object assigned but not owned.
+     * Instantiate default properties.
+     */
+    public MassProperties() {
+        long propertiesVa = createMassProperties();
+        setVirtualAddress(propertiesVa, () -> free(propertiesVa));
+    }
+
+    /**
+     * Instantiate with the specified native object assigned.
      *
      * @param propertiesVa the virtual address of the native object to assign
      * (not zero)
@@ -44,7 +54,151 @@ final public class MassProperties extends JoltPhysicsObject {
         setVirtualAddress(propertiesVa, freeingAction);
     }
     // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Rotate the inertia by the specified 3x3 matrix.
+     *
+     * @param rotation the rotation to apply (not null, unaffected)
+     */
+    public void rotate(Mat44Arg rotation) {
+        long propertiesVa = va();
+        long matrixVa = rotation.va();
+        rotate(propertiesVa, matrixVa);
+    }
+
+    /**
+     * Scale the mass and inertia by the specified factors. Note that factors
+     * can be &lt;0 to flip the shape.
+     *
+     * @param scaleFactors the desired scaling (not null unaffected)
+     */
+    public void scale(Vec3Arg scaleFactors) {
+        long propertiesVa = va();
+        float x = scaleFactors.getX();
+        float y = scaleFactors.getY();
+        float z = scaleFactors.getZ();
+        scale(propertiesVa, x, y, z);
+    }
+
+    /**
+     * Set the mass and scale the inertia tensor to match.
+     *
+     * @param mass the desired mass (in kilograms, &ge;0)
+     */
+    public void scaleToMass(float mass) {
+        long propertiesVa = va();
+        scaleToMass(propertiesVa, mass);
+    }
+
+    /**
+     * Alter the inertia tensor. (native field: mIntertia)
+     *
+     * @param inertia the desired value (not null, unaffected, default=zero)
+     */
+    public void setInertia(Mat44Arg inertia) {
+        long propertiesVa = va();
+        long matrixVa = inertia.va();
+        setInertia(propertiesVa, matrixVa);
+    }
+
+    /**
+     * Alter the mass. (native field: mMass)
+     *
+     * @param mass the desired mass (in kilograms, &ge;0, default=0)
+     */
+    public void setMass(float mass) {
+        long propertiesVa = va();
+        setMass(propertiesVa, mass);
+    }
+
+    /**
+     * Translate the inertia by the specified offset.
+     *
+     * @param offset the amount of translation (not null, unaffected)
+     */
+    public void translate(Vec3Arg offset) {
+        long propertiesVa = va();
+        float x = offset.getX();
+        float y = offset.getY();
+        float z = offset.getZ();
+        translate(propertiesVa, x, y, z);
+    }
+    // *************************************************************************
+    // ConstMassProperties methods
+
+    /**
+     * Decompose the inertia tensor into a diagonal matrix and a right-handed
+     * rotation matrix.
+     *
+     * @param storeRotation storage for the rotation matrix (not null, modified)
+     * @param storeDiagonal storage for the diagonal matrix (not null, modified)
+     * @return true if successful, otherwise false
+     */
+    @Override
+    public boolean decomposePrincipalMomentsOfInertia(
+            Mat44 storeRotation, Vec3 storeDiagonal) {
+        long propertiesVa = va();
+        long matrixVa = storeRotation.va();
+        float[] diagonal = new float[3];
+        boolean result = decomposePrincipalMomentsOfInertia(
+                propertiesVa, matrixVa, diagonal);
+        storeDiagonal.set(diagonal);
+
+        return result;
+    }
+
+    /**
+     * Copy the inertia tensor. (native field: mIntertia)
+     *
+     * @return a new matrix (in kilogram.meters squared)
+     */
+    @Override
+    public Mat44 getInertia() {
+        long propertiesVa = va();
+        long matrixVa = getInertia(propertiesVa);
+        Mat44 result = new Mat44(matrixVa, true);
+
+        return result;
+    }
+
+    /**
+     * Return the mass. (native field: mMass)
+     *
+     * @return the mass (in kilograms, &ge;0)
+     */
+    @Override
+    public float getMass() {
+        long propertiesVa = va();
+        float result = getMass(propertiesVa);
+
+        return result;
+    }
+    // *************************************************************************
     // native private methods
 
+    native private static long createMassProperties();
+
+    native private static boolean decomposePrincipalMomentsOfInertia(
+            long propertiesVa, long matrixVa, float[] diagonal);
+
     native private static void free(long propertiesVa);
+
+    native private static long getInertia(long propertiesVa);
+
+    native private static float getMass(long propertiesVa);
+
+    native private static void rotate(long propertiesVa, long matrixVa);
+
+    native private static void scale(
+            long propertiesVa, float x, float y, float z);
+
+    native private static void scaleToMass(long propertiesVa, float mass);
+
+    native private static void setInertia(long propertiesVa, long matrixVa);
+
+    native private static void setMass(long propertiesVa, float mass);
+
+    native private static void translate(
+            long propertiesVa, float x, float y, float z);
 }
---
+++ src/main/native/glue/MassProperties.cpp
@@ -0,0 +1,182 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Body/MassProperties.h>
+#include "auto/com_github_stephengold_joltjni_MassProperties.h"
+#include "glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    createMassProperties
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_MassProperties_createMassProperties
+  (JNIEnv *, jclass) {
+    MassProperties * const pProperties = new MassProperties();
+    TRACE_NEW("MassProperties", pProperties)
+    return reinterpret_cast<jlong> (pProperties);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    decomposePrincipalMomentsOfInertia
+ * Signature: (JJ[F)Z
+ */
+JNIEXPORT jboolean JNICALL Java_com_github_stephengold_joltjni_MassProperties_decomposePrincipalMomentsOfInertia
+  (JNIEnv *pEnv, jclass, jlong propertiesVa, jlong matrixVa, jfloatArray diagonalArray) {
+    const MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+    Mat44 * const pStoreRotation = reinterpret_cast<Mat44 *> (matrixVa);
+    Vec3 diagonal;
+    bool success = pProperties->DecomposePrincipalMomentsOfInertia(
+            *pStoreRotation, diagonal);
+    jboolean isCopy;
+    jfloat * const pDiagonal
+            = pEnv->GetFloatArrayElements(diagonalArray, &isCopy);
+    pDiagonal[0] = diagonal.GetX();
+    pDiagonal[1] = diagonal.GetY();
+    pDiagonal[2] = diagonal.GetZ();
+    pEnv->ReleaseFloatArrayElements(diagonalArray, pDiagonal, 0);
+    return success;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    free
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MassProperties_free
+  (JNIEnv *, jclass, jlong propertiesVa) {
+    MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+    TRACE_DELETE("MassProperties", pProperties)
+    delete pProperties;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    getInertia
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_MassProperties_getInertia
+  (JNIEnv *, jclass, jlong propertiesVa) {
+    const MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+    const Mat44 * const pResult = new Mat44(pProperties->mInertia);
+    TRACE_NEW("Mat44", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    getMass
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MassProperties_getMass
+  (JNIEnv *, jclass, jlong propertiesVa) {
+    const MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+    const float result = pProperties->mMass;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    rotate
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MassProperties_rotate
+  (JNIEnv *, jclass, jlong propertiesVa, jlong matrixVa) {
+    MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+    const Mat44 * const pMatrix = reinterpret_cast<Mat44 *> (matrixVa);
+    pProperties->Rotate(*pMatrix);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    scale
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MassProperties_scale
+  (JNIEnv *, jclass, jlong propertiesVa, jfloat x, jfloat y, jfloat z) {
+    MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+    const Vec3 scaleFactors(x, y, z);
+    pProperties->Scale(scaleFactors);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    scaleToMass
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MassProperties_scaleToMass
+  (JNIEnv *, jclass, jlong propertiesVa, jfloat mass) {
+    MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+    pProperties->ScaleToMass(mass);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    setInertia
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MassProperties_setInertia
+  (JNIEnv *, jclass, jlong propertiesVa, jlong matrixVa) {
+    MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+    const Mat44 * const pMatrix = reinterpret_cast<Mat44 *> (matrixVa);
+    pProperties->mInertia = *pMatrix;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    setMass
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MassProperties_setMass
+  (JNIEnv *, jclass, jlong propertiesVa, jfloat mass) {
+    MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+   pProperties->mMass = mass;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MassProperties
+ * Method:    translate
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MassProperties_translate
+  (JNIEnv *, jclass, jlong propertiesVa, jfloat x, jfloat y, jfloat z) {
+    MassProperties * const pProperties
+            = reinterpret_cast<MassProperties *> (propertiesVa);
+    Vec3 offset(x, y, z);
+    pProperties->Translate(offset);
+}
