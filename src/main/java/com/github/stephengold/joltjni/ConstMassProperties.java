e1aaa9910e7dc22497cbae4e2421e2cc01072d74
stephengold <sgold@sonic.net>
2024-07-20 10:20:36 -0700

    add the ConstMassProperties, Mat44Arg, and UVec4Arg interfaces

---
+++ src/main/java/com/github/stephengold/joltjni/ConstMassProperties.java
@@ -0,0 +1,57 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Read-only access to a {@code MassProperties} object.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public interface ConstMassProperties extends ConstJoltPhysicsObject {
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Decompose the inertia tensor into a diagonal matrix and a right-handed
+     * rotation matrix.
+     *
+     * @param storeRotation storage for the rotation matrix (not null, modified)
+     * @param storeDiagonal storage for the diagonal matrix (not null, modified)
+     * @return true if successful, otherwise false
+     */
+    boolean decomposePrincipalMomentsOfInertia(
+            Mat44 storeRotation, Vec3 storeDiagonal);
+
+    /**
+     * Copy the inertia tensor.
+     *
+     * @return a new matrix (in kilogram.meters squared)
+     */
+    Mat44 getInertia();
+
+    /**
+     * Return the mass.
+     *
+     * @return the mass (in kilograms, &ge;0)
+     */
+    float getMass();
+}
---
+++ src/main/java/com/github/stephengold/joltjni/Mat44Arg.java
@@ -0,0 +1,72 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Read-only access to a {@code Mat44}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public interface Mat44Arg extends ConstJoltPhysicsObject {
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the specified element.
+     *
+     * @param row the zero-origin index of the row (&ge;0, &lt;4)
+     * @param column the zero-origin index of the column (&ge;0, &lt;4)
+     * @return the element's value
+     */
+    float getElement(int row, int column);
+
+    /**
+     * Convert the rotation to a {@code Quat}.
+     *
+     * @return a new rotation quaternion
+     */
+    Quat getQuaternion();
+
+    /**
+     * Multiply the current 3x3 matrix by the specified 3x3 matrix.
+     *
+     * @param arg the factor (not null, unaffected)
+     * @return a new matrix
+     */
+    Mat44 multiply3x3(Mat44Arg arg);
+
+    /**
+     * Multiply the 3x3 matrix by the specified vector.
+     *
+     * @param vec3Arg the right factor (not null, unaffected)
+     * @return a new vector
+     */
+    Vec3 multiply3x3(Vec3Arg vec3Arg);
+
+    /**
+     * Multiply the transpose of the 3x3 matrix by the specified vector.
+     *
+     * @param vec3Arg the right factor (not null, unaffected)
+     * @return a new vector
+     */
+    Vec3 multiply3x3Transposed(Vec3Arg vec3Arg);
+}
