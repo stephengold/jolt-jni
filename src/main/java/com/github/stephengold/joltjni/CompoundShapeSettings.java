52e94a5486df9407b04c9b3a4aa655b9ca07bacb
stephengold <sgold@sonic.net>
2024-07-24 13:18:53 -0700

    add the CompoundShape and CompoundShapeSettings classes

---
+++ src/main/java/com/github/stephengold/joltjni/CompoundShapeSettings.java
@@ -0,0 +1,103 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Settings used to construct a {@code CompoundShape}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+abstract public class CompoundShapeSettings extends ShapeSettings {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a settings object with no native object assigned.
+     */
+    protected CompoundShapeSettings() {
+    }
+
+    /**
+     * Instantiate with the specified native object assigned but not owned.
+     *
+     * @param settingsVa the virtual address of the native object to assign (not
+     * zero)
+     */
+    protected CompoundShapeSettings(long settingsVa) {
+        super(settingsVa);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Add the specified sub shape at the specified position.
+     *
+     * @param offset (not null, unaffected)
+     * @param rotation (not null, not zero, unaffected)
+     * @param subRef a reference to the sub shape (not null)
+     */
+    public void addShape(Vec3Arg offset, QuatArg rotation, ShapeRefC subRef) {
+        long settingsVa = va();
+        float offsetX = offset.getX();
+        float offsetY = offset.getY();
+        float offsetZ = offset.getZ();
+        float rotW = rotation.getW();
+        float rotX = rotation.getX();
+        float rotY = rotation.getY();
+        float rotZ = rotation.getZ();
+        long subRefVa = subRef.va();
+        addShapeRef(settingsVa, offsetX, offsetY, offsetZ,
+                rotX, rotY, rotZ, rotW, subRefVa);
+    }
+
+    /**
+     * Add the specified sub shape at the specified position.
+     *
+     * @param offset (not null, unaffected)
+     * @param rotation (not null, not zero, unaffected)
+     * @param subSettings (not null)
+     */
+    public void addShape(
+            Vec3Arg offset, QuatArg rotation, ShapeSettings subSettings) {
+        long settingsVa = va();
+        float offsetX = offset.getX();
+        float offsetY = offset.getY();
+        float offsetZ = offset.getZ();
+        float rotW = rotation.getW();
+        float rotX = rotation.getX();
+        float rotY = rotation.getY();
+        float rotZ = rotation.getZ();
+        long subSettingsVa = subSettings.va();
+        addShapeSettings(settingsVa, offsetX, offsetY, offsetZ,
+                rotX, rotY, rotZ, rotW, subSettingsVa);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static void addShapeRef(long settingsVa, float offsetX,
+            float offsetY, float offsetZ, float rotX, float rotY, float rotZ,
+            float rotW, long subRefVa);
+
+    native private static void addShapeSettings(long settingsVa, float offsetX,
+            float offsetY, float offsetZ, float rotX, float rotY, float rotZ,
+            float rotW, long subSettingsVa);
+}
---
+++ src/main/native/glue/CompoundShapeSettings.cpp
@@ -0,0 +1,64 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Collision/Shape/CompoundShape.h>
+#include "auto/com_github_stephengold_joltjni_CompoundShapeSettings.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_CompoundShapeSettings
+ * Method:    addShapeRef
+ * Signature: (JFFFFFFFJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_CompoundShapeSettings_addShapeRef
+  (JNIEnv *, jclass, jlong settingsVa, jfloat offsetX, jfloat offsetY, jfloat offsetZ,
+  jfloat rotX, jfloat rotY, jfloat rotZ, jfloat rotW, jlong subRefVa) {
+    CompoundShapeSettings * const pSettings
+            = reinterpret_cast<CompoundShapeSettings *> (settingsVa);
+    Vec3 offset(offsetX, offsetY, offsetZ);
+    Quat rotation(rotX, rotY, rotZ, rotW);
+    const ShapeRefC * const pSubShapeRef = reinterpret_cast<ShapeRefC *> (subRefVa);
+    const Shape * const pSubShape = pSubShapeRef->GetPtr();
+    pSettings->AddShape(offset, rotation, pSubShape);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_CompoundShapeSettings
+ * Method:    addShapeSettings
+ * Signature: (JFFFFFFFJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_CompoundShapeSettings_addShapeSettings
+  (JNIEnv *, jclass, jlong settingsVa, jfloat offsetX, jfloat offsetY, jfloat offsetZ,
+  jfloat rotX, jfloat rotY, jfloat rotZ, jfloat rotW, jlong subSettingsVa) {
+    CompoundShapeSettings * const pSettings
+            = reinterpret_cast<CompoundShapeSettings *> (settingsVa);
+    Vec3 offset(offsetX, offsetY, offsetZ);
+    Quat rotation(rotX, rotY, rotZ, rotW);
+    const ShapeSettings * const pSubSettings
+            = reinterpret_cast<ShapeSettings *> (subSettingsVa);
+    pSettings->AddShape(offset, rotation, pSubSettings);
+}
