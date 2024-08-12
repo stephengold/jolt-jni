131cead5d69832c2f1cc35450cba329032f62b48
stephengold <sgold@sonic.net>
2024-08-12 14:22:43 -0700

    add the MotorSettings class

---
+++ src/main/java/com/github/stephengold/joltjni/MotorSettings.java
@@ -0,0 +1,273 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Settings used to construct a constraint motor.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+final public class MotorSettings extends JoltPhysicsObject {
+    // *************************************************************************
+    // fields
+
+    /**
+     * prevent premature garbage collection of the underlying
+     * {@code ConstraintSettings}
+     */
+    final private ConstraintSettingsRef constraintSettingsRef;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate settings with the specified native object assigned but not
+     * owned.
+     *
+     * @param constraintSettings the underlying {@code ConstraintSettings} (not
+     * null)
+     * @param settingsVa the virtual address of the native object to assign (not
+     * zero)
+     */
+    MotorSettings(ConstraintSettings constraintSettings, long settingsVa) {
+        this.constraintSettingsRef = constraintSettings.toRef();
+        setVirtualAddress(settingsVa, null);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Access the underlying {@code ConstraintSettings}. The motor settings are
+     * unaffected.
+     *
+     * @return the pre-existing instance
+     */
+    public ConstraintSettings getConstraintSettings() {
+        return constraintSettingsRef.getPtr();
+    }
+
+    /**
+     * Return the maximum force to apply in a linear constraint. Ignored in an
+     * angular motor. The settings are unaffected. (native field:
+     * mMaxForceLimit)
+     *
+     * @return the amount of force (in Newtons, typically positive)
+     */
+    public float getMaxForceLimit() {
+        long settingsVa = va();
+        float result = getMaxForceLimit(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the maximum torque to apply in an angular constraint. Ignored in a
+     * linear motor. The settings are unaffected. (native field:
+     * mMaxTorqueLimit)
+     *
+     * @return the amount of torque (in Newton-meters, typically positive)
+     */
+    public float getMaxTorqueLimit() {
+        long settingsVa = va();
+        float result = getMaxTorqueLimit(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the minimum force to apply in a linear constraint. Ignored in an
+     * angular motor. The settings are unaffected. (native field:
+     * mMinForceLimit)
+     *
+     * @return the amount of force (in Newtons, typically negative)
+     */
+    public float getMinForceLimit() {
+        long settingsVa = va();
+        float result = getMinForceLimit(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the minimum torque to apply in an angular constraint. Ignored in a
+     * linear motor. The settings are unaffected. (native field:
+     * mMinTorqueLimit)
+     *
+     * @return the amount of torque (in Newton-meters, typically negative)
+     */
+    public float getMinTorqueLimit() {
+        long settingsVa = va();
+        float result = getMinTorqueLimit(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Access the settings for the spring used to drive to the position target.
+     * Ignored in a velocity motor. The motor settings are unaffected. (native
+     * field: mSpringSettings)
+     *
+     * @return a new JVM object with the pre-existing native object assigned
+     */
+    public SpringSettings getSpringSettings() {
+        long motorSettingsVa = va();
+        long springSettingsVa = getSpringSettings(motorSettingsVa);
+        ConstraintSettings constraintSettings = getConstraintSettings();
+        SpringSettings result
+                = new SpringSettings(constraintSettings, springSettingsVa);
+
+        return result;
+    }
+
+    /**
+     * Test whether the settings are valid. They are unaffected.
+     *
+     * @return true if valid, otherwise false
+     */
+    public boolean isValid() {
+        long settingsVa = va();
+        boolean result = isValid(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Alter both force limits of a linear motor to be symmetrical.
+     *
+     * @param limit the desired limit (in Newtons)
+     */
+    public void setForceLimit(float limit) {
+        long settingsVa = va();
+        setForceLimit(settingsVa, limit);
+    }
+
+    /**
+     * Alter both force limits of a linear motor.
+     *
+     * @param min the desired lower limit (in Newtons, default=-MAX_FLOAT)
+     * @param max the desired upper limit (in Newtons, default=MAX_FLOAT)
+     */
+    public void setForceLimits(float min, float max) {
+        long settingsVa = va();
+        setForceLimits(settingsVa, min, max);
+    }
+
+    /**
+     * Alter the maximum force to apply in a linear constraint. Ignored in an
+     * angular motor. (native field: mMaxForceLimit)
+     *
+     * @param force the desired amount of force (in Newtons, typically positive,
+     * default=MAX_VALUE)
+     */
+    public void setMaxForceLimit(float force) {
+        long settingsVa = va();
+        setMaxForceLimit(settingsVa, force);
+    }
+
+    /**
+     * Alter the maximum torque to apply in an angular constraint. Ignored in a
+     * linear motor. (native field: mMaxTorqueLimit)
+     *
+     * @param torque the desired amount of torque (in Newton-meters, typically
+     * positive, default=MAX_VALUE)
+     */
+    public void setMaxTorqueLimit(float torque) {
+        long settingsVa = va();
+        setMaxTorqueLimit(settingsVa, torque);
+    }
+
+    /**
+     * Alter the minimum force to apply in a linear constraint. Ignored in an
+     * angular motor. (native field: mMinForceLimit)
+     *
+     * @param force the desired amount of force (in Newtons, typically negative,
+     * default=-MAX_VALUE)
+     */
+    public void setMinForceLimit(float force) {
+        long settingsVa = va();
+        setMinForceLimit(settingsVa, force);
+    }
+
+    /**
+     * Alter the minimum torque to apply in an angular constraint. Ignored in a
+     * linear motor. (native field: mMinTorqueLimit)
+     *
+     * @param torque the desired amount of torque (in Newton-meters, typically
+     * negative, default=-MAX_VALUE)
+     */
+    public void setMinTorqueLimit(float torque) {
+        long settingsVa = va();
+        setMinTorqueLimit(settingsVa, torque);
+    }
+
+    /**
+     * Alter both torque limits of an angular motor to be symmetrical.
+     *
+     * @param limit the desired upper limit (in Newton-meters)
+     */
+    public void setTorqueLimit(float limit) {
+        long settingsVa = va();
+        setTorqueLimit(settingsVa, limit);
+    }
+
+    /**
+     * Alter both torque limits of an angular motor.
+     *
+     * @param min the desired lower limit (in Newton-meters, default=-MAX_FLOAT)
+     * @param max the desired upper limit (in Newton-meters, default=MAX_FLOAT)
+     */
+    public void setTorqueLimits(float min, float max) {
+        long settingsVa = va();
+        setTorqueLimits(settingsVa, min, max);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static float getMaxForceLimit(long settingsVa);
+
+    native private static float getMaxTorqueLimit(long settingsVa);
+
+    native private static float getMinForceLimit(long settingsVa);
+
+    native private static float getMinTorqueLimit(long settingsVa);
+
+    native private static long getSpringSettings(long motorSettingsVa);
+
+    native private static boolean isValid(long settingsVa);
+
+    native private static void setForceLimit(long settingsVa, float limit);
+
+    native private static void setForceLimits(
+            long settingsVa, float min, float max);
+
+    native private static void setMaxForceLimit(long settingsVa, float force);
+
+    native private static void setMaxTorqueLimit(long settingsVa, float torque);
+
+    native private static void setMinForceLimit(long settingsVa, float force);
+
+    native private static void setMinTorqueLimit(long settingsVa, float torque);
+
+    native private static void setTorqueLimit(long settingsVa, float limit);
+
+    native private static void setTorqueLimits(
+            long settingsVa, float min, float max);
+}
---
+++ src/main/native/glue/m/MotorSettings.cpp
@@ -0,0 +1,204 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Constraints/MotorSettings.h>
+#include "auto/com_github_stephengold_joltjni_MotorSettings.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    getMaxForceLimit
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorSettings_getMaxForceLimit
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    const float result = pSettings->mMaxForceLimit;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    getMaxTorqueLimit
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorSettings_getMaxTorqueLimit
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    const float result = pSettings->mMaxTorqueLimit;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    getMinForceLimit
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorSettings_getMinForceLimit
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    const float result = pSettings->mMinForceLimit;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    getMinTorqueLimit
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorSettings_getMinTorqueLimit
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    const float result = pSettings->mMinTorqueLimit;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    getSpringSettings
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_MotorSettings_getSpringSettings
+  (JNIEnv *, jclass, jlong motorSettingsVa) {
+    MotorSettings * const pMotorSettings
+            = reinterpret_cast<MotorSettings *> (motorSettingsVa);
+    SpringSettings * const pResult = &pMotorSettings->mSpringSettings;
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    isValid
+ * Signature: (J)Z
+ */
+JNIEXPORT jboolean JNICALL Java_com_github_stephengold_joltjni_MotorSettings_isValid
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    const bool result = pSettings->IsValid();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    setForceLimit
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorSettings_setForceLimit
+  (JNIEnv *, jclass, jlong settingsVa, jfloat limit) {
+    MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    pSettings->SetForceLimit(limit);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    setForceLimits
+ * Signature: (JFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorSettings_setForceLimits
+  (JNIEnv *, jclass, jlong settingsVa, jfloat min, jfloat max) {
+    MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    pSettings->SetForceLimits(min, max);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    setMaxForceLimit
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorSettings_setMaxForceLimit
+  (JNIEnv *, jclass, jlong settingsVa, jfloat force) {
+    MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    pSettings->mMaxForceLimit = force;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    setMaxTorqueLimit
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorSettings_setMaxTorqueLimit
+  (JNIEnv *, jclass, jlong settingsVa, jfloat torque) {
+    MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    pSettings->mMaxTorqueLimit = torque;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    setMinForceLimit
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorSettings_setMinForceLimit
+  (JNIEnv *, jclass, jlong settingsVa, jfloat force) {
+    MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    pSettings->mMinForceLimit = force;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    setMinTorqueLimit
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorSettings_setMinTorqueLimit
+  (JNIEnv *, jclass, jlong settingsVa, jfloat torque) {
+    MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    pSettings->mMinTorqueLimit = torque;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    setTorqueLimit
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorSettings_setTorqueLimit
+  (JNIEnv *, jclass, jlong settingsVa, jfloat limit) {
+    MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    pSettings->SetTorqueLimit(limit);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorSettings
+ * Method:    setTorqueLimits
+ * Signature: (JFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorSettings_setTorqueLimits
+  (JNIEnv *, jclass, jlong settingsVa, jfloat min, jfloat max) {
+    MotorSettings * const pSettings
+            = reinterpret_cast<MotorSettings *> (settingsVa);
+    pSettings->SetTorqueLimits(min, max);
+}
