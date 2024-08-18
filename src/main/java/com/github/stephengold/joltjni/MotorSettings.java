a6da7b1ae0f02f871786e818c1972ce0581360e9
stephengold <sgold@sonic.net>
2024-08-17 23:41:56 -0700

    MotorSettings:  add the constraintRef field

--- src/main/java/com/github/stephengold/joltjni/MotorSettings.java
+++ src/main/java/com/github/stephengold/joltjni/MotorSettings.java
@@ -31,12 +31,32 @@ final public class MotorSettings extends JoltPhysicsObject {
     // fields
 
     /**
+     * prevent premature garbage collection of the underlying {@code Constraint}
+     * if any
+     */
+    final private ConstraintRef constraintRef;
+    /**
      * prevent premature garbage collection of the underlying
-     * {@code ConstraintSettings}
+     * {@code ConstraintSettings} if any
      */
     final private ConstraintSettingsRef constraintSettingsRef;
     // *************************************************************************
     // constructors
+
+    /**
+     * Instantiate settings with the specified native object assigned but not
+     * owned.
+     *
+     * @param constraint the underlying {@code Constraint} (not null)
+     * @param settingsVa the virtual address of the native object to assign (not
+     * zero)
+     */
+    MotorSettings(Constraint constraint, long settingsVa) {
+        this.constraintRef = constraint.toRef();
+        this.constraintSettingsRef = null;
+
+        setVirtualAddress(settingsVa, null);
+    }
 
     /**
      * Instantiate settings with the specified native object assigned but not
@@ -48,17 +68,27 @@ final public class MotorSettings extends JoltPhysicsObject {
      * zero)
      */
     MotorSettings(ConstraintSettings constraintSettings, long settingsVa) {
+        this.constraintRef = null;
         this.constraintSettingsRef = constraintSettings.toRef();
+
         setVirtualAddress(settingsVa, null);
     }
     // *************************************************************************
     // new methods exposed
 
     /**
-     * Access the underlying {@code ConstraintSettings}. The motor settings are
-     * unaffected.
+     * Access the underlying {@code Constraint}, if any.
      *
-     * @return the pre-existing instance
+     * @return the pre-existing instance, or null if none
+     */
+    public Constraint getConstraint() {
+        return constraintRef.getPtr();
+    }
+
+    /**
+     * Access the underlying {@code ConstraintSettings}, if any.
+     *
+     * @return the pre-existing instance, or null if none
      */
     public ConstraintSettings getConstraintSettings() {
         return constraintSettingsRef.getPtr();
