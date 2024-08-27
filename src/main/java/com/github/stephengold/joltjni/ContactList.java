452d87ca06853981822f657d259749bdc3e9494e
stephengold <sgold@sonic.net>
2024-08-26 17:58:32 -0700

    add the ContactList class

---
+++ src/main/java/com/github/stephengold/joltjni/ContactList.java
@@ -0,0 +1,118 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * A variable-length list of character contacts.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class ContactList extends JoltPhysicsObject {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a list with the specified native object assigned.
+     *
+     * @param listVa the virtual address of the native object to assign (not
+     * zero)
+     * @param owner true &rarr; make the JVM object the owner, false &rarr; it
+     * isn't the owner
+     */
+    ContactList(long listVa, boolean owner) {
+        Runnable freeingAction = owner ? () -> free(listVa) : null;
+        setVirtualAddress(listVa, freeingAction);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Count how many elements the currently allocated storage can hold.
+     *
+     * @return the number of elements (&ge;size)
+     */
+    public int capacity() {
+        long listVa = va();
+        int result = capacity(listVa);
+        return result;
+    }
+
+    /**
+     * Test whether the list contains any elements.
+     *
+     * @return true if empty, otherwise false
+     */
+    public boolean empty() {
+        if (size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Access the contact at the specified index.
+     *
+     * @param elementIndex the index from which to get the contact
+     * @return a new reference to the contact
+     */
+    public Contact get(int elementIndex) {
+        long listVa = va();
+        long refVa = get(listVa, elementIndex);
+        Contact result = new Contact(refVa, true);
+
+        return result;
+    }
+
+    /**
+     * Expand or truncate the list.
+     *
+     * @param numElements the desired size (number of elements)
+     */
+    public void resize(int numElements) {
+        long listVa = va();
+        resize(listVa, numElements);
+    }
+
+    /**
+     * Count how many elements are in the list.
+     *
+     * @return the number of elements (&ge;0, &le;capacity)
+     */
+    public int size() {
+        long listVa = va();
+        int result = size(listVa);
+        return result;
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static int capacity(long listVa);
+
+    native private static void free(long listVa);
+
+    native private static long get(long listVa, int elementIndex);
+
+    native private static void resize(long listVa, int numElements);
+
+    native private static int size(long listVa);
+}
---
+++ src/main/native/glue/c/ContactList.cpp
@@ -0,0 +1,95 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Character/CharacterVirtual.h>
+#include "auto/com_github_stephengold_joltjni_ContactList.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_ContactList
+ * Method:    capacity
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_ContactList_capacity
+  (JNIEnv *, jclass, jlong listVa) {
+    const CharacterVirtual::ContactList * const pList
+            = reinterpret_cast<CharacterVirtual::ContactList *> (listVa);
+    const CharacterVirtual::ContactList::size_type result = pList->capacity();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_ContactList
+ * Method:    free
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_ContactList_free
+  (JNIEnv *, jclass, jlong listVa) {
+    CharacterVirtual::ContactList * const pList
+            = reinterpret_cast<CharacterVirtual::ContactList *> (listVa);
+    TRACE_DELETE("ContactList", pList)
+    delete pList;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_ContactList
+ * Method:    get
+ * Signature: (JI)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_ContactList_get
+  (JNIEnv *, jclass, jlong listVa, jint elementIndex) {
+    CharacterVirtual::ContactList * const pList
+            = reinterpret_cast<CharacterVirtual::ContactList *> (listVa);
+    CharacterVirtual::Contact& result = pList->at(elementIndex);
+    return reinterpret_cast<jlong> (&result);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_ContactList
+ * Method:    resize
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_ContactList_resize
+  (JNIEnv *, jclass, jlong listVa, jint numElements) {
+    CharacterVirtual::ContactList * const pList
+            = reinterpret_cast<CharacterVirtual::ContactList *> (listVa);
+    pList->resize(numElements);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_ContactList
+ * Method:    size
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_ContactList_size
+  (JNIEnv *, jclass, jlong listVa) {
+    const CharacterVirtual::ContactList * const pList
+            = reinterpret_cast<CharacterVirtual::ContactList *> (listVa);
+    const CharacterVirtual::ContactList::size_type result = pList->size();
+    return result;
+}
