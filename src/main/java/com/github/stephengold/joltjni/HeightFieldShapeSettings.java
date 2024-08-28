0efc14f041fd52c773a9ee1a3abaedce38cb65ea
stephengold <sgold@sonic.net>
2024-08-27 17:56:43 -0700

    HeightFieldShapeSettings:  add 11 public methods

--- src/main/java/com/github/stephengold/joltjni/HeightFieldShapeSettings.java
+++ src/main/java/com/github/stephengold/joltjni/HeightFieldShapeSettings.java
@@ -68,9 +68,189 @@ public class HeightFieldShapeSettings extends ShapeSettings {
         setSubType(EShapeSubType.HeightField);
     }
     // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the cosine of the active edge threshold angle. The settings are
+     * unaffected. (native field: mActiveEdgeCosThresholdAngle)
+     *
+     * @return the cosine
+     */
+    public float getActiveEdgeCosThresholdAngle() {
+        long settingsVa = va();
+        float result = getActiveEdgeCosThresholdAngle(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the artificial maximum height. The settings are unaffected.
+     * (native field: mMaxHeightValue)
+     *
+     * @return the height value
+     */
+    public float getMaxHeightValue() {
+        long settingsVa = va();
+        float result = getMaxHeightValue(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the artificial minimum height. The settings are unaffected.
+     * (native field: mMinHeightValue)
+     *
+     * @return the height value
+     */
+    public float getMinHeightValue() {
+        long settingsVa = va();
+        float result = getMinHeightValue(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the offset of the first sample. The settings are unaffected.
+     * (native field: mOffset)
+     *
+     * @return a new offset vector
+     */
+    public Vec3 getOffset() {
+        long settingsVa = va();
+        float dx = getOffsetX(settingsVa);
+        float dy = getOffsetY(settingsVa);
+        float dz = getOffsetZ(settingsVa);
+        Vec3 result = new Vec3(dx, dy, dz);
+
+        return result;
+    }
+
+    /**
+     * Count the samples. The settings are unaffected. (native field:
+     * mSampleCount)
+     *
+     * @return the number of samples (&ge;0)
+     */
+    public int getSampleCount() {
+        long settingsVa = va();
+        int result = getSampleCount(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the scale factors. The settings are unaffected. (native field:
+     * mScale)
+     *
+     * @return a new scaling vector
+     */
+    public Vec3 getScale() {
+        long settingsVa = va();
+        float sx = getScaleX(settingsVa);
+        float sy = getScaleY(settingsVa);
+        float sz = getScaleZ(settingsVa);
+        Vec3 result = new Vec3(sx, sy, sz);
+
+        return result;
+    }
+
+    /**
+     * Alter the active edge threshold angle. (native field:
+     * mActiveEdgeCosThresholdAngle)
+     *
+     * @param cosine the cosine of the desired angle (default=0.996195)
+     */
+    public void setActiveEdgeCosThresholdAngle(float cosine) {
+        long settingsVa = va();
+        setActiveEdgeCosThresholdAngle(settingsVa, cosine);
+    }
+
+    /**
+     * Alter the artificial maximum height. (native field: mMaxHeightValue)
+     *
+     * @param maxHeight the desired height value (default=-MAX_VALUE)
+     */
+    public void setMaxHeightValue(float maxHeight) {
+        long settingsVa = va();
+        setMaxHeightValue(settingsVa, maxHeight);
+    }
+
+    /**
+     * Alter the artificial minimum height. (native field: mMinHeightValue)
+     *
+     * @param minHeight the desired height value (default=MAX_VALUE)
+     */
+    public void setMinHeightValue(float minHeight) {
+        long settingsVa = va();
+        setMinHeightValue(settingsVa, minHeight);
+    }
+
+    /**
+     * Alter the offset of the first sample. (native field: mOffset)
+     *
+     * @param offset the desired offset vector (not null, unaffected,
+     * default=(0,0,0))
+     */
+    public void setOffset(Vec3Arg offset) {
+        long settingsVa = va();
+        float dx = offset.getX();
+        float dy = offset.getY();
+        float dz = offset.getZ();
+        setOffset(settingsVa, dx, dy, dz);
+    }
+
+    /**
+     * Alter the scale factors. (native field: mScale)
+     *
+     * @param scale the desired scale factors (not null, unaffected,
+     * default=(1,1,1))
+     */
+    public void setScale(Vec3Arg scale) {
+        long settingsVa = va();
+        float sx = scale.getX();
+        float sy = scale.getY();
+        float sz = scale.getZ();
+        setScale(settingsVa, sx, sy, sz);
+    }
+    // *************************************************************************
     // native private methods
 
     native private static long createHeightFieldShapeSettings(
             FloatBuffer samples, float offsetX, float offsetY, float offsetZ,
             float scaleX, float scaleY, float scaleZ, int sampleCount);
+
+    native private static float getActiveEdgeCosThresholdAngle(long settingsVa);
+
+    native private static float getMaxHeightValue(long settingsVa);
+
+    native private static float getMinHeightValue(long setingsVa);
+
+    native private static float getOffsetX(long settingsVa);
+
+    native private static float getOffsetY(long settingsVa);
+
+    native private static float getOffsetZ(long settingsVa);
+
+    native private static int getSampleCount(long settingsVa);
+
+    native private static float getScaleX(long settingsVa);
+
+    native private static float getScaleY(long settingsVa);
+
+    native private static float getScaleZ(long settingsVa);
+
+    native private static void setActiveEdgeCosThresholdAngle(
+            long settingsVa, float cosine);
+
+    native private static void setMaxHeightValue(
+            long settingsVa, float maxHeight);
+
+    native private static void setMinHeightValue(
+            long settingsVa, float minHeight);
+
+    native private static void setOffset(
+            long settingsVa, float dx, float dy, float dz);
+
+    native private static void setScale(
+            long settingsVa, float sx, float sy, float sz);
 }
--- src/main/native/glue/h/HeightFieldShapeSettings.cpp
+++ src/main/native/glue/h/HeightFieldShapeSettings.cpp
@@ -47,3 +47,195 @@ JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSett
     TRACE_NEW("HeightFieldShapeSettings", pSettings)
     return reinterpret_cast<jlong> (pSettings);
 }
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getActiveEdgeCosThresholdAngle
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getActiveEdgeCosThresholdAngle
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const float result = pSettings->mActiveEdgeCosThresholdAngle;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getMaxHeightValue
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getMaxHeightValue
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const float result = pSettings->mMaxHeightValue;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getMinHeightValue
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getMinHeightValue
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const float result = pSettings->mMinHeightValue;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getOffsetX
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getOffsetX
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const float result = pSettings->mOffset.GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getOffsetY
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getOffsetY
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const float result = pSettings->mOffset.GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getOffsetZ
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getOffsetZ
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const float result = pSettings->mOffset.GetZ();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getSampleCount
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getSampleCount
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const uint32 result = pSettings->mSampleCount;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getScaleX
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getScaleX
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const float result = pSettings->mScale.GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getScaleY
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getScaleY
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const float result = pSettings->mScale.GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    getScaleZ
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_getScaleZ
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const float result = pSettings->mScale.GetZ();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    setActiveEdgeCosThresholdAngle
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_setActiveEdgeCosThresholdAngle
+  (JNIEnv *, jclass, jlong settingsVa, jfloat cosine) {
+    HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    pSettings->mActiveEdgeCosThresholdAngle = cosine;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    setMaxHeightValue
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_setMaxHeightValue
+  (JNIEnv *, jclass, jlong settingsVa, jfloat height) {
+    HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    pSettings->mMaxHeightValue = height;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    setMinHeightValue
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_setMinHeightValue
+  (JNIEnv *, jclass, jlong settingsVa, jfloat height) {
+    HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    pSettings->mMinHeightValue = height;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    setOffset
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_setOffset
+  (JNIEnv *, jclass, jlong settingsVa, jfloat dx, jfloat dy, jfloat dz) {
+    HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const Vec3 offset(dx, dy, dz);
+    pSettings->mOffset = offset;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_HeightFieldShapeSettings
+ * Method:    setScale
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_HeightFieldShapeSettings_setScale
+  (JNIEnv *, jclass, jlong settingsVa, jfloat sx, jfloat sy, jfloat sz) {
+    HeightFieldShapeSettings * const pSettings
+            = reinterpret_cast<HeightFieldShapeSettings *> (settingsVa);
+    const Vec3 scale(sx, sy, sz);
+    pSettings->mScale = scale;
+}
