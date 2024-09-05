765e2697af9e524fe39b0b92782b46a22c664f03
stephengold <sgold@sonic.net>
2024-09-05 12:57:26 -0700

    add the Part class

---
+++ src/main/java/com/github/stephengold/joltjni/Part.java
@@ -0,0 +1,78 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Settings used to generate a rigid body in a {@code Ragdoll}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class Part extends BodyCreationSettings {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate settings with the specified native object assigned, but not
+     * owned.
+     *
+     * @param partVa the virtual address of the native object to assign (not
+     * zero)
+     */
+    Part(long partVa) {
+        super(partVa, false);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the settings to create the joint to the part's parent. The part is
+     * unaffected. (native field: mToParent)
+     *
+     * @return a new JVM with the pre-exising native object assigned
+     */
+    public ConstraintSettings getToParent() {
+        long partVa = va();
+        long settingsVa = getToParent(partVa);
+        ConstraintSettings result
+                = ConstraintSettings.newConstraintSettings(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Alter the settings to create the joint to the part's parent. (native
+     * field: mToParent)
+     *
+     * @param settings the desired settings (not null)
+     */
+    public void setToParent(ConstraintSettings settings) {
+        long partVa = va();
+        long settingsVa = settings.va();
+        setToParent(partVa, settingsVa);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long getToParent(long partVa);
+
+    native private static void setToParent(long partVa, long settingsVa);
+}
---
+++ src/main/native/glue/p/Part.cpp
@@ -0,0 +1,58 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Ragdoll/Ragdoll.h>
+#include "auto/com_github_stephengold_joltjni_Part.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_Part
+ * Method:    getToParent
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Part_getToParent
+  (JNIEnv *, jclass, jlong partVa) {
+    const RagdollSettings::Part * const pPart
+            = reinterpret_cast<RagdollSettings::Part *> (partVa);
+    Ref<TwoBodyConstraintSettings> ref = pPart->mToParent;
+    TwoBodyConstraintSettings * const pResult = ref.GetPtr();
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Part
+ * Method:    setToParent
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Part_setToParent
+  (JNIEnv *, jclass, jlong partVa, jlong settingsVa) {
+    RagdollSettings::Part * const pPart
+            = reinterpret_cast<RagdollSettings::Part *> (partVa);
+    TwoBodyConstraintSettings * const pSettings
+            = reinterpret_cast<TwoBodyConstraintSettings *> (settingsVa);
+    pPart->mToParent = pSettings;
+}
