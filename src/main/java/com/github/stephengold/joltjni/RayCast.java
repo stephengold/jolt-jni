030916afac0dc60914fec705bcc4f3ae4210d468
stephengold <sgold@sonic.net>
2024-09-09 11:32:08 -0700

    add the RayCast class

---
+++ src/main/java/com/github/stephengold/joltjni/RayCast.java
@@ -0,0 +1,83 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+import com.github.stephengold.joltjni.readonly.Vec3Arg;
+
+/**
+ * A raycast using single-precision coordinates.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class RayCast extends JoltPhysicsObject {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a raycast with the specified starting location and direction.
+     *
+     * @param startLocation the desired starting location (not null, unaffected)
+     * @param direction the desired direction (not null, unaffected)
+     */
+    public RayCast(Vec3Arg startLocation, Vec3Arg direction) {
+        float startX = startLocation.getX();
+        float startY = startLocation.getY();
+        float startZ = startLocation.getZ();
+        float dx = direction.getX();
+        float dy = direction.getY();
+        float dz = direction.getZ();
+        long raycastVa = createRayCast(startX, startY, startZ, dx, dy, dz);
+        setVirtualAddress(raycastVa, () -> free(raycastVa));
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return a point on the ray with the specified fraction. The raycast is
+     * unaffected.
+     *
+     * @param fraction (0&rarr;start of ray, 1&rarr;end of ray)
+     * @return a new location vector
+     */
+    public Vec3 getPointOnRay(float fraction) {
+        long raycastVa = va();
+        float x = getPointOnRayX(raycastVa, fraction);
+        float y = getPointOnRayY(raycastVa, fraction);
+        float z = getPointOnRayZ(raycastVa, fraction);
+        Vec3 result = new Vec3(x, y, z);
+
+        return result;
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createRayCast(float startX, float startY,
+            float startZ, float dx, float dy, float dz);
+
+    native private static void free(long raycastVa);
+
+    native private static float getPointOnRayX(long raycastVa, float fraction);
+
+    native private static float getPointOnRayY(long raycastVa, float fraction);
+
+    native private static float getPointOnRayZ(long raycastVa, float fraction);
+}
---
+++ src/main/native/glue/r/RayCast.cpp
@@ -0,0 +1,97 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Collision/RayCast.h>
+#include "auto/com_github_stephengold_joltjni_RayCast.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_RayCast
+ * Method:    createRayCast
+ * Signature: (FFFFFF)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_RayCast_createRayCast
+  (JNIEnv *, jclass, jfloat startX, jfloat startY, jfloat startZ,
+  jfloat dx, jfloat dy, jfloat dz) {
+    const Vec3 startLocation(startX, startY, startZ);
+    const Vec3 direction(dx, dy, dz);
+    RayCast * const pResult = new RayCast(startLocation, direction);
+    TRACE_NEW("RayCast", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_RayCast
+ * Method:    free
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_RayCast_free
+  (JNIEnv *, jclass, jlong raycastVa) {
+    RayCast * const pRayCast = reinterpret_cast<RayCast *> (raycastVa);
+    TRACE_DELETE("RayCast", pRayCast)
+    delete pRayCast;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_RayCast
+ * Method:    getPointOnRayX
+ * Signature: (JF)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_RayCast_getPointOnRayX
+  (JNIEnv *, jclass, jlong raycastVa, jfloat fraction) {
+    const RayCast * const pRayCast = reinterpret_cast<RayCast *> (raycastVa);
+    const Vec3 vec3 = pRayCast->GetPointOnRay(fraction);
+    const float result = vec3.GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_RayCast
+ * Method:    getPointOnRayY
+ * Signature: (JF)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_RayCast_getPointOnRayY
+  (JNIEnv *, jclass, jlong raycastVa, jfloat fraction) {
+    const RayCast * const pRayCast = reinterpret_cast<RayCast *> (raycastVa);
+    const Vec3 vec3 = pRayCast->GetPointOnRay(fraction);
+    const float result = vec3.GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_RayCast
+ * Method:    getPointOnRayZ
+ * Signature: (JF)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_RayCast_getPointOnRayZ
+  (JNIEnv *, jclass, jlong raycastVa, jfloat fraction) {
+    const RayCast * const pRayCast = reinterpret_cast<RayCast *> (raycastVa);
+    const Vec3 vec3 = pRayCast->GetPointOnRay(fraction);
+    const float result = vec3.GetZ();
+    return result;
+}
