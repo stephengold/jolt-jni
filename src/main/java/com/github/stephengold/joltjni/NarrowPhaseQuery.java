d7f1824c2f7add1f17404b3fd23cc32a95e53683
stephengold <sgold@sonic.net>
2024-09-11 16:37:50 -0700

    NarrowPhaseQuery:  add castShape(), collidePoint(), and collideShape() methods

--- src/main/java/com/github/stephengold/joltjni/NarrowPhaseQuery.java
+++ src/main/java/com/github/stephengold/joltjni/NarrowPhaseQuery.java
@@ -21,6 +21,11 @@ SOFTWARE.
  */
 package com.github.stephengold.joltjni;
 
+import com.github.stephengold.joltjni.readonly.ConstShape;
+import com.github.stephengold.joltjni.readonly.RMat44Arg;
+import com.github.stephengold.joltjni.readonly.RVec3Arg;
+import com.github.stephengold.joltjni.readonly.Vec3Arg;
+
 /**
  * Interface for doing precise collision detection against a
  * {@code PhysicsSpace}, using a broad-phase query followed by a narrow-phase
@@ -226,6 +231,326 @@ public class NarrowPhaseQuery extends NonCopyable {
     }
 
     /**
+     * Cast a shape and collect the resulting hits.
+     *
+     * @param shapeCast the desired shape cast (not null, unaffected)
+     * @param settings the shape-cast configuration options to use (not null,
+     * unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     */
+    public void castShape(RShapeCast shapeCast, ShapeCastSettings settings,
+            RVec3Arg base, CastShapeCollector collector) {
+        castShape(shapeCast, settings, base, collector,
+                new BroadPhaseLayerFilter());
+    }
+
+    /**
+     * Cast a shape and collect the resulting hits.
+     *
+     * @param shapeCast the desired shape cast (not null, unaffected)
+     * @param settings the shape-cast configuration options to use (not null,
+     * unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     */
+    public void castShape(RShapeCast shapeCast, ShapeCastSettings settings,
+            RVec3Arg base, CastShapeCollector collector,
+            BroadPhaseLayerFilter bplFilter) {
+        castShape(shapeCast, settings, base, collector, bplFilter,
+                new ObjectLayerFilter());
+    }
+
+    /**
+     * Cast a shape and collect the resulting hits.
+     *
+     * @param shapeCast the desired shape cast (not null, unaffected)
+     * @param settings the shape-cast configuration options to use (not null,
+     * unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     * @param olFilter the object-layer filter to apply (not null, unaffected)
+     */
+    public void castShape(RShapeCast shapeCast, ShapeCastSettings settings,
+            RVec3Arg base, CastShapeCollector collector,
+            BroadPhaseLayerFilter bplFilter, ObjectLayerFilter olFilter) {
+        castShape(shapeCast, settings, base, collector, bplFilter, olFilter,
+                new BodyFilter());
+    }
+
+    /**
+     * Cast a shape and collect the resulting hits.
+     *
+     * @param shapeCast the desired shape cast (not null, unaffected)
+     * @param settings the shape-cast configuration options to use (not null,
+     * unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     * @param olFilter the object-layer filter to apply (not null, unaffected)
+     * @param bodyFilter the body filter to apply (not null, unaffected)
+     */
+    public void castShape(RShapeCast shapeCast, ShapeCastSettings settings,
+            RVec3Arg base, CastShapeCollector collector,
+            BroadPhaseLayerFilter bplFilter, ObjectLayerFilter olFilter,
+            BodyFilter bodyFilter) {
+        castShape(shapeCast, settings, base, collector, bplFilter, olFilter,
+                bodyFilter, new ShapeFilter());
+    }
+
+    /**
+     * Cast a shape and collect the resulting hits.
+     *
+     * @param shapeCast the desired shape cast (not null, unaffected)
+     * @param settings the shape-cast configuration options to use (not null,
+     * unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     * @param olFilter the object-layer filter to apply (not null, unaffected)
+     * @param bodyFilter the body filter to apply (not null, unaffected)
+     * @param shapeFilter the shape filter to apply (not null, unaffected)
+     */
+    public void castShape(RShapeCast shapeCast, ShapeCastSettings settings,
+            RVec3Arg base, CastShapeCollector collector,
+            BroadPhaseLayerFilter bplFilter, ObjectLayerFilter olFilter,
+            BodyFilter bodyFilter, ShapeFilter shapeFilter) {
+        long queryVa = va();
+        long shapeCastVa = shapeCast.va();
+        long settingsVa = settings.va();
+        long collectorVa = collector.va();
+        long bplFilterVa = bplFilter.va();
+        long olFilterVa = olFilter.va();
+        long bodyFilterVa = bodyFilter.va();
+        long shapeFilterVa = shapeFilter.va();
+        castShape(queryVa, shapeCastVa, settingsVa, collectorVa, bplFilterVa,
+                olFilterVa, bodyFilterVa, shapeFilterVa);
+    }
+
+    /**
+     * Collect collisions with the specified point.
+     *
+     * @param point the location of the point to test (not null, unaffected)
+     * @param collector the hit collector to use (not null)
+     */
+    public void collidePoint(RVec3Arg point, CollidePointCollector collector) {
+        collidePoint(point, collector, new BroadPhaseLayerFilter());
+    }
+
+    /**
+     * Collect collisions with the specified point.
+     *
+     * @param point the location of the point to test (not null, unaffected)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     */
+    public void collidePoint(RVec3Arg point, CollidePointCollector collector,
+            BroadPhaseLayerFilter bplFilter) {
+        collidePoint(point, collector, bplFilter, new ObjectLayerFilter());
+    }
+
+    /**
+     * Collect collisions with the specified point.
+     *
+     * @param point the location of the point to test (not null, unaffected)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     * @param olFilter the object-layer filter to apply (not null, unaffected)
+     */
+    public void collidePoint(RVec3Arg point, CollidePointCollector collector,
+            BroadPhaseLayerFilter bplFilter, ObjectLayerFilter olFilter) {
+        collidePoint(point, collector, bplFilter, olFilter, new BodyFilter());
+    }
+
+    /**
+     * Collect collisions with the specified point.
+     *
+     * @param point the location of the point to test (not null, unaffected)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     * @param olFilter the object-layer filter to apply (not null, unaffected)
+     * @param bodyFilter the body filter to apply (not null, unaffected)
+     */
+    public void collidePoint(RVec3Arg point, CollidePointCollector collector,
+            BroadPhaseLayerFilter bplFilter, ObjectLayerFilter olFilter,
+            BodyFilter bodyFilter) {
+        collidePoint(point, collector, bplFilter, olFilter, bodyFilter,
+                new ShapeFilter());
+    }
+
+    /**
+     * Collect collisions with the specified point.
+     *
+     * @param point the location of the point to test (not null, unaffected)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     * @param olFilter the object-layer filter to apply (not null, unaffected)
+     * @param bodyFilter the body filter to apply (not null, unaffected)
+     * @param shapeFilter the shape filter to apply (not null, unaffected)
+     */
+    public void collidePoint(RVec3Arg point, CollidePointCollector collector,
+            BroadPhaseLayerFilter bplFilter, ObjectLayerFilter olFilter,
+            BodyFilter bodyFilter, ShapeFilter shapeFilter) {
+        long queryVa = va();
+        double xx = point.xx();
+        double yy = point.yy();
+        double zz = point.zz();
+        long collectorVa = collector.va();
+        long bplFilterVa = bplFilter.va();
+        long olFilterVa = olFilter.va();
+        long bodyFilterVa = bodyFilter.va();
+        long shapeFilterVa = shapeFilter.va();
+        collidePoint(queryVa, xx, yy, zz, collectorVa, bplFilterVa,
+                olFilterVa, bodyFilterVa, shapeFilterVa);
+    }
+
+    /**
+     * Collect collisions with the specified shape.
+     *
+     * @param shape the shape to test (not null, unaffected)
+     * @param shapeScale the scaling vector for the shape (not null, unaffected)
+     * @param comTransform the coordinate transform to apply to the shape's
+     * center of mass (not null, unaffected)
+     * @param settings the collision settings to use (not null, unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     */
+    public void collideShape(ConstShape shape, Vec3Arg shapeScale,
+            RMat44Arg comTransform, CollideShapeSettings settings,
+            RVec3Arg base, CollideShapeCollector collector) {
+        collideShape(shape, shapeScale, comTransform, settings, base, collector,
+                new BroadPhaseLayerFilter());
+    }
+
+    /**
+     * Collect collisions with the specified shape.
+     *
+     * @param shape the shape to test (not null, unaffected)
+     * @param shapeScale the scaling vector for the shape (not null, unaffected)
+     * @param comTransform the coordinate transform to apply to the shape's
+     * center of mass (not null, unaffected)
+     * @param settings the collision settings to use (not null, unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     */
+    public void collideShape(ConstShape shape, Vec3Arg shapeScale,
+            RMat44Arg comTransform, CollideShapeSettings settings,
+            RVec3Arg base, CollideShapeCollector collector,
+            BroadPhaseLayerFilter bplFilter) {
+        collideShape(shape, shapeScale, comTransform, settings, base, collector,
+                bplFilter, new ObjectLayerFilter());
+    }
+
+    /**
+     * Collect collisions with the specified shape.
+     *
+     * @param shape the shape to test (not null, unaffected)
+     * @param shapeScale the scaling vector for the shape (not null, unaffected)
+     * @param comTransform the coordinate transform to apply to the shape's
+     * center of mass (not null, unaffected)
+     * @param settings the collision settings to use (not null, unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     * @param olFilter the object-layer filter to apply (not null, unaffected)
+     */
+    public void collideShape(ConstShape shape, Vec3Arg shapeScale,
+            RMat44Arg comTransform, CollideShapeSettings settings,
+            RVec3Arg base, CollideShapeCollector collector,
+            BroadPhaseLayerFilter bplFilter, ObjectLayerFilter olFilter) {
+        collideShape(shape, shapeScale, comTransform, settings, base, collector,
+                bplFilter, olFilter, new BodyFilter());
+    }
+
+    /**
+     * Collect collisions with the specified shape.
+     *
+     * @param shape the shape to test (not null, unaffected)
+     * @param shapeScale the scaling vector for the shape (not null, unaffected)
+     * @param comTransform the coordinate transform to apply to the shape's
+     * center of mass (not null, unaffected)
+     * @param settings the collision settings to use (not null, unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     * @param olFilter the object-layer filter to apply (not null, unaffected)
+     * @param bodyFilter the body filter to apply (not null, unaffected)
+     */
+    public void collideShape(ConstShape shape, Vec3Arg shapeScale,
+            RMat44Arg comTransform, CollideShapeSettings settings,
+            RVec3Arg base, CollideShapeCollector collector,
+            BroadPhaseLayerFilter bplFilter, ObjectLayerFilter olFilter,
+            BodyFilter bodyFilter) {
+        collideShape(shape, shapeScale, comTransform, settings, base, collector,
+                bplFilter, olFilter, bodyFilter, new ShapeFilter());
+    }
+
+    /**
+     * Collect collisions with the specified shape.
+     *
+     * @param shape the shape to test (not null, unaffected)
+     * @param shapeScale the scaling vector for the shape (not null, unaffected)
+     * @param comTransform the coordinate transform to apply to the shape's
+     * center of mass (not null, unaffected)
+     * @param settings the collision settings to use (not null, unaffected)
+     * @param base the base location for reporting hits (not null, unaffected,
+     * (0,0,0)&rarr;world coordinates)
+     * @param collector the hit collector to use (not null)
+     * @param bplFilter the broadphase-layer filter to apply (not null,
+     * unaffected)
+     * @param olFilter the object-layer filter to apply (not null, unaffected)
+     * @param bodyFilter the body filter to apply (not null, unaffected)
+     * @param shapeFilter the shape filter to apply (not null, unaffected)
+     */
+    public void collideShape(ConstShape shape, Vec3Arg shapeScale,
+            RMat44Arg comTransform, CollideShapeSettings settings,
+            RVec3Arg base, CollideShapeCollector collector,
+            BroadPhaseLayerFilter bplFilter, ObjectLayerFilter olFilter,
+            BodyFilter bodyFilter, ShapeFilter shapeFilter) {
+        long queryVa = va();
+        long shapeVa = shape.va();
+        float sx = shapeScale.getX();
+        float sy = shapeScale.getY();
+        float sz = shapeScale.getZ();
+        long transformVa = comTransform.va();
+        long settingsVa = settings.va();
+        double baseX = base.xx();
+        double baseY = base.yy();
+        double baseZ = base.zz();
+        long collectorVa = collector.va();
+        long bplFilterVa = bplFilter.va();
+        long olFilterVa = olFilter.va();
+        long bodyFilterVa = bodyFilter.va();
+        long shapeFilterVa = shapeFilter.va();
+        collideShape(queryVa, shapeVa, sx, sy, sz, transformVa, settingsVa,
+                baseX, baseY, baseZ, collectorVa, bplFilterVa,
+                olFilterVa, bodyFilterVa, shapeFilterVa);
+    }
+
+    /**
      * Access the underlying {@code PhysicsSystem}.
      *
      * @return the pre-existing instance
@@ -243,4 +568,18 @@ public class NarrowPhaseQuery extends NonCopyable {
     native private static void castRay(long queryVa, long raycastVa,
             long settingsVa, long collectorVa, long bplFilterVa,
             long olFilterVa, long bodyFilterVa, long shapeFilterVa);
+
+    native private static void castShape(long queryVa, long shapeCastVa,
+            long settingsVa, long collectorVa, long bplFilterVa,
+            long olFilterVa, long bodyFilterVa, long shapeFilterVa);
+
+    native private static void collidePoint(long queryVa, double xx, double yy,
+            double zz, long collectorVa, long bplFilterVa,
+            long olFilterVa, long bodyFilterVa, long shapeFilterVa);
+
+    native private static void collideShape(long queryVa, long shapeVa,
+            float sx, float sy, float sz, long transformVa, long settingsVa,
+            double baseX, double baseY, double baseZ, long collectorVa,
+            long bplFilterVa, long olFilterVa, long bodyFilterVa,
+            long shapeFilterVa);
 }
