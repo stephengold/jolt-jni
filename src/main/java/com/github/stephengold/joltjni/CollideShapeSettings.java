10bb80b6658c0d12d8ac713dae677a5f4d55ba38
stephengold <sgold@sonic.net>
2024-09-11 15:59:23 -0700

    add the CollideShapeSettings class

---
+++ src/main/java/com/github/stephengold/joltjni/CollideShapeSettings.java
@@ -0,0 +1,107 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+import com.github.stephengold.joltjni.enumerate.EBackFaceMode;
+
+/**
+ * Configurable options for a collide-shape query.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class CollideShapeSettings extends CollideSettingsBase {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate default settings.
+     */
+    public CollideShapeSettings() {
+        long settingsVa = createDefault();
+        setVirtualAddress(settingsVa, true);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the treatment of back-facing triangles. The settings are
+     * unaffected. (native attribute: mBackFaceMode)
+     *
+     * @return the enum value (not null)
+     */
+    public EBackFaceMode getBackFaceMode() {
+        long settingsVa = va();
+        int ordinal = getBackFaceMode(settingsVa);
+        EBackFaceMode result = EBackFaceMode.values()[ordinal];
+
+        return result;
+    }
+
+    /**
+     * Return the maximum separation for which contacts will be reported. The
+     * settings are unaffected. (native attribute: mMaxSeparationDistance)
+     *
+     * @return the separation limit (in meters)
+     */
+    public float getMaxSeparationDistance() {
+        long settingsVa = va();
+        float result = getMaxSeparationDistance(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Alter the treatment of back-facing triangles. (native attribute:
+     * mBackFaceMode)
+     *
+     * @param mode the desired mode (not null, default=IgnoreBackFaces)
+     */
+    public void setBackFaceMode(EBackFaceMode mode) {
+        long settingsVa = va();
+        int ordinal = mode.ordinal();
+        setBackFaceMode(settingsVa, ordinal);
+    }
+
+    /**
+     * Alter the maximum separation for which contacts will be reported. (native
+     * attribute: mMaxSeparationDistance)
+     *
+     * @param distance the desired separation limit (in meters, default=0)
+     */
+    public void setMaxSeparationDistance(float distance) {
+        long settingsVa = va();
+        setMaxSeparationDistance(settingsVa, distance);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createDefault();
+
+    native private static int getBackFaceMode(long settingsVa);
+
+    native private static float getMaxSeparationDistance(long settingsVa);
+
+    native private static void setBackFaceMode(long settingsVa, int ordinal);
+
+    native private static void setMaxSeparationDistance(
+            long settingsVa, float distance);
+}
---
+++ src/main/native/glue/co/CollideShapeSettings.cpp
@@ -0,0 +1,94 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Collision/CollideShape.h>
+#include "auto/com_github_stephengold_joltjni_CollideShapeSettings.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_CollideShapeSettings
+ * Method:    createDefault
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_CollideShapeSettings_createDefault
+  (JNIEnv *, jclass) {
+    CollideShapeSettings * const pSettings = new CollideShapeSettings();
+    TRACE_NEW("CollideShapeSettings", pSettings);
+    return reinterpret_cast<jlong> (pSettings);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_CollideShapeSettings
+ * Method:    getBackFaceMode
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_CollideShapeSettings_getBackFaceMode
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const CollideShapeSettings * const pSettings
+            = reinterpret_cast<CollideShapeSettings *> (settingsVa);
+    const EBackFaceMode result = pSettings->mBackFaceMode;
+    return (jint) result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_CollideShapeSettings
+ * Method:    getMaxSeparationDistance
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_CollideShapeSettings_getMaxSeparationDistance
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const CollideShapeSettings * const pSettings
+            = reinterpret_cast<CollideShapeSettings *> (settingsVa);
+    const float result = pSettings->mMaxSeparationDistance;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_CollideShapeSettings
+ * Method:    setBackFaceMode
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_CollideShapeSettings_setBackFaceMode
+  (JNIEnv *, jclass, jlong settingsVa, jint ordinal) {
+    CollideShapeSettings * const pSettings
+            = reinterpret_cast<CollideShapeSettings *> (settingsVa);
+    const EBackFaceMode mode = (EBackFaceMode) ordinal;
+    pSettings->mBackFaceMode = mode;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_CollideShapeSettings
+ * Method:    setMaxSeparationDistance
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_CollideShapeSettings_setMaxSeparationDistance
+  (JNIEnv *, jclass, jlong settingsVa, jfloat distance) {
+    CollideShapeSettings * const pSettings
+            = reinterpret_cast<CollideShapeSettings *> (settingsVa);
+    pSettings->mMaxSeparationDistance = distance;
+}
