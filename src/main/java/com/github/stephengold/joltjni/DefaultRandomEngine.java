6e405bb71a172d8df1b15fa670810f778e833938
stephengold <sgold@sonic.net>
2024-09-17 16:02:04 -0700

    add the DefaultRandomEngine class

---
+++ src/main/java/com/github/stephengold/joltjni/DefaultRandomEngine.java
@@ -0,0 +1,78 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Generate a pseudo-random sequence of integers for casual use. (native type:
+ * std::default_random_engine)
+ * <p>
+ * The algorithm isn't specific to Jolt Physics; it's included in jolt-jni for
+ * expediency, since BroadPhaseTest.cpp uses it.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class DefaultRandomEngine extends JoltPhysicsObject {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a generator.
+     */
+    public DefaultRandomEngine() {
+        long generatorVa = createDefault();
+        setVirtualAddress(generatorVa, () -> free(generatorVa));
+    }
+
+    /**
+     * Instantiate a generator using the specified 32-bit seed.
+     *
+     * @param seed for initialization
+     */
+    public DefaultRandomEngine(int seed) {
+        long generatorVa = createSeeded(seed);
+        setVirtualAddress(generatorVa, () -> free(generatorVa));
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the next integer in the sequence.
+     *
+     * @return an integer value
+     */
+    public int nextInt() {
+        long generatorVa = va();
+        int result = nextInt(generatorVa);
+
+        return result;
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createDefault();
+
+    native private static long createSeeded(int seed);
+
+    native private static void free(long generatorVa);
+
+    native private static int nextInt(long generatorVa);
+}
---
+++ src/main/native/glue/d/DefaultRandomEngine.cpp
@@ -0,0 +1,80 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include "auto/com_github_stephengold_joltjni_DefaultRandomEngine.h"
+#include "glue/glue.h"
+
+using namespace std;
+
+/*
+ * Class:     com_github_stephengold_joltjni_DefaultRandomEngine
+ * Method:    createDefault
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_DefaultRandomEngine_createDefault
+  (JNIEnv *, jclass) {
+    default_random_engine * const pResult = new default_random_engine();
+    TRACE_NEW("default_random_engine", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_DefaultRandomEngine
+ * Method:    createSeeded
+ * Signature: (I)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_DefaultRandomEngine_createSeeded
+  (JNIEnv *, jclass, jint seed) {
+    default_random_engine * const pResult = new default_random_engine(seed);
+    TRACE_NEW("default_random_engine", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_DefaultRandomEngine
+ * Method:    free
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_DefaultRandomEngine_free
+  (JNIEnv *, jclass, jlong generatorVa) {
+    default_random_engine * const pGenerator
+            = reinterpret_cast<default_random_engine *> (generatorVa);
+    TRACE_DELETE("default_random_engine", pGenerator)
+    delete pGenerator;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_DefaultRandomEngine
+ * Method:    nextInt
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_DefaultRandomEngine_nextInt
+  (JNIEnv *, jclass, jlong generatorVa) {
+    default_random_engine * const pGenerator
+            = reinterpret_cast<default_random_engine *> (generatorVa);
+    default_random_engine::result_type result = (*pGenerator)();
+    return result;
+}
