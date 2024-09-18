84c671b5c3c98f54acb57d70de53eb55bfffc324
stephengold <sgold@sonic.net>
2024-09-18 11:56:40 -0700

    add the BodyVector class

---
+++ src/main/java/com/github/stephengold/joltjni/BodyVector.java
@@ -0,0 +1,141 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+import com.github.stephengold.joltjni.template.Array;
+
+/**
+ * A variable-length vector (array) of bodies. (native type:
+ * {@code Array<Body *>})
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class BodyVector extends Array<Body> {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate an empty vector.
+     */
+    public BodyVector() {
+        long vectorVa = createBodyVector();
+        setVirtualAddress(vectorVa, () -> free(vectorVa));
+    }
+
+    /**
+     * Instantiate a vector with the specified native object assigned.
+     *
+     * @param vectorVa the virtual address of the native object to assign (not
+     * zero)
+     * @param owner true &rarr; make the current object the owner, false &rarr;
+     * the current object isn't the owner
+     */
+    BodyVector(long vectorVa, boolean owner) {
+        Runnable freeingAction = owner ? () -> free(vectorVa) : null;
+        setVirtualAddress(vectorVa, freeingAction);
+    }
+    // *************************************************************************
+    // Array<Body> methods
+
+    /**
+     * Count how many bodies the currently allocated storage can hold. The
+     * vector is unaffected.
+     *
+     * @return the number of bodies (&ge;size)
+     */
+    @Override
+    public int capacity() {
+        long vectorVa = va();
+        int result = capacity(vectorVa);
+
+        return result;
+    }
+
+    /**
+     * Access the body at the specified index.
+     *
+     * @param elementIndex the index from which to get the body (&ge;0)
+     * @return a new JVM object with the pre-existing native object assigned
+     */
+    @Override
+    public Body get(int elementIndex) {
+        long vectorVa = va();
+        long bodyVa = getBody(vectorVa, elementIndex);
+        Body result = new Body(bodyVa);
+
+        return result;
+    }
+
+    /**
+     * Expand or truncate the vector.
+     *
+     * @param numBodies the desired size (number of bodies, &ge;0)
+     */
+    @Override
+    public void resize(int numBodies) {
+        long vectorVa = va();
+        resize(vectorVa, numBodies);
+    }
+
+    /**
+     * Put the specified body at the specified index.
+     *
+     * @param elementIndex the index at which to put the body (&ge;0)
+     * @param body the body to put (not null)
+     */
+    @Override
+    public void set(int elementIndex, Body body) {
+        long vectorVa = va();
+        long bodyVa = body.va();
+        setBody(vectorVa, elementIndex, bodyVa);
+    }
+
+    /**
+     * Count how many bodies are in the vector. The vector is unaffected.
+     *
+     * @return the number of bodies (&ge;0, &le;capacity)
+     */
+    @Override
+    public int size() {
+        long vectorVa = va();
+        int result = size(vectorVa);
+
+        return result;
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static int capacity(long vectorVa);
+
+    native private static long createBodyVector();
+
+    native private static void free(long vectorVa);
+
+    native private static long getBody(long vectorVa, int elementIndex);
+
+    native private static void resize(long vectorVa, int numBodies);
+
+    native private static void setBody(
+            long vectorVa, int elementIndex, long bodyVa);
+
+    native private static int size(long vectorVa);
+}
---
+++ src/main/native/glue/b/BodyVector.cpp
@@ -0,0 +1,116 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Body/BodyManager.h>
+#include "auto/com_github_stephengold_joltjni_BodyVector.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_BodyVector
+ * Method:    capacity
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_BodyVector_capacity
+  (JNIEnv *, jclass, jlong vectorVa) {
+    const BodyVector * const pVector
+            = reinterpret_cast<BodyVector *> (vectorVa);
+    const BodyVector::size_type result = pVector->capacity();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_BodyVector
+ * Method:    createBodyVector
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_BodyVector_createBodyVector
+  (JNIEnv *, jclass) {
+    BodyVector * const pVector = new BodyVector();
+    TRACE_NEW("BodyVector", pVector)
+    return reinterpret_cast<jlong> (pVector);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_BodyVector
+ * Method:    free
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_BodyVector_free
+  (JNIEnv *, jclass, jlong vectorVa) {
+    BodyVector * const pVector = reinterpret_cast<BodyVector *> (vectorVa);
+    TRACE_DELETE("BodyVector", pVector)
+    delete pVector;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_BodyVector
+ * Method:    getBody
+ * Signature: (JI)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_BodyVector_getBody
+  (JNIEnv *, jclass, jlong vectorVa, jint elementIndex) {
+    BodyVector * const pVector = reinterpret_cast<BodyVector *> (vectorVa);
+    Body * pResult = pVector->at(elementIndex);
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_BodyVector
+ * Method:    resize
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_BodyVector_resize
+  (JNIEnv *, jclass, jlong vectorVa, jint numElements) {
+    BodyVector * const pVector = reinterpret_cast<BodyVector *> (vectorVa);
+    pVector->resize(numElements);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_BodyVector
+ * Method:    setBody
+ * Signature: (JIJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_BodyVector_setBody
+  (JNIEnv *, jclass, jlong vectorVa, jint elementIndex, jlong bodyVa) {
+    BodyVector * const pVector = reinterpret_cast<BodyVector *> (vectorVa);
+    Body * const pBody = reinterpret_cast<Body *> (bodyVa);
+    pVector->at(elementIndex) = pBody;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_BodyVector
+ * Method:    size
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_BodyVector_size
+  (JNIEnv *, jclass, jlong vectorVa) {
+    const BodyVector * const pVector
+            = reinterpret_cast<BodyVector *> (vectorVa);
+    const BodyVector::size_type result = pVector->size();
+    return result;
+}
