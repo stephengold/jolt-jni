07bf74a40c0e3c12640a6c10ec38a08b4f9de08c
stephengold <sgold@sonic.net>
2024-09-21 12:19:21 -0700

    bugfix:  typographic error in Body.cpp

--- src/main/native/glue/b/Body.cpp
+++ src/main/native/glue/b/Body.cpp
@@ -345,7 +345,7 @@ JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Body_getId
   (JNIEnv *, jclass, jlong bodyVa) {
     const Body * const pBody = reinterpret_cast<Body *> (bodyVa);
     const BodyID &id = pBody->GetID();
-    BodyId * pResult = new BodyID(id);
+    BodyID * pResult = new BodyID(id);
     TRACE_NEW("BodyID", pResult)
     return reinterpret_cast<jlong> (pResult);
 }
