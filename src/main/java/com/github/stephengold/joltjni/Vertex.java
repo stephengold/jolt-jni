718c472ec0d3b8d37da0016224d1a5f68fde2a05
stephengold <sgold@sonic.net>
2024-10-08 15:34:07 -0700

    add the Vertex class

---
+++ src/main/java/com/github/stephengold/joltjni/Vertex.java
@@ -0,0 +1,155 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+import com.github.stephengold.joltjni.readonly.Vec3Arg;
+
+/**
+ * Attributes of a particle, for use during the creation of a soft body. (native
+ * type: {@code SoftBodySharedSettings::Vertex})
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class Vertex extends JoltPhysicsObject {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a default vertex.
+     */
+    public Vertex() {
+        long vertexVa = createDefault();
+        setVirtualAddress(vertexVa, () -> free(vertexVa));
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the inverse mass. The vertex is unaffected. (native attribute:
+     * mInvMass)
+     *
+     * @return the inverse of the mass (in 1/kilograms)
+     */
+    public float getInvMass() {
+        long vertexVa = va();
+        float result = getInvMass(vertexVa);
+
+        return result;
+    }
+
+    /**
+     * Copy the initial location. The vertex is unaffected. (native attribute:
+     * mPosition)
+     *
+     * @return a new location vector
+     */
+    public Float3 getPosition() {
+        long vertexVa = va();
+        float x = getPositionX(vertexVa);
+        float y = getPositionY(vertexVa);
+        float z = getPositionZ(vertexVa);
+        Float3 result = new Float3(x, y, z);
+
+        return result;
+    }
+
+    /**
+     * Copy the initial velocity. The vertex is unaffected. (native attribute:
+     * mVelocity)
+     *
+     * @return a new velocity vector (in meters per second)
+     */
+    public Float3 getVelocity() {
+        long vertexVa = va();
+        float x = getVelocityX(vertexVa);
+        float y = getVelocityY(vertexVa);
+        float z = getVelocityZ(vertexVa);
+        Float3 result = new Float3(x, y, z);
+
+        return result;
+    }
+
+    /**
+     * Alter the inverse mass.
+     *
+     * @param invMass the desired inverse mass (in 1/kilograms)
+     */
+    public void setInvMass(float invMass) {
+        long vertexVa = va();
+        setInvMass(vertexVa, invMass);
+    }
+
+    /**
+     * Alter the initial location of the vertex.
+     *
+     * @param location the desired location (not null, unaffected)
+     */
+    public void setPosition(Vec3Arg location) {
+        long vertexVa = va();
+        float x = location.getX();
+        float y = location.getY();
+        float z = location.getZ();
+        setPosition(vertexVa, x, y, z);
+    }
+
+    /**
+     * Alter the initial velocity of the vertex.
+     *
+     * @param velocity the desired velocity (in meters per second, not null,
+     * unaffected)
+     */
+    public void setVelocity(Vec3Arg velocity) {
+        long vertexVa = va();
+        float x = velocity.getX();
+        float y = velocity.getY();
+        float z = velocity.getZ();
+        setVelocity(vertexVa, x, y, z);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createDefault();
+
+    native private static void free(long vertexVa);
+
+    native private static float getInvMass(long vertexVa);
+
+    native private static float getPositionX(long vertexVa);
+
+    native private static float getPositionY(long vertexVa);
+
+    native private static float getPositionZ(long vertexVa);
+
+    native private static float getVelocityX(long vertexVa);
+
+    native private static float getVelocityY(long vertexVa);
+
+    native private static float getVelocityZ(long vertexVa);
+
+    native private static void setInvMass(long vertexVa, float invMass);
+
+    native private static void setPosition(
+            long vertexVa, float x, float y, float z);
+
+    native private static void setVelocity(
+            long vertexVa, float vx, float vy, float vz);
+}
---
+++ src/main/native/glue/v/Vertex.cpp
@@ -0,0 +1,186 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "Jolt/Jolt.h"
+#include "Jolt/Physics/SoftBody/SoftBodySharedSettings.h"
+#include "auto/com_github_stephengold_joltjni_Vertex.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    createDefault
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Vertex_createDefault
+  (JNIEnv *, jclass) {
+    SoftBodySharedSettings::Vertex * const pResult
+            = new SoftBodySharedSettings::Vertex();
+    TRACE_NEW("SoftBodySharedSettings::Vertex", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    free
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Vertex_free
+  (JNIEnv *, jclass, jlong vertexVa) {
+    SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    TRACE_DELETE("SoftBodySharedSettings::Vertex", pVertex)
+    delete pVertex;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    getInvMass
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Vertex_getInvMass
+  (JNIEnv *, jclass, jlong vertexVa) {
+    const SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    const float result = pVertex->mInvMass;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    getPositionX
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Vertex_getPositionX
+  (JNIEnv *, jclass, jlong vertexVa) {
+    const SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    const float result = pVertex->mPosition.x;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    getPositionY
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Vertex_getPositionY
+  (JNIEnv *, jclass, jlong vertexVa) {
+    const SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    const float result = pVertex->mPosition.y;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    getPositionZ
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Vertex_getPositionZ
+  (JNIEnv *, jclass, jlong vertexVa) {
+    const SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    const float result = pVertex->mPosition.z;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    getVelocityX
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Vertex_getVelocityX
+  (JNIEnv *, jclass, jlong vertexVa) {
+    const SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    const float result = pVertex->mVelocity.x;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    getVelocityY
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Vertex_getVelocityY
+  (JNIEnv *, jclass, jlong vertexVa) {
+    const SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    const float result = pVertex->mVelocity.y;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    getVelocityZ
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Vertex_getVelocityZ
+  (JNIEnv *, jclass, jlong vertexVa) {
+    const SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    const float result = pVertex->mVelocity.z;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    setInvMass
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Vertex_setInvMass
+  (JNIEnv *, jclass, jlong vertexVa, jfloat invMass) {
+    SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    pVertex->mInvMass = invMass;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    setPosition
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Vertex_setPosition
+  (JNIEnv *, jclass, jlong vertexVa, jfloat x, jfloat y, jfloat z) {
+    SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    const Float3 position(x, y, z);
+    pVertex->mPosition = position;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Vertex
+ * Method:    setVelocity
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Vertex_setVelocity
+  (JNIEnv *, jclass, jlong vertexVa, jfloat vx, jfloat vy, jfloat vz) {
+    SoftBodySharedSettings::Vertex * const pVertex
+            = reinterpret_cast<SoftBodySharedSettings::Vertex *> (vertexVa);
+    const Float3 velocity(vx, vy, vz);
+    pVertex->mVelocity = velocity;
+}
