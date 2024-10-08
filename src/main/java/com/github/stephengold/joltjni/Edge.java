5057599bad7ec8cca71e3f18d3f5bd2b3b044a3d
stephengold <sgold@sonic.net>
2024-10-08 16:20:02 -0700

    add the Edge class

---
+++ src/main/java/com/github/stephengold/joltjni/Edge.java
@@ -0,0 +1,132 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Join 2 soft-body vertices with a spring. (native type:
+ * {@code SoftBodySharedSettings::Edge})
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class Edge extends JoltPhysicsObject {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a default edge constraint.
+     */
+    public Edge() {
+        long edgeVa = createDefault();
+        setVirtualAddress(edgeVa, () -> free(edgeVa));
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the inverse of the spring's stiffness. The edge is unaffected.
+     * (native attribute: mCompliance)
+     *
+     * @return the compliance value
+     */
+    public float getCompliance() {
+        long edgeVa = va();
+        float result = getCompliance(edgeVa);
+
+        return result;
+    }
+
+    /**
+     * Return the rest length of the spring. The edge is unaffected. (native
+     * attribute: mRestLength)
+     *
+     * @return the length (in meters)
+     */
+    public float getRestLength() {
+        long edgeVa = va();
+        float result = getRestLength(edgeVa);
+
+        return result;
+    }
+
+    /**
+     * Return the mesh vertex at the specified end. (native attribute: mVertex)
+     *
+     * @param indexInEdge which end of the edge (0 or 1)
+     * @return the mesh index of the vertex (&ge;0)
+     */
+    public int getVertex(int indexInEdge) {
+        long edgeVa = va();
+        int result = getVertex(edgeVa, indexInEdge);
+
+        return result;
+    }
+
+    /**
+     * Alter the stiffness of the spring. (native attribute: mCompliance)
+     *
+     * @param compliance the inverse of the desired stiffness (default=0)
+     */
+    public void setCompliance(float compliance) {
+        long edgeVa = va();
+        setCompliance(edgeVa, compliance);
+    }
+
+    /**
+     * Alter the rest length of the spring. (native attribute: mRestLength)
+     *
+     * @param length the desired length (in meters, default=1)
+     */
+    public void setRestLength(float length) {
+        long edgeVa = va();
+        setRestLength(edgeVa, length);
+    }
+
+    /**
+     * Assign the specified mesh vertex to the edge. (native attribute: mVertex)
+     *
+     * @param indexInEdge which end of the edge (0 or 1)
+     * @param indexInMesh the index of the vertex to assign (&ge;0)
+     */
+    public void setVertex(int indexInEdge, int indexInMesh) {
+        long edgeVa = va();
+        setVertex(edgeVa, indexInEdge, indexInMesh);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createDefault();
+
+    native private static void free(long edgeVa);
+
+    native private static float getCompliance(long edgeVa);
+
+    native private static float getRestLength(long edgeVa);
+
+    native private static int getVertex(long edgeVa, int indexInEdge);
+
+    native private static void setCompliance(long edgeVa, float compliance);
+
+    native private static void setRestLength(long edgeVa, float length);
+
+    native private static void setVertex(
+            long edgeVa, int indexInEdge, int indexInMesh);
+}
---
+++ src/main/native/glue/e/Edge.cpp
@@ -0,0 +1,132 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "Jolt/Jolt.h"
+#include "Jolt/Physics/SoftBody/SoftBodySharedSettings.h"
+#include "auto/com_github_stephengold_joltjni_Edge.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_Edge
+ * Method:    createDefault
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Edge_createDefault
+  (JNIEnv *, jclass) {
+    SoftBodySharedSettings::Edge * const pResult
+            = new SoftBodySharedSettings::Edge();
+    TRACE_NEW("SoftBodySharedSettings::Edge", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Edge
+ * Method:    free
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Edge_free
+  (JNIEnv *, jclass, jlong edgeVa) {
+    SoftBodySharedSettings::Edge * const pEdge
+            = reinterpret_cast<SoftBodySharedSettings::Edge *> (edgeVa);
+    TRACE_DELETE("SoftBodySharedSettings::Edge", pEdge)
+    delete pEdge;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Edge
+ * Method:    getCompliance
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Edge_getCompliance
+  (JNIEnv *, jclass, jlong edgeVa) {
+    const SoftBodySharedSettings::Edge * const pEdge
+            = reinterpret_cast<SoftBodySharedSettings::Edge *> (edgeVa);
+    const float result = pEdge->mCompliance;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Edge
+ * Method:    getRestLength
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Edge_getRestLength
+  (JNIEnv *, jclass, jlong edgeVa) {
+    const SoftBodySharedSettings::Edge * const pEdge
+            = reinterpret_cast<SoftBodySharedSettings::Edge *> (edgeVa);
+    const float result = pEdge->mRestLength;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Edge
+ * Method:    getVertex
+ * Signature: (JI)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_Edge_getVertex
+  (JNIEnv *, jclass, jlong edgeVa, jint indexInEdge) {
+    const SoftBodySharedSettings::Edge * const pEdge
+            = reinterpret_cast<SoftBodySharedSettings::Edge *> (edgeVa);
+    const uint32 result = pEdge->mVertex[indexInEdge];
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Edge
+ * Method:    setCompliance
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Edge_setCompliance
+  (JNIEnv *, jclass, jlong edgeVa, jfloat compliance) {
+    SoftBodySharedSettings::Edge * const pEdge
+            = reinterpret_cast<SoftBodySharedSettings::Edge *> (edgeVa);
+    pEdge->mCompliance = compliance;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Edge
+ * Method:    setRestLength
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Edge_setRestLength
+  (JNIEnv *, jclass, jlong edgeVa, jfloat restLength) {
+    SoftBodySharedSettings::Edge * const pEdge
+            = reinterpret_cast<SoftBodySharedSettings::Edge *> (edgeVa);
+    pEdge->mRestLength = restLength;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Edge
+ * Method:    setVertex
+ * Signature: (JII)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Edge_setVertex
+  (JNIEnv *, jclass, jlong edgeVa, jint indexInEdge, jint indexInMesh) {
+    SoftBodySharedSettings::Edge * const pEdge
+            = reinterpret_cast<SoftBodySharedSettings::Edge *> (edgeVa);
+    pEdge->mVertex[indexInEdge] = indexInMesh;
+}
