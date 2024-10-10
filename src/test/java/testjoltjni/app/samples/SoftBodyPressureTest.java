f5f3175a415ba838d60dde2e845caccc140b574e
stephengold <sgold@sonic.net>
2024-10-10 11:31:23 -0700

    app.samples:  add the SoftBodyPressureTest class

---
+++ src/test/java/testjoltjni/app/samples/SoftBodyPressureTest.java
@@ -0,0 +1,48 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package testjoltjni.app.samples;
+import com.github.stephengold.joltjni.*;
+import com.github.stephengold.joltjni.enumerate.*;
+/**
+ * A line-for-line Java translation of the Jolt Physics soft-body pressure test.
+ * <p>
+ * Compare with the original by Jorrit Rouwe at
+ * https://github.com/jrouwe/JoltPhysics/blob/master/Samples/Tests/SoftBody/SoftBodyPressureTest.cpp
+ */
+class SoftBodyPressureTest extends Test{
+
+void Initialize()
+{
+	// Floor
+	CreateFloor();
+
+	// Bodies with increasing pressure
+	SoftBodyCreationSettings sphere=new SoftBodyCreationSettings(SoftBodyCreator.CreateSphere(2.0f).getPtr(), RVec3.sZero(), Quat.sIdentity(), Layers.MOVING);
+
+	for (int i = 0; i <= 10; ++i)
+	{
+		sphere.setPosition (new RVec3(-50.0f + i * 10.0f, 10.0f, 0));
+		sphere.setPressure ( 1000.0f * i);
+		mBodyInterface.createAndAddSoftBody(sphere, EActivation.Activate);
+	}
+}
+}
