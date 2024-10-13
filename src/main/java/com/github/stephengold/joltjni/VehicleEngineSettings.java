0bc9ff736826b0abd363c42c03afed01cc2d2a06
stephengold <sgold@sonic.net>
2024-10-13 15:27:01 -0700

    add the VehicleEngineSettings class

---
+++ src/main/java/com/github/stephengold/joltjni/VehicleEngineSettings.java
@@ -0,0 +1,195 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Settings used to configure the engine of a {@code WheeledVehicleController}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class VehicleEngineSettings extends JoltPhysicsObject {
+    // *************************************************************************
+    // fields
+
+    /**
+     * prevent premature garbage collection of the underlying
+     * {@code WheeledVehicleControllerSettings}
+     */
+    final private WheeledVehicleControllerSettings vehicle;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a settings with the specified native object assigned but not
+     * owned.
+     *
+     * @param vehicle the underlying {@code WheeledVehicleControllerSettings}
+     * @param settingsVa the virtual address of the native object to assign (not
+     * zero)
+     */
+    VehicleEngineSettings(
+            WheeledVehicleControllerSettings vehicle, long settingsVa) {
+        this.vehicle = vehicle;
+        setVirtualAddress(settingsVa, null);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the angular damping factor. The settings are unaffected. (native
+     * attribute: mAngularDamping)
+     *
+     * @return the damping factor (per second)
+     */
+    public float getAngularDamping() {
+        long settingsVa = va();
+        float result = getAngularDamping(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the engine's rotational inertia. The settings are unaffected.
+     * (native attribute: mInertia)
+     *
+     * @return the inertia (in kilogram.meter^2)
+     */
+    public float getInertia() {
+        long settingsVa = va();
+        float result = getInertia(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the maximum rotation rate. The settings are unaffected. (native
+     * attribute: mMaxRPM)
+     *
+     * @return the limit (in revolutions per minute)
+     */
+    public float getMaxRpm() {
+        long settingsVa = va();
+        float result = getMaxRpm(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the maximum torque. The settings are unaffected. (native
+     * attribute: mMaxTorque)
+     *
+     * @return the limit (in Newton.meters)
+     */
+    public float getMaxTorque() {
+        long settingsVa = va();
+        float result = getMaxTorque(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the minimum rotation rate to avoid stalling. The settings are
+     * unaffected. (native attribute: mMinRPM)
+     *
+     * @return the limit (in revolutions per minute)
+     */
+    public float getMinRpm() {
+        long settingsVa = va();
+        float result = getMinRpm(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Alter the angular damping factor. (native attribute: mAngularDamping)
+     *
+     * @param damping the desired damping factor (per second, default=0.2)
+     */
+    public void setAngularDamping(float damping) {
+        long settingsVa = va();
+        setAngularDamping(settingsVa, damping);
+    }
+
+    /**
+     * Alter the engine's rotational inertia. (native attribute: mInertia)
+     *
+     * @param inertia the desired inertia (in kg.meter^2, default=0.5)
+     */
+    public void setInertia(float inertia) {
+        long settingsVa = va();
+        setInertia(settingsVa, inertia);
+    }
+
+    /**
+     * Alter the maximum rotation rate. (native attribute: mMaxRPM)
+     *
+     * @param rpm the desired limit (in revolutions per minute, default=6000)
+     */
+    public void setMaxRpm(float rpm) {
+        long settingsVa = va();
+        setMaxRpm(settingsVa, rpm);
+    }
+
+    /**
+     * Alter the maximum torque. (native attribute: mMaxTorque)
+     *
+     * @param torque the desired limit (in Newton.meters, default=500)
+     */
+    public void setMaxTorque(float torque) {
+        long settingsVa = va();
+        setMaxTorque(settingsVa, torque);
+    }
+
+    /**
+     * Alter the minimum rotation rate to avoid stalling. (native attribute:
+     * mMinRPM)
+     *
+     * @param rpm the desired limit (in revolutions per minute, default=1000)
+     */
+    public void setMinRpm(float rpm) {
+        long settingsVa = va();
+        setMinRpm(settingsVa, rpm);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static float getAngularDamping(long settingsVa);
+
+    native private static float getInertia(long settingsVa);
+
+    native private static float getMaxRpm(long settingsVa);
+
+    native private static float getMaxTorque(long settingsVa);
+
+    native private static float getMinRpm(long settingsVa);
+
+    native private static void setAngularDamping(
+            long settingsVa, float damping);
+
+    native private static void setInertia(long settingsVa, float inertia);
+
+    native private static void setMaxRpm(long settingsVa, float rpm);
+
+    native private static void setMaxTorque(long settingsVa, float torque);
+
+    native private static void setMinRpm(long settingsVa, float rpm);
+}
---
+++ src/main/native/glue/v/VehicleEngineSettings.cpp
@@ -0,0 +1,155 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "Jolt/Jolt.h"
+#include "Jolt/Physics/Vehicle/VehicleEngine.h"
+#include "auto/com_github_stephengold_joltjni_VehicleEngineSettings.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    getAngularDamping
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_getAngularDamping
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    const float result = pSettings->mAngularDamping;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    getInertia
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_getInertia
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    const float result = pSettings->mInertia;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    getMaxRpm
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_getMaxRpm
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    const float result = pSettings->mMaxRPM;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    getMaxTorque
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_getMaxTorque
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    const float result = pSettings->mMaxTorque;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    getMinRpm
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_getMinRpm
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    const float result = pSettings->mMinRPM;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    setAngularDamping
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_setAngularDamping
+  (JNIEnv *, jclass, jlong settingsVa, jfloat damping) {
+    VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    pSettings->mAngularDamping = damping;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    setInertia
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_setInertia
+  (JNIEnv *, jclass, jlong settingsVa, jfloat inertia) {
+    VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    pSettings->mInertia = inertia;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    setMaxRpm
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_setMaxRpm
+  (JNIEnv *, jclass, jlong settingsVa, jfloat rpm) {
+    VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    pSettings->mMaxRPM = rpm;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    setMaxTorque
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_setMaxTorque
+  (JNIEnv *, jclass, jlong settingsVa, jfloat torque) {
+    VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    pSettings->mMaxTorque = torque;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleEngineSettings
+ * Method:    setMinRpm
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_VehicleEngineSettings_setMinRpm
+  (JNIEnv *, jclass, jlong settingsVa, jfloat rpm) {
+    VehicleEngineSettings * const pSettings
+            = reinterpret_cast<VehicleEngineSettings *> (settingsVa);
+    pSettings->mMinRPM = rpm;
+}
