eef52dfef61e975fa4e1a2bc2a73535999e72521
stephengold <sgold@sonic.net>
2024-10-13 15:10:39 -0700

    add the VehicleDifferentialSettings class

---
+++ src/main/java/com/github/stephengold/joltjni/VehicleDifferentialSettings.java
@@ -0,0 +1,146 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Settings used to configure the differential of a
+ * {@code WheeledVehicleController}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class VehicleDifferentialSettings extends JoltPhysicsObject {
+    // *************************************************************************
+    // fields
+
+    /**
+     * prevent premature garbage collection of the underlying
+     * {@code WheeledVehicleControllerSettings}
+     */
+    final private WheeledVehicleControllerSettings vehicle;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a settings with the specified native object assigned but not
+     * owned.
+     *
+     * @param vehicle the underlying {@code WheeledVehicleControllerSettings}
+     * @param settingsVa the virtual address of the native object to assign (not
+     * zero)
+     */
+    VehicleDifferentialSettings(
+            WheeledVehicleControllerSettings vehicle, long settingsVa) {
+        this.vehicle = vehicle;
+        setVirtualAddress(settingsVa, null);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the number of gearbox rotations per wheel rotation. The settings
+     * are unaffected. (native attribute: mDifferentialRatio)
+     *
+     * @return the ratio
+     */
+    public float getDifferentialRatio() {
+        long settingsVa = va();
+        float result = getDifferentialRatio(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the index of left wheel. The settings are unaffected. (native
+     * attribute: mLeftWheel)
+     *
+     * @return the index of the wheel (&ge;0) or -1 for none
+     */
+    public int getLeftWheel() {
+        long settingsVa = va();
+        int result = getLeftWheel(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the index of right wheel. The settings are unaffected. (native
+     * attribute: mRightWheel)
+     *
+     * @return the index of the wheel (&ge;0) or -1 for none
+     */
+    public int getRightWheel() {
+        long settingsVa = va();
+        int result = getRightWheel(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Alter the number of gearbox rotations per wheel rotation. (native
+     * attribute: mDifferentialRatio)
+     *
+     * @param ratio the desired ratio (default=3.42)
+     */
+    public void setDifferentialRatio(float ratio) {
+        long settingsVa = va();
+        setDifferentialRatio(settingsVa, ratio);
+    }
+
+    /**
+     * Alter which left wheel is assigned to the differential. (native
+     * attribute: mLeftWheel)
+     *
+     * @param wheelIndex the index of the wheel (&ge;0) or -1 for none
+     * (default=-1)
+     */
+    public void setLeftWheel(int wheelIndex) {
+        long settingsVa = va();
+        setLeftWheel(settingsVa, wheelIndex);
+    }
+
+    /**
+     * Alter which right wheel is assigned to the differential. (native
+     * attribute: mRightWheel)
+     *
+     * @param wheelIndex the index of the wheel (&ge;0) or -1 for none
+     * (default=-1)
+     */
+    public void setRightWheel(int wheelIndex) {
+        long settingsVa = va();
+        setRightWheel(settingsVa, wheelIndex);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static float getDifferentialRatio(long settingsVa);
+
+    native private static int getLeftWheel(long settingsVa);
+
+    native private static int getRightWheel(long settingsVa);
+
+    native private static void setDifferentialRatio(
+            long settingsVa, float ratio);
+
+    native private static void setLeftWheel(long settingsVa, int wheelIndex);
+
+    native private static void setRightWheel(long settingsVa, int wheelIndex);
+}
---
+++ src/main/native/glue/v/VehicleDifferentialSettings.cpp
@@ -0,0 +1,105 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "Jolt/Jolt.h"
+#include "Jolt/Physics/Vehicle/VehicleDifferential.h"
+#include "auto/com_github_stephengold_joltjni_VehicleDifferentialSettings.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleDifferentialSettings
+ * Method:    getDifferentialRatio
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_VehicleDifferentialSettings_getDifferentialRatio
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const VehicleDifferentialSettings * const pSettings
+            = reinterpret_cast<VehicleDifferentialSettings *> (settingsVa);
+    const float result = pSettings->mDifferentialRatio;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleDifferentialSettings
+ * Method:    getLeftWheel
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_VehicleDifferentialSettings_getLeftWheel
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const VehicleDifferentialSettings * const pSettings
+            = reinterpret_cast<VehicleDifferentialSettings *> (settingsVa);
+    const int result = pSettings->mLeftWheel;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleDifferentialSettings
+ * Method:    getRightWheel
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_VehicleDifferentialSettings_getRightWheel
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const VehicleDifferentialSettings * const pSettings
+            = reinterpret_cast<VehicleDifferentialSettings *> (settingsVa);
+    const int result = pSettings->mRightWheel;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleDifferentialSettings
+ * Method:    setDifferentialRatio
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_VehicleDifferentialSettings_setDifferentialRatio
+  (JNIEnv *, jclass, jlong settingsVa, jfloat ratio) {
+    VehicleDifferentialSettings * const pSettings
+            = reinterpret_cast<VehicleDifferentialSettings *> (settingsVa);
+    pSettings->mDifferentialRatio = ratio;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleDifferentialSettings
+ * Method:    setLeftWheel
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_VehicleDifferentialSettings_setLeftWheel
+  (JNIEnv *, jclass, jlong settingsVa, jint wheelIndex) {
+    VehicleDifferentialSettings * const pSettings
+            = reinterpret_cast<VehicleDifferentialSettings *> (settingsVa);
+    pSettings->mLeftWheel = wheelIndex;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_VehicleDifferentialSettings
+ * Method:    setRightWheel
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_VehicleDifferentialSettings_setRightWheel
+  (JNIEnv *, jclass, jlong settingsVa, jint wheelIndex) {
+    VehicleDifferentialSettings * const pSettings
+            = reinterpret_cast<VehicleDifferentialSettings *> (settingsVa);
+    pSettings->mRightWheel = wheelIndex;
+}
