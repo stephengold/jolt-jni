2ecd3cd53d96834663e5a606f013bbcbb8b56026
stephengold <sgold@sonic.net>
2024-10-16 17:38:59 -0700

    add the WheelSettingsTv{/Ref} classes

---
+++ src/main/java/com/github/stephengold/joltjni/WheelSettingsTv.java
@@ -0,0 +1,101 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Settings used to construct a {@code WheelTv}. (native type: WheelSettingsTV)
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class WheelSettingsTv extends WheelSettings {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate default settings.
+     */
+    public WheelSettingsTv() {
+        super(true);
+        long settingsVa = createDefault();
+        setVirtualAddress(settingsVa, true);
+    }
+
+    /**
+     * Instantiate with the specified native object assigned but not owned.
+     *
+     * @param settingsVa the virtual address of the native object to assign (not
+     * zero)
+     */
+    WheelSettingsTv(long settingsVa) {
+        super(false);
+        setVirtualAddress(settingsVa, false); // not owner due to ref counting
+    }
+    // *************************************************************************
+    // WheelSettings methods
+
+    /**
+     * Count the active references to the native {@code WheelSettingsTV}. The
+     * settings are unaffected.
+     *
+     * @return the count (&ge;0)
+     */
+    @Override
+    public int getRefCount() {
+        long settingsVa = va();
+        int result = getRefCount(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Mark the native {@code WheelSettingsTV} as embedded.
+     */
+    @Override
+    public void setEmbedded() {
+        long settingsVa = va();
+        setEmbedded(settingsVa);
+    }
+
+    /**
+     * Create a counted reference to the native {@code WheelSettingsTV}.
+     *
+     * @return a new JVM object with a new native object assigned
+     */
+    @Override
+    public WheelSettingsTvRef toRef() {
+        long settingsVa = va();
+        long refVa = toRef(settingsVa);
+        WheelSettingsTvRef result = new WheelSettingsTvRef(refVa, true);
+
+        return result;
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createDefault();
+
+    native private static int getRefCount(long settingsVa);
+
+    native private static void setEmbedded(long settingsVa);
+
+    native private static long toRef(long settingsVa);
+}
---
+++ src/main/native/glue/w/WheelSettingsTv.cpp
@@ -0,0 +1,90 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "Jolt/Jolt.h"
+#include "Jolt/Physics/Vehicle/TrackedVehicleController.h"
+
+#include "auto/com_github_stephengold_joltjni_WheelSettingsTv.h"
+#include "auto/com_github_stephengold_joltjni_WheelSettingsTvRef.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+IMPLEMENT_REF(WheelSettingsTV,
+  Java_com_github_stephengold_joltjni_WheelSettingsTvRef_copy,
+  Java_com_github_stephengold_joltjni_WheelSettingsTvRef_createEmpty,
+  Java_com_github_stephengold_joltjni_WheelSettingsTvRef_free,
+  Java_com_github_stephengold_joltjni_WheelSettingsTvRef_getPtr)
+
+/*
+ * Class:     com_github_stephengold_joltjni_WheelSettingsTv
+ * Method:    createDefault
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_WheelSettingsTv_createDefault
+  (JNIEnv *, jclass) {
+    const WheelSettingsTV * const pSettings = new WheelSettingsTV();
+    TRACE_NEW("WheelSettingsTV", pSettings)
+    return reinterpret_cast<jlong> (pSettings);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_WheelSettingsTv
+ * Method:    getRefCount
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_WheelSettingsTv_getRefCount
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const WheelSettingsTV * const pSettings
+            = reinterpret_cast<WheelSettingsTV *> (settingsVa);
+    const uint32 result = pSettings->GetRefCount();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_WheelSettingsTv
+ * Method:    setEmbedded
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_WheelSettingsTv_setEmbedded
+  (JNIEnv *, jclass, jlong settingsVa) {
+    WheelSettingsTV * const pSettings
+            = reinterpret_cast<WheelSettingsTV *> (settingsVa);
+    pSettings->SetEmbedded();
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_WheelSettingsTv
+ * Method:    toRef
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_WheelSettingsTv_toRef
+  (JNIEnv *, jclass, jlong settingsVa) {
+    WheelSettingsTV * const pSettings
+            = reinterpret_cast<WheelSettingsTV *> (settingsVa);
+    Ref<WheelSettingsTV> * const pResult = new Ref<WheelSettingsTV>(pSettings);
+    TRACE_NEW("Ref<WheelSettingsTV>", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
