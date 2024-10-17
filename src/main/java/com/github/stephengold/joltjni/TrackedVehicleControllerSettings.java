6433cb8b9107bf2e58782590cf73013d311b7772
stephengold <sgold@sonic.net>
2024-10-16 19:54:37 -0700

    add the TrackedVehicleControllerSettings class

---
+++ src/main/java/com/github/stephengold/joltjni/TrackedVehicleControllerSettings.java
@@ -0,0 +1,89 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * Settings used to construct a {@code TrackedVehicleController}.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class TrackedVehicleControllerSettings
+        extends VehicleControllerSettings {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate default settings.
+     */
+    public TrackedVehicleControllerSettings() {
+        long settingsVa = createDefault();
+        setVirtualAddress(settingsVa, true);
+    }
+
+    /**
+     * Instantiate settings with the specified native object assigned but not
+     * owned.
+     *
+     * @param settingsVa the virtual address of the native object to assign (not
+     * zero)
+     */
+    TrackedVehicleControllerSettings(long settingsVa) {
+        super(settingsVa);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Count how many tracks the vehicle will have. The settings are unaffected.
+     * (native attribute: mTracks)
+     *
+     * @return the count (&ge;0)
+     */
+    public int getNumTracks() {
+        long vehicleSettingsVa = va();
+        int result = getNumTracks(vehicleSettingsVa);
+
+        return result;
+    }
+
+    /**
+     * Access the settings for the specified track. (native field: mTracks)
+     *
+     * @param index the index of the track to access (&ge;0)
+     * @return a new JVM object with the pre-existing native object assigned
+     */
+    public VehicleTrackSettings getTrack(int index) {
+        long vehicleSettingsVa = va();
+        long trackVa = getTrack(vehicleSettingsVa, index);
+        VehicleTrackSettings result = new VehicleTrackSettings(this, trackVa);
+
+        return result;
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createDefault();
+
+    native private static int getNumTracks(long vehicleSettingsVa);
+
+    native private static long getTrack(long vehicleSettingsVa, int index);
+}
---
+++ src/main/native/glue/t/TrackedVehicleControllerSettings.cpp
@@ -0,0 +1,69 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "Jolt/Jolt.h"
+#include "Jolt/Physics/Vehicle/TrackedVehicleController.h"
+#include "auto/com_github_stephengold_joltjni_TrackedVehicleControllerSettings.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_TrackedVehicleControllerSettings
+ * Method:    createDefault
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_TrackedVehicleControllerSettings_createDefault
+  (JNIEnv *, jclass) {
+    TrackedVehicleControllerSettings * const pSettings
+            = new TrackedVehicleControllerSettings();
+    TRACE_NEW("TrackedVehicleControllerSettings", pSettings)
+    return reinterpret_cast<jlong> (pSettings);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_TrackedVehicleControllerSettings
+ * Method:    getNumTracks
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_TrackedVehicleControllerSettings_getNumTracks
+  (JNIEnv *, jclass, jlong) {
+    const int result = (int) ETrackSide::Num;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_TrackedVehicleControllerSettings
+ * Method:    getTrack
+ * Signature: (JI)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_TrackedVehicleControllerSettings_getTrack
+  (JNIEnv *, jclass, jlong settingsVa, jint trackIndex) {
+    TrackedVehicleControllerSettings * const pVehicleSettings
+            = reinterpret_cast<TrackedVehicleControllerSettings *> (settingsVa);
+    VehicleTrackSettings * const pResult
+            = &pVehicleSettings->mTracks[trackIndex];
+    return reinterpret_cast<jlong> (pResult);
+}
