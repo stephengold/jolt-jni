7dcea8bedf8eb19862b8d6d519de700468472c3b
stephengold <sgold@sonic.net>
2024-10-17 21:10:31 -0700

    PulleyConstraintSettings:  add 16 public accessors

--- src/main/java/com/github/stephengold/joltjni/PulleyConstraintSettings.java
+++ src/main/java/com/github/stephengold/joltjni/PulleyConstraintSettings.java
@@ -21,7 +21,9 @@ SOFTWARE.
  */
 package com.github.stephengold.joltjni;
 
+import com.github.stephengold.joltjni.enumerate.EConstraintSpace;
 import com.github.stephengold.joltjni.enumerate.EConstraintSubType;
+import com.github.stephengold.joltjni.readonly.RVec3Arg;
 
 /**
  * Settings used to construct a {@code PulleyConstraint}.
@@ -52,7 +54,288 @@ public class PulleyConstraintSettings extends TwoBodyConstraintSettings {
         setSubType(EConstraintSubType.Pulley);
     }
     // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Copy the location at which the constraint will attach to body 1. The
+     * settings are unaffected. (native attribute: mBodyPoint1)
+     *
+     * @return a location vector
+     */
+    public RVec3 getBodyPoint1() {
+        long settingsVa = va();
+        double x = getBodyPoint1X(settingsVa);
+        double y = getBodyPoint1Y(settingsVa);
+        double z = getBodyPoint1Z(settingsVa);
+        RVec3 result = new RVec3(x, y, z);
+
+        return result;
+    }
+
+    /**
+     * Copy the location at which the constraint will attach to body 2. The
+     * settings are unaffected. (native attribute: mBodyPoint2)
+     *
+     * @return a location vector
+     */
+    public RVec3 getBodyPoint2() {
+        long settingsVa = va();
+        double x = getBodyPoint2X(settingsVa);
+        double y = getBodyPoint2Y(settingsVa);
+        double z = getBodyPoint2Z(settingsVa);
+        RVec3 result = new RVec3(x, y, z);
+
+        return result;
+    }
+
+    /**
+     * Copy the location to which body 1 is connected. The settings are
+     * unaffected. (native attribute: mFixedPoint1)
+     *
+     * @return a location vector (in system coordinates)
+     */
+    public RVec3 getFixedPoint1() {
+        long settingsVa = va();
+        double x = getFixedPoint1X(settingsVa);
+        double y = getFixedPoint1Y(settingsVa);
+        double z = getFixedPoint1Z(settingsVa);
+        RVec3 result = new RVec3(x, y, z);
+
+        return result;
+    }
+
+    /**
+     * Copy the location to which body 2 is connected. The settings are
+     * unaffected. (native attribute: mFixedPoint2)
+     *
+     * @return a location vector (in system coordinates)
+     */
+    public RVec3 getFixedPoint2() {
+        long settingsVa = va();
+        double x = getFixedPoint2X(settingsVa);
+        double y = getFixedPoint2Y(settingsVa);
+        double z = getFixedPoint2Z(settingsVa);
+        RVec3 result = new RVec3(x, y, z);
+
+        return result;
+    }
+
+    /**
+     * Return the maximum length of the line segments. The settings are
+     * unaffected. (native attribute: mMaxLength)
+     *
+     * @return the length (in meters, or -1 to use the creation length)
+     */
+    public float getMaxLength() {
+        long settingsVa = va();
+        float result = getMaxLength(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the minimum length of the line segments. The settings are
+     * unaffected. (native attribute: mMinLength)
+     *
+     * @return the length (in meters, or -1 to use the creation length)
+     */
+    public float getMinLength() {
+        long settingsVa = va();
+        float result = getMinLength(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the ratio between the 2 line segments. The settings are
+     * unaffected. (native attribute: mRatio)
+     *
+     * @return the ratio
+     */
+    public float getRatio() {
+        long settingsVa = va();
+        float result = getRatio(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return which space the body points are specified in. The settings are
+     * unaffected. (native attribute: mSpace)
+     *
+     * @return an enum value (not null)
+     */
+    public EConstraintSpace getSpace() {
+        long settingsVa = va();
+        int ordinal = getSpace(settingsVa);
+        EConstraintSpace result = EConstraintSpace.values()[ordinal];
+
+        return result;
+    }
+
+    /**
+     * Alter the location at which the constraint will attach to body 1. (native
+     * attribute: mBodyPoint1)
+     *
+     * @param location the desired location (not null, unaffected,
+     * default=(0,0,0))
+     */
+    public void setBodyPoint1(RVec3Arg location) {
+        long settingsVa = va();
+        double locX = location.xx();
+        double locY = location.yy();
+        double locZ = location.zz();
+        setBodyPoint1(settingsVa, locX, locY, locZ);
+    }
+
+    /**
+     * Alter the location at which the constraint will attach to body 2. (native
+     * attribute: mBodyPoint2)
+     *
+     * @param location the desired location (not null, unaffected,
+     * default=(0,0,0))
+     */
+    public void setBodyPoint2(RVec3Arg location) {
+        long settingsVa = va();
+        double locX = location.xx();
+        double locY = location.yy();
+        double locZ = location.zz();
+        setBodyPoint2(settingsVa, locX, locY, locZ);
+    }
+
+    /**
+     * Alter the location to which body 1 is connected. (native attribute:
+     * mFixedPoint1)
+     *
+     * @param location the desired location (in system coordinates, not null,
+     * unaffected, default=(0,0,0))
+     */
+    public void setFixedPoint1(RVec3Arg location) {
+        long settingsVa = va();
+        double locX = location.xx();
+        double locY = location.yy();
+        double locZ = location.zz();
+        setFixedPoint1(settingsVa, locX, locY, locZ);
+    }
+
+    /**
+     * Alter the location to which body 2 is connected. (native attribute:
+     * mFixedPoint2)
+     *
+     * @param location the desired location (in system coordinates, not null,
+     * unaffected, default=(0,0,0))
+     */
+    public void setFixedPoint2(RVec3Arg location) {
+        long settingsVa = va();
+        double locX = location.xx();
+        double locY = location.yy();
+        double locZ = location.zz();
+        setFixedPoint2(settingsVa, locX, locY, locZ);
+    }
+
+    /**
+     * Alter the maximum length of the line segments. (native attribute:
+     * mMaxLength)
+     *
+     * @param length (in meters, or -1 to use the creation length, default=-1)
+     * @return the argument, for chaining
+     */
+    public float setMaxLength(float length) {
+        long settingsVa = va();
+        setMaxLength(settingsVa, length);
+
+        return length;
+    }
+
+    /**
+     * Alter the minimum length of the line segments. (native attribute:
+     * mMinLength)
+     *
+     * @param length (in meters, or -1 to use the creation length, default=0)
+     * @return the argument, for chaining
+     */
+    public float setMinLength(float length) {
+        long settingsVa = va();
+        setMinLength(settingsVa, length);
+
+        return length;
+    }
+
+    /**
+     * Alter the ratio between the 2 line segments. (native attribute: mRatio)
+     *
+     * @param ratio the desired ratio (default=1)
+     */
+    public void setRatio(float ratio) {
+        long settingsVa = va();
+        setRatio(settingsVa, ratio);
+    }
+
+    /**
+     * Alter which space the body points are specified in. (native attribute:
+     * mSpace)
+     *
+     * @param space the desired constraint space (not null, default=WorldSpace)
+     */
+    public void setSpace(EConstraintSpace space) {
+        long settingsVa = va();
+        int ordinal = space.ordinal();
+        setSpace(settingsVa, ordinal);
+    }
+    // *************************************************************************
     // native private methods
 
     native private static long createDefault();
+
+    native private static double getBodyPoint1X(long settingsVa);
+
+    native private static double getBodyPoint1Y(long settingsVa);
+
+    native private static double getBodyPoint1Z(long settingsVa);
+
+    native private static double getBodyPoint2X(long settingsVa);
+
+    native private static double getBodyPoint2Y(long settingsVa);
+
+    native private static double getBodyPoint2Z(long settingsVa);
+
+    native private static double getFixedPoint1X(long settingsVa);
+
+    native private static double getFixedPoint1Y(long settingsVa);
+
+    native private static double getFixedPoint1Z(long settingsVa);
+
+    native private static double getFixedPoint2X(long settingsVa);
+
+    native private static double getFixedPoint2Y(long settingsVa);
+
+    native private static double getFixedPoint2Z(long settingsVa);
+
+    native private static float getMaxLength(long settingsVa);
+
+    native private static float getMinLength(long settingsVa);
+
+    native private static float getRatio(long settingsVa);
+
+    native private static int getSpace(long settingsVa);
+
+    native private static void setBodyPoint1(
+            long settingsVa, double locX, double locY, double locZ);
+
+    native private static void setBodyPoint2(
+            long settingsVa, double locX, double locY, double locZ);
+
+    native private static void setFixedPoint1(
+            long settingsVa, double locX, double locY, double locZ);
+
+    native private static void setFixedPoint2(
+            long settingsVa, double locX, double locY, double locZ);
+
+    native private static void setMaxLength(long settingsVa, float length);
+
+    native private static void setMinLength(long settingsVa, float length);
+
+    native private static void setRatio(long settingsVa, float ratio);
+
+    native private static void setSpace(long settingsVa, int ordinal);
 }
--- src/main/native/glue/p/PulleyConstraintSettings.cpp
+++ src/main/native/glue/p/PulleyConstraintSettings.cpp
@@ -41,3 +41,312 @@ JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSett
     TRACE_NEW("PulleyConstraintSettings", pResult)
     return reinterpret_cast<jlong> (pResult);
 }
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getBodyPoint1X
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getBodyPoint1X
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mBodyPoint1.GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getBodyPoint1Y
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getBodyPoint1Y
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mBodyPoint1.GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getBodyPoint1Z
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getBodyPoint1Z
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mBodyPoint1.GetZ();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getBodyPoint2X
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getBodyPoint2X
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mBodyPoint2.GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getBodyPoint2Y
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getBodyPoint2Y
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mBodyPoint2.GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getBodyPoint2Z
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getBodyPoint2Z
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mBodyPoint2.GetZ();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getFixedPoint1X
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getFixedPoint1X
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mFixedPoint1.GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getFixedPoint1Y
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getFixedPoint1Y
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mFixedPoint1.GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getFixedPoint1Z
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getFixedPoint1Z
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mFixedPoint1.GetZ();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getFixedPoint2X
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getFixedPoint2X
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mFixedPoint2.GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getFixedPoint2Y
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getFixedPoint2Y
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mFixedPoint2.GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getFixedPoint2Z
+ * Signature: (J)D
+ */
+JNIEXPORT jdouble JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getFixedPoint2Z
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const Real result = pSettings->mFixedPoint2.GetZ();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getMaxLength
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getMaxLength
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const float result = pSettings->mMaxLength;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getMinLength
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getMinLength
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const float result = pSettings->mMinLength;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getRatio
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getRatio
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const float result = pSettings->mRatio;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    getSpace
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_getSpace
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const EConstraintSpace result = pSettings->mSpace;
+    return (jint) result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    setBodyPoint1
+ * Signature: (JDDD)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_setBodyPoint1
+  (JNIEnv *, jclass, jlong settingsVa, jdouble locX, jdouble locY, jdouble locZ) {
+    PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const RVec3 location(locX, locY, locZ);
+    pSettings->mBodyPoint1 = location;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    setBodyPoint2
+ * Signature: (JDDD)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_setBodyPoint2
+  (JNIEnv *, jclass, jlong settingsVa, jdouble locX, jdouble locY, jdouble locZ) {
+    PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const RVec3 location(locX, locY, locZ);
+    pSettings->mBodyPoint2 = location;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    setFixedPoint1
+ * Signature: (JDDD)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_setFixedPoint1
+  (JNIEnv *, jclass, jlong settingsVa, jdouble locX, jdouble locY, jdouble locZ) {
+    PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const RVec3 location(locX, locY, locZ);
+    pSettings->mFixedPoint1 = location;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    setFixedPoint2
+ * Signature: (JDDD)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_setFixedPoint2
+  (JNIEnv *, jclass, jlong settingsVa, jdouble locX, jdouble locY, jdouble locZ) {
+    PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const RVec3 location(locX, locY, locZ);
+    pSettings->mFixedPoint2 = location;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    setMaxLength
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_setMaxLength
+  (JNIEnv *, jclass, jlong settingsVa, jfloat length) {
+    PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    pSettings->mMaxLength = length;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    setMinLength
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_setMinLength
+  (JNIEnv *, jclass, jlong settingsVa, jfloat length) {
+    PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    pSettings->mMinLength = length;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    setRatio
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_setRatio
+  (JNIEnv *, jclass, jlong settingsVa, jfloat ratio) {
+    PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    pSettings->mRatio = ratio;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PulleyConstraintSettings
+ * Method:    setSpace
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PulleyConstraintSettings_setSpace
+  (JNIEnv *, jclass, jlong settingsVa, jint ordinal) {
+    PulleyConstraintSettings * const pSettings
+            = reinterpret_cast<PulleyConstraintSettings *> (settingsVa);
+    const EConstraintSpace space = (EConstraintSpace) ordinal;
+    pSettings->mSpace = space;
+}
