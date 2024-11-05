1a7fa02dded82f95bf3e58b4ce5d789f75ff3b63
stephengold <sgold@sonic.net>
2024-11-04 19:12:08 -0800

    finalize the WheelSettingsTvRef class

--- src/main/java/com/github/stephengold/joltjni/WheelSettingsTvRef.java
+++ src/main/java/com/github/stephengold/joltjni/WheelSettingsTvRef.java
@@ -29,7 +29,7 @@ import com.github.stephengold.joltjni.template.Ref;
  *
  * @author Stephen Gold sgold@sonic.net
  */
-public class WheelSettingsTvRef extends Ref {
+final public class WheelSettingsTvRef extends Ref {
     // *************************************************************************
     // constructors
 
