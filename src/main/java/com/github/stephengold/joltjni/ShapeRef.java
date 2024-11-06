2f1a52877527637f46f64ba31d6051d881522051
stephengold <sgold@sonic.net>
2024-11-06 13:26:21 -0800

    ShapeRef:  add a public no-arg constructor

--- src/main/java/com/github/stephengold/joltjni/ShapeRef.java
+++ src/main/java/com/github/stephengold/joltjni/ShapeRef.java
@@ -33,6 +33,14 @@ final public class ShapeRef extends Ref {
     // constructors
 
     /**
+     * Instantiate an empty reference.
+     */
+    public ShapeRef() {
+        long refVa = createEmpty();
+        setVirtualAddress(refVa, () -> free(refVa));
+    }
+
+    /**
      * Instantiate a reference with the specified native object assigned.
      *
      * @param refVa the virtual address of the native object to assign (not
@@ -78,6 +86,8 @@ final public class ShapeRef extends Ref {
     // native private methods
 
     native private static long copy(long refVa);
+
+    native private static long createEmpty();
 
     native private static void free(long refVa);
 
