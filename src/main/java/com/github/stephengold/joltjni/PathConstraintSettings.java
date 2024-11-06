b7a5e5760ea91c37474e78daf9a5aaa073a10c98
stephengold <sgold@sonic.net>
2024-11-06 12:24:11 -0800

    PathConstraintSettings:  add 14 public methods

--- src/main/java/com/github/stephengold/joltjni/PathConstraintSettings.java
+++ src/main/java/com/github/stephengold/joltjni/PathConstraintSettings.java
@@ -22,6 +22,9 @@ SOFTWARE.
 package com.github.stephengold.joltjni;
 
 import com.github.stephengold.joltjni.enumerate.EConstraintSubType;
+import com.github.stephengold.joltjni.enumerate.EPathRotationConstraintType;
+import com.github.stephengold.joltjni.readonly.QuatArg;
+import com.github.stephengold.joltjni.readonly.Vec3Arg;
 
 /**
  * Settings used to construct a {@code PathConstraint}.
@@ -52,7 +55,235 @@ public class PathConstraintSettings extends TwoBodyConstraintSettings {
         setSubType(EConstraintSubType.Path);
     }
     // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the maximum friction force when not driven by a motor. The
+     * settings are unaffected. (native field: mMaxFrictionForce)
+     *
+     * @return the force (in Newtons)
+     */
+    public float getMaxFrictionForce() {
+        long settingsVa = va();
+        float result = getMaxFrictionForce(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Access the path. (native field: mPath)
+     *
+     * @return a new JVM object with the pre-existing native object assigned
+     */
+    public PathConstraintPath getPath() {
+        long settingsVa = va();
+        long pathVa = getPath(settingsVa);
+        PathConstraintPath result = new PathConstraintPath(pathVa);
+
+        return result;
+    }
+
+    /**
+     * Return how far the body is along the path initially. The settings are
+     * unaffected. (native field: mPathFraction)
+     *
+     * @return the amount
+     */
+    public float getPathFraction() {
+        long settingsVa = va();
+        float result = getPathFraction(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Copy the starting location. The settings are unaffected. (native field:
+     * mPathPosition)
+     *
+     * @return a new vector
+     */
+    public Vec3 getPathPosition() {
+        long settingsVa = va();
+        float x = getPathPositionX(settingsVa);
+        float y = getPathPositionY(settingsVa);
+        float z = getPathPositionZ(settingsVa);
+        Vec3 result = new Vec3(x, y, z);
+
+        return result;
+    }
+
+    /**
+     * Copy the initial rotation. The path is unaffected. (native field:
+     * mPathRotation)
+     *
+     * @return a new quaternion
+     */
+    public Quat getPathRotation() {
+        long settingsVa = va();
+        float qw = getPathRotationW(settingsVa);
+        float qx = getPathRotationX(settingsVa);
+        float qy = getPathRotationY(settingsVa);
+        float qz = getPathRotationZ(settingsVa);
+        Quat result = new Quat(qx, qy, qz, qw);
+
+        return result;
+    }
+
+    /**
+     * Access the settings of the position motor. (native field:
+     * mPositionMotorSettings)
+     *
+     * @return new JVM object with the pre-existing native object assigned
+     */
+    public MotorSettings getPositionMotorSettings() {
+        long constraintSettingsVa = va();
+        long motorSettingsVa = getPositionMotorSettings(constraintSettingsVa);
+        MotorSettings result = new MotorSettings(this, motorSettingsVa);
+
+        return result;
+    }
+
+    /**
+     * The settings are unaffected. (native field: mRotationConstraintType)
+     *
+     * @return the enum value (not null)
+     */
+    public EPathRotationConstraintType getRotationConstraintType() {
+        long settingsVa = va();
+        int ordinal = getRotationConstraintType(settingsVa);
+        EPathRotationConstraintType result
+                = EPathRotationConstraintType.values()[ordinal];
+
+        return result;
+    }
+
+    /**
+     * Alter the maximum friction force when not driven by a motor. (native
+     * field: mMaxFrictionForce)
+     *
+     * @param force the desired force (in Newtons)
+     */
+    public void setMaxFrictionForce(float force) {
+        long settingsVa = va();
+        setMaxFrictionForce(settingsVa, force);
+    }
+
+    /**
+     * Replace the current path with the specified one. (native field: mPath)
+     *
+     * @param path the desired path (not null)
+     */
+    public void setPath(PathConstraintPath path) {
+        long settingsVa = va();
+        long pathVa = path.va();
+        setPath(settingsVa, pathVa);
+    }
+
+    /**
+     * Alter how far the body is along the path initially. (native field:
+     * mPathFraction)
+     *
+     * @param amount the desired amount (&ge;0)
+     */
+    public void setPathFraction(float amount) {
+        long settingsVa = va();
+        setPathFraction(settingsVa, amount);
+    }
+
+    /**
+     * Alter the initial location. (native field: mPathPosition)
+     *
+     * @param location the desired location (not null, unaffected)
+     */
+    public void setPathPosition(Vec3Arg location) {
+        long settingsVa = va();
+        float locX = location.getX();
+        float locY = location.getY();
+        float locZ = location.getZ();
+        setPathPosition(settingsVa, locX, locY, locZ);
+    }
+
+    /**
+     * Alter the initial orientation. (native field: mPathRotation)
+     *
+     * @param orientation the desired orientation (not null, unaffected)
+     */
+    public void setPathRotation(QuatArg orientation) {
+        long settingsVa = va();
+        float qw = orientation.getW();
+        float qx = orientation.getX();
+        float qy = orientation.getY();
+        float qz = orientation.getZ();
+        setPathRotation(settingsVa, qx, qy, qz, qw);
+    }
+
+    /**
+     * Alter the motor settings. (native field: mPositionMotorSettings)
+     *
+     * @param motorSettings the desired settings (not null, unaffected)
+     */
+    public void setPositionMotorSettings(MotorSettings motorSettings) {
+        long constraintSettingsVa = va();
+        long motorSettingsVa = motorSettings.va();
+        setPositionMotorSettings(constraintSettingsVa, motorSettingsVa);
+    }
+
+    /**
+     * Alter the (native field: mRotationConstraintType)
+     *
+     * @param type the desired type (not null)
+     */
+    public void setRotationConstraintType(EPathRotationConstraintType type) {
+        long settingsVa = va();
+        int ordinal = type.ordinal();
+        setRotationConstraintType(settingsVa, ordinal);
+    }
+    // *************************************************************************
     // native private methods
 
     native private static long createDefault();
+
+    native private static float getMaxFrictionForce(long settingsVa);
+
+    native private static long getPath(long settingsVa);
+
+    native private static float getPathFraction(long settingsVa);
+
+    native private static float getPathPositionX(long settingsVa);
+
+    native private static float getPathPositionY(long settingsVa);
+
+    native private static float getPathPositionZ(long settingsVa);
+
+    native private static float getPathRotationW(long settingsVa);
+
+    native private static float getPathRotationX(long settingsVa);
+
+    native private static float getPathRotationY(long settingsVa);
+
+    native private static float getPathRotationZ(long settingsVa);
+
+    native private static long getPositionMotorSettings(
+            long constraintSettingsVa);
+
+    native private static int getRotationConstraintType(long settingsVa);
+
+    native private static void setMaxFrictionForce(
+            long settingsVa, float force);
+
+    native private static void setPath(long settingsVa, long pathVa);
+
+    native private static void setPathFraction(long settingsVa, float amount);
+
+    native private static void setPathPosition(
+            long settingsVa, float locX, float locY, float locZ);
+
+    native private static void setPathRotation(
+            long settingsVa, float qx, float qy, float qz, float qw);
+
+    native private static void setPositionMotorSettings(
+            long constraintSettingsVa, long motorSettingsVa);
+
+    native private static void setRotationConstraintType(
+            long settingsVa, int ordinal);
 }
--- src/main/native/glue/p/PathConstraintSettings.cpp
+++ src/main/native/glue/p/PathConstraintSettings.cpp
@@ -41,3 +41,253 @@ JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettin
     TRACE_NEW("PathConstraintSettings", pResult)
     return reinterpret_cast<jlong> (pResult);
 }
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getMaxFrictionForce
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getMaxFrictionForce
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const float result = pSettings->mMaxFrictionForce;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPath
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPath
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    RefConst<PathConstraintPath> const ref = pSettings->mPath;
+    const PathConstraintPath * const pResult = ref.GetPtr();
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPathFraction
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPathFraction
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const float result = pSettings->mPathFraction;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPathPositionX
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPathPositionX
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const float result = pSettings->mPathPosition.GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPathPositionY
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPathPositionY
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const float result = pSettings->mPathPosition.GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPathPositionZ
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPathPositionZ
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const float result = pSettings->mPathPosition.GetZ();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPathRotationW
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPathRotationW
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const float result = pSettings->mPathRotation.GetW();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPathRotationX
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPathRotationX
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const float result = pSettings->mPathRotation.GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPathRotationY
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPathRotationY
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const float result = pSettings->mPathRotation.GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPathRotationZ
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPathRotationZ
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const float result = pSettings->mPathRotation.GetZ();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getPositionMotorSettings
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getPositionMotorSettings
+  (JNIEnv *, jclass, jlong constraintSettingsVa) {
+    PathConstraintSettings * const pConstraintSettings
+            = reinterpret_cast<PathConstraintSettings *> (constraintSettingsVa);
+    MotorSettings * const pResult
+            = &pConstraintSettings->mPositionMotorSettings;
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    getRotationConstraintType
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_getRotationConstraintType
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const EPathRotationConstraintType result
+            = pSettings->mRotationConstraintType;
+    return (jint) result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    setMaxFrictionForce
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_setMaxFrictionForce
+  (JNIEnv *, jclass, jlong settingsVa, jfloat force) {
+    PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    pSettings->mMaxFrictionForce = force;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    setPath
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_setPath
+  (JNIEnv *, jclass, jlong settingsVa, jlong pathVa) {
+    PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const PathConstraintPath * pPath = pSettings->mPath;
+    pSettings->mPath = pPath;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    setPathFraction
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_setPathFraction
+  (JNIEnv *, jclass, jlong settingsVa, jfloat amount) {
+    PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    pSettings->mPathFraction = amount;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    setPathPosition
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_setPathPosition
+  (JNIEnv *, jclass, jlong settingsVa, jfloat x, jfloat y, jfloat z) {
+    PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const Vec3 position(x, y, z);
+    pSettings->mPathPosition = position;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    setPathRotation
+ * Signature: (JFFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_setPathRotation
+  (JNIEnv *, jclass, jlong settingsVa, jfloat qx, jfloat qy, jfloat qz, jfloat qw) {
+    PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const Quat orientation(qx, qy, qz, qw);
+    pSettings->mPathRotation = orientation;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    setPositionMotorSettings
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_setPositionMotorSettings
+  (JNIEnv *, jclass, jlong constraintSettingsVa, jlong motorSettingsVa) {
+    PathConstraintSettings * const pConstraintSettings
+            = reinterpret_cast<PathConstraintSettings *> (constraintSettingsVa);
+    const MotorSettings * const pMotorSettings
+            = reinterpret_cast<MotorSettings *> (motorSettingsVa);
+    pConstraintSettings->mPositionMotorSettings = *pMotorSettings;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_PathConstraintSettings
+ * Method:    setRotationConstraintType
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_PathConstraintSettings_setRotationConstraintType
+  (JNIEnv *, jclass, jlong settingsVa, jint ordinal) {
+    PathConstraintSettings * const pSettings
+            = reinterpret_cast<PathConstraintSettings *> (settingsVa);
+    const EPathRotationConstraintType constraintType
+            = (EPathRotationConstraintType)ordinal;
+    pSettings->mRotationConstraintType = constraintType;
+}
