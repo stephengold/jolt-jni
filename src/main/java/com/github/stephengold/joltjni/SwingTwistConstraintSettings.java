d84f7340e0cca07bd41bc6b614a0351290cb77e3
stephengold <sgold@sonic.net>
2024-11-08 23:21:44 -0800

    SwingTwistConstraintSettings:  add 17 public methods

--- src/main/java/com/github/stephengold/joltjni/SwingTwistConstraintSettings.java
+++ src/main/java/com/github/stephengold/joltjni/SwingTwistConstraintSettings.java
@@ -21,7 +21,11 @@ SOFTWARE.
  */
 package com.github.stephengold.joltjni;
 
+import com.github.stephengold.joltjni.enumerate.EConstraintSpace;
 import com.github.stephengold.joltjni.enumerate.EConstraintSubType;
+import com.github.stephengold.joltjni.enumerate.ESwingType;
+import com.github.stephengold.joltjni.readonly.RVec3Arg;
+import com.github.stephengold.joltjni.readonly.Vec3Arg;
 
 /**
  * Settings used to construct a {@code SwingTwistConstraint}.
@@ -52,7 +56,282 @@ public class SwingTwistConstraintSettings extends TwoBodyConstraintSettings {
         setSubType(EConstraintSubType.SwingTwist);
     }
     // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Access the swing motor settings. (native attribute: mSwingMotorSettings)
+     *
+     * @return a new JVM object with the pre-existing native object assigned
+     */
+    public MotorSettings getSwingMotorSettings() {
+        long constraintSettingsVa = va();
+        long motorSettingsVa = getSwingMotorSettings(constraintSettingsVa);
+        MotorSettings result = new MotorSettings(this, motorSettingsVa);
+
+        return result;
+    }
+
+    /**
+     * Access the twist motor settings. (native attribute: mTwistMotorSettings)
+     *
+     * @return a new JVM object with the pre-existing native object assigned
+     */
+    public MotorSettings getTwistMotorSettings() {
+        long constraintSettingsVa = va();
+        long motorSettingsVa = getTwistMotorSettings(constraintSettingsVa);
+        MotorSettings result = new MotorSettings(this, motorSettingsVa);
+
+        return result;
+    }
+
+    /**
+     * Alter the maximum friction torque when not driven by a motor. (native
+     * attribute: mMaxFrictionTorque)
+     *
+     * @param torque the desired limit (in Newton-meters, default=0)
+     */
+    public void setMaxFrictionTorque(float torque) {
+        long settingsVa = va();
+        setMaxFrictionTorque(settingsVa, torque);
+    }
+
+    /**
+     * Alter the normal half-cone angle. (native attribute:
+     * mNormalHalfConeAngle)
+     *
+     * @param angle the desired angle (in radians, default=0)
+     */
+    public void setNormalHalfConeAngle(float angle) {
+        long settingsVa = va();
+        setNormalHalfConeAngle(settingsVa, angle);
+    }
+
+    /**
+     * Alter the plane axis of body 1. (native attribute: mPlaneAxis1)
+     *
+     * @param axisDirection the desired direction (not null, unaffected,
+     * default=(0,1,0))
+     * @return the argument, for chaining
+     */
+    public Vec3Arg setPlaneAxis1(Vec3Arg axisDirection) {
+        long settingsVa = va();
+        float dx = axisDirection.getX();
+        float dy = axisDirection.getY();
+        float dz = axisDirection.getZ();
+        setPlaneAxis1(settingsVa, dx, dy, dz);
+
+        return axisDirection;
+    }
+
+    /**
+     * Alter the plane axis of body 2. (native attribute: mPlaneAxis2)
+     *
+     * @param axisDirection the desired direction (not null, unaffected,
+     * default=(0,1,0))
+     * @return the argument, for chaining
+     */
+    public Vec3Arg setPlaneAxis2(Vec3Arg axisDirection) {
+        long settingsVa = va();
+        float dx = axisDirection.getX();
+        float dy = axisDirection.getY();
+        float dz = axisDirection.getZ();
+        setPlaneAxis2(settingsVa, dx, dy, dz);
+
+        return axisDirection;
+    }
+
+    /**
+     * Alter the half-angle of the plane cone. (native attribute:
+     * mPlaneHalfConeAngle)
+     *
+     * @param angle the desired limit (in radians, default=0)
+     */
+    public void setPlaneHalfConeAngle(float angle) {
+        long settingsVa = va();
+        setPlaneHalfConeAngle(settingsVa, angle);
+    }
+
+    /**
+     * Alter the initial location of the body 1. (native attribute: mPosition1)
+     *
+     * @param location the desired location (not null, unaffected,
+     * default=(0,0,0))
+     * @return the argument, for chaining
+     */
+    public RVec3Arg setPosition1(RVec3Arg location) {
+        long settingsVa = va();
+        double locX = location.xx();
+        double locY = location.yy();
+        double locZ = location.zz();
+        setPosition1(settingsVa, locX, locY, locZ);
+
+        return location;
+    }
+
+    /**
+     * Alter the initial location of body 2. (native attribute: mPosition2)
+     *
+     * @param location the desired location (not null, unaffected,
+     * default=(0,0,0))
+     * @return the argument, for chaining
+     */
+    public RVec3Arg setPosition2(RVec3Arg location) {
+        long settingsVa = va();
+        double locX = location.xx();
+        double locY = location.yy();
+        double locZ = location.zz();
+        setPosition2(settingsVa, locX, locY, locZ);
+
+        return location;
+    }
+
+    /**
+     * Alter which space the other properties are specified in. (native
+     * attribute: mSpace)
+     *
+     * @param space enum value (not null, default=WorldSpace)
+     */
+    public void setSpace(EConstraintSpace space) {
+        long settingsVa = va();
+        int ordinal = space.ordinal();
+        setSpace(settingsVa, ordinal);
+    }
+
+    /**
+     * Alter the settings for the swing motor. (native attribute:
+     * mSwingMotorSettings)
+     *
+     * @param motorSettings the desired settings (not null, unaffected)
+     */
+    public void setSwingMotorSettings(MotorSettings motorSettings) {
+        long constraintSettingsVa = va();
+        long motorSettingsVa = motorSettings.va();
+        setSwingMotorSettings(constraintSettingsVa, motorSettingsVa);
+    }
+
+    /**
+     * Alter the type of swing constraint. (native attribute: mSwingType)
+     *
+     * @param swingType the desired enum value (not null, default=Cone)
+     */
+    public void setSwingType(ESwingType swingType) {
+        long settingsVa = va();
+        int ordinal = swingType.ordinal();
+        setSwingType(settingsVa, ordinal);
+    }
+
+    /**
+     * Alter the twist axis of the body 1. (native attribute: mTwistAxis1)
+     *
+     * @param axisDirection the desired direction (not null, unaffected,
+     * default=(1,0,0))
+     * @return the argument, for chaining
+     */
+    public Vec3Arg setTwistAxis1(Vec3Arg axisDirection) {
+        long settingsVa = va();
+        float dx = axisDirection.getX();
+        float dy = axisDirection.getY();
+        float dz = axisDirection.getZ();
+        setTwistAxis1(settingsVa, dx, dy, dz);
+
+        return axisDirection;
+    }
+
+    /**
+     * Alter the twist axis of body 2. (native attribute: mTwistAxis2)
+     *
+     * @param axisDirection the desired direction (not null, unaffected,
+     * default=(1,0,0))
+     * @return the argument, for chaining
+     */
+    public Vec3Arg setTwistAxis2(Vec3Arg axisDirection) {
+        long settingsVa = va();
+        float dx = axisDirection.getX();
+        float dy = axisDirection.getY();
+        float dz = axisDirection.getZ();
+        setTwistAxis2(settingsVa, dx, dy, dz);
+
+        return axisDirection;
+    }
+
+    /**
+     * Alter the maximum twist angle. (native attribute: mTwistMaxAngle)
+     *
+     * @param angle the desired limit (in radians, &ge;-Pi, &le;Pi, default=0)
+     */
+    public void setTwistMaxAngle(float angle) {
+        long settingsVa = va();
+        setTwistMaxAngle(settingsVa, angle);
+    }
+
+    /**
+     * Alter the minimum twist angle. (native attribute: mTwistMinAngle)
+     *
+     * @param angle the desired limit (in radians, &ge;-Pi, &le;Pi, default=0)
+     */
+    public void setTwistMinAngle(float angle) {
+        long settingsVa = va();
+        setTwistMinAngle(settingsVa, angle);
+    }
+
+    /**
+     * Alter the settings for the twist motor. (native attribute:
+     * mTwistMotorSetting)
+     *
+     * @param motorSettings the desired settings (not null, unaffected)
+     */
+    public void setTwistMotorSettings(MotorSettings motorSettings) {
+        long constraintSettingsVa = va();
+        long motorSettingsVa = motorSettings.va();
+        setTwistMotorSettings(constraintSettingsVa, motorSettingsVa);
+    }
+    // *************************************************************************
     // native private methods
 
     native private static long createDefault();
+
+    native private static long getSwingMotorSettings(long constraintSettingsVa);
+
+    native private static long getTwistMotorSettings(long constraintSettingsVa);
+
+    native private static void setMaxFrictionTorque(
+            long settingsVa, float torque);
+
+    native private static void setNormalHalfConeAngle(
+            long settingsVa, float angle);
+
+    native private static void setPlaneAxis1(
+            long settingsVa, float dx, float dy, float dz);
+
+    native private static void setPlaneAxis2(
+            long settingsVa, float dx, float dy, float dz);
+
+    native private static void setPlaneHalfConeAngle(
+            long settingsVa, float angle);
+
+    native private static void setPosition1(
+            long settingsVa, double locX, double locY, double locZ);
+
+    native private static void setPosition2(
+            long settingsVa, double locX, double locY, double locZ);
+
+    native private static void setSpace(long settingsVa, int ordinal);
+
+    native private static void setSwingMotorSettings(
+            long constraintSettingsVa, long motorSettingsVa);
+
+    native private static void setSwingType(long settingsVa, int ordinal);
+
+    native private static void setTwistAxis1(
+            long settingsVa, float dx, float dy, float dz);
+
+    native private static void setTwistAxis2(
+            long settingsVa, float dx, float dy, float dz);
+
+    native private static void setTwistMaxAngle(long settingsVa, float angle);
+
+    native private static void setTwistMinAngle(long settingsVa, float angle);
+
+    native private static void setTwistMotorSettings(
+            long constraintSettingsVa, long motorSettingsVa);
 }
--- src/main/native/glue/s/SwingTwistConstraintSettings.cpp
+++ src/main/native/glue/s/SwingTwistConstraintSettings.cpp
@@ -42,3 +42,221 @@ JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraint
     TRACE_NEW("SwingTwistConstraintSettings", pResult)
     return reinterpret_cast<jlong> (pResult);
 }
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    getSwingMotorSettings
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_getSwingMotorSettings
+  (JNIEnv *, jclass, jlong constraintSettingsVa) {
+    SwingTwistConstraintSettings * const pConstraintSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (constraintSettingsVa);
+    MotorSettings * const pResult = &pConstraintSettings->mSwingMotorSettings;
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    getTwistMotorSettings
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_getTwistMotorSettings
+  (JNIEnv *, jclass, jlong constraintSettingsVa) {
+    SwingTwistConstraintSettings * const pConstraintSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (constraintSettingsVa);
+    MotorSettings * const pResult = &pConstraintSettings->mTwistMotorSettings;
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setMaxFrictionTorque
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setMaxFrictionTorque
+  (JNIEnv *, jclass, jlong settingsVa, jfloat torque) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    pSettings->mMaxFrictionTorque = torque;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setNormalHalfConeAngle
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setNormalHalfConeAngle
+  (JNIEnv *, jclass, jlong settingsVa, jfloat angle) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    pSettings->mNormalHalfConeAngle = angle;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setPlaneAxis1
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setPlaneAxis1
+  (JNIEnv *, jclass, jlong settingsVa, jfloat dx, jfloat dy, jfloat dz) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    const Vec3 axisDirection(dx, dy, dz);
+    pSettings->mPlaneAxis1 = axisDirection;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setPlaneAxis2
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setPlaneAxis2
+  (JNIEnv *, jclass, jlong settingsVa, jfloat dx, jfloat dy, jfloat dz) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    const Vec3 axisDirection(dx, dy, dz);
+    pSettings->mPlaneAxis2 = axisDirection;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setPlaneHalfConeAngle
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setPlaneHalfConeAngle
+  (JNIEnv *, jclass, jlong settingsVa, jfloat angle) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    pSettings->mPlaneHalfConeAngle = angle;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setPosition1
+ * Signature: (JDDD)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setPosition1
+  (JNIEnv *, jclass, jlong settingsVa, jdouble locX, jdouble locY, jdouble locZ) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    const RVec3 location(locX, locY, locZ);
+    pSettings->mPosition1 = location;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setPosition2
+ * Signature: (JDDD)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setPosition2
+  (JNIEnv *, jclass, jlong settingsVa, jdouble locX, jdouble locY, jdouble locZ) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    const RVec3 location(locX, locY, locZ);
+    pSettings->mPosition2 = location;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setSpace
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setSpace
+  (JNIEnv *, jclass, jlong settingsVa, jint ordinal) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    const EConstraintSpace space = (EConstraintSpace) ordinal;
+    pSettings->mSpace = space;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setSwingMotorSettings
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setSwingMotorSettings
+  (JNIEnv *, jclass, jlong constraintSettingsVa, jlong motorSettingsVa) {
+    SwingTwistConstraintSettings * const pConstraintSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (constraintSettingsVa);
+    const MotorSettings * const pMotorSettings
+            = reinterpret_cast<MotorSettings *> (motorSettingsVa);
+    pConstraintSettings->mSwingMotorSettings = *pMotorSettings;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setSwingType
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setSwingType
+  (JNIEnv *, jclass, jlong settingsVa, jint ordinal) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    const ESwingType swingType = (ESwingType) ordinal;
+    pSettings->mSwingType = swingType;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setTwistAxis1
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setTwistAxis1
+  (JNIEnv *, jclass, jlong settingsVa, jfloat dx, jfloat dy, jfloat dz) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    const Vec3 axisDirection(dx, dy, dz);
+    pSettings->mTwistAxis1 = axisDirection;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setTwistAxis2
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setTwistAxis2
+  (JNIEnv *, jclass, jlong settingsVa, jfloat dx, jfloat dy, jfloat dz) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    const Vec3 axisDirection(dx, dy, dz);
+    pSettings->mTwistAxis2 = axisDirection;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setTwistMaxAngle
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setTwistMaxAngle
+  (JNIEnv *, jclass, jlong settingsVa, jfloat angle) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    pSettings->mTwistMaxAngle = angle;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setTwistMinAngle
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setTwistMinAngle
+  (JNIEnv *, jclass, jlong settingsVa, jfloat angle) {
+    SwingTwistConstraintSettings * const pSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (settingsVa);
+    pSettings->mTwistMinAngle = angle;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_SwingTwistConstraintSettings
+ * Method:    setTwistMotorSettings
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_SwingTwistConstraintSettings_setTwistMotorSettings
+  (JNIEnv *, jclass, jlong constraintSettingsVa, jlong motorSettingsVa) {
+    SwingTwistConstraintSettings * const pConstraintSettings
+            = reinterpret_cast<SwingTwistConstraintSettings *> (constraintSettingsVa);
+    const MotorSettings * const pMotorSettings
+            = reinterpret_cast<MotorSettings *> (motorSettingsVa);
+    pConstraintSettings->mTwistMotorSettings = *pMotorSettings;
+}
