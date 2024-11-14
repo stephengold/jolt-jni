17838d9e6b0794d076fbf4ba6071165e2314586b
stephengold <sgold@sonic.net>
2024-11-14 15:19:37 -0800

    StreamIn:  add a setVirtualAddress() method

--- src/main/java/com/github/stephengold/joltjni/StreamIn.java
+++ src/main/java/com/github/stephengold/joltjni/StreamIn.java
@@ -38,4 +38,23 @@ abstract public class StreamIn extends NonCopyable {
      */
     StreamIn() {
     }
+    // *************************************************************************
+    // protected methods
+
+    /**
+     * Assign a native object, assuming there's none already assigned.
+     *
+     * @param streamVa the virtual address of the native object to assign (not
+     * zero)
+     * @param owner {@code true} &rarr; make the JVM object the owner,
+     * {@code false} &rarr; it isn't the owner
+     */
+    final void setVirtualAddress(long streamVa, boolean owner) {
+        Runnable freeingAction = owner ? () -> free(streamVa) : null;
+        setVirtualAddress(streamVa, freeingAction);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static void free(long streamVa);
 }
