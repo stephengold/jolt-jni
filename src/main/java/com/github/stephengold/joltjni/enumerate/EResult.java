51291a455dc8cc167efcb0a28321641df776b18f
stephengold <sgold@sonic.net>
2024-11-18 22:02:52 -0800

    add the EResult enum

---
+++ src/main/java/com/github/stephengold/joltjni/enumerate/EResult.java
@@ -0,0 +1,54 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni.enumerate;
+
+/**
+ * Enumerate possible outcomes from attempting to construct a convex hull.
+ * (native type: {@code ConvexHullBuilder::EResult})
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public enum EResult {
+    // *************************************************************************
+    // values - sequence must match "Jolt/Geometry/ConvexHullBuilder.h"
+
+    /**
+     * successful completion with the requested accuracy
+     */
+    Success,
+    /**
+     * terminated by max vertices before the requested accuracy was reached
+     */
+    MaxVerticesReached,
+    /**
+     * too few points to construct a hull
+     */
+    TooFewPoints,
+    /**
+     * too few faces in the constructed hull (due to precision errors)
+     */
+    TooFewFaces,
+    /**
+     * detected a degenerate hull
+     */
+    Degenerate
+}
