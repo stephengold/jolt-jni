150c4f49887b60a57f9343b231f84e5929d4c794
stephengold <sgold@sonic.net>
2024-11-21 10:49:31 -0800

    add the Sphere class

---
+++ src/main/java/com/github/stephengold/joltjni/Sphere.java
@@ -0,0 +1,141 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+import com.github.stephengold.joltjni.readonly.Vec3Arg;
+
+/**
+ * 3-D geometric description of a sphere.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class Sphere extends JoltPhysicsObject {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a sphere with the specified center and radius.
+     *
+     * @param center the desired location for the center (not null, unaffected)
+     * @param radius the desired radius
+     */
+    public Sphere(Float3 center, float radius) {
+        long sphereVa = create(center.x, center.y, center.z, radius);
+        setVirtualAddress(sphereVa, () -> free(sphereVa));
+    }
+
+    /**
+     * Instantiate a sphere with the specified center and radius.
+     *
+     * @param center the desired location for the center (not null, unaffected)
+     * @param radius the desired radius
+     */
+    public Sphere(Vec3Arg center, float radius) {
+        float x = center.getX();
+        float y = center.getY();
+        float z = center.getZ();
+        long sphereVa = create(x, y, z, radius);
+        setVirtualAddress(sphereVa, () -> free(sphereVa));
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Construct the smallest sphere that encapsulates the sphere and the
+     * specified point.
+     *
+     * @param point the point to encapsulate (not null, unaffected)
+     */
+    public void encapsulatePoint(Vec3Arg point) {
+        long sphereVa = va();
+        float x = point.getX();
+        float y = point.getY();
+        float z = point.getZ();
+        encapsulatePoint(sphereVa, x, y, z);
+    }
+
+    /**
+     * Copy the location of the sphere's center.
+     *
+     * @return a new location vector
+     */
+    public Vec3 getCenter() {
+        long sphereVa = va();
+        float x = getCenterX(sphereVa);
+        float y = getCenterY(sphereVa);
+        float z = getCenterZ(sphereVa);
+        Vec3 result = new Vec3(x, y, z);
+
+        return result;
+    }
+
+    /**
+     * Return the radius of the sphere.
+     *
+     * @return the radius
+     */
+    public float getRadius() {
+        long sphereVa = va();
+        float radius = getRadius(sphereVa);
+
+        return radius;
+    }
+
+    /**
+     * Calculate the support vector for the specified direction.
+     *
+     * @param direction the direction to use (not null, unaffected)
+     * @return a new offset vector
+     */
+    public Vec3 getSupport(Vec3Arg direction) {
+        long sphereVa = va();
+        float dx = direction.getX();
+        float dy = direction.getY();
+        float dz = direction.getZ();
+        float[] storeFloats = new float[3];
+        getSupport(sphereVa, dx, dy, dz, storeFloats);
+        Vec3 result = new Vec3(storeFloats);
+
+        return result;
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long create(
+            float cx, float cy, float cz, float radius);
+
+    native private static void encapsulatePoint(
+            long sphereVa, float x, float y, float z);
+
+    native private static void free(long sphereVa);
+
+    native private static float getCenterX(long sphereVa);
+
+    native private static float getCenterY(long sphereVa);
+
+    native private static float getCenterZ(long sphereVa);
+
+    native private static float getRadius(long sphereVa);
+
+    native private static void getSupport(
+            long sphereVa, float dx, float dy, float dz, float[] storeFloats);
+}
---
+++ src/main/native/glue/s/Sphere.cpp
@@ -0,0 +1,136 @@
+/*
+Copyright (c) 2024 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "Jolt/Jolt.h"
+#include "Jolt/Geometry/Sphere.h"
+#include "auto/com_github_stephengold_joltjni_Sphere.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_Sphere
+ * Method:    create
+ * Signature: (FFFF)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Sphere_create
+  (JNIEnv *, jclass, jfloat cx, jfloat cy, jfloat cz, jfloat radius) {
+    const Vec3 center(cx, cy, cz);
+    Sphere * const pResult = new Sphere(center, radius);
+    TRACE_NEW("Sphere", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Sphere
+ * Method:    encapsulatePoint
+ * Signature: (JFFF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Sphere_encapsulatePoint
+  (JNIEnv *, jclass, jlong sphereVa, jfloat x, jfloat y, jfloat z) {
+    Sphere * const pSphere = reinterpret_cast<Sphere *> (sphereVa);
+    const Vec3 point(x, y, z);
+    pSphere->EncapsulatePoint(point);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Sphere
+ * Method:    free
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Sphere_free
+  (JNIEnv *, jclass, jlong sphereVa) {
+    Sphere * const pSphere = reinterpret_cast<Sphere *> (sphereVa);
+    TRACE_DELETE("Sphere", pSphere)
+    delete pSphere;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Sphere
+ * Method:    getCenterX
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Sphere_getCenterX
+  (JNIEnv *, jclass, jlong sphereVa) {
+    const Sphere * const pSphere = reinterpret_cast<Sphere *> (sphereVa);
+    const float result = pSphere->GetCenter().GetX();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Sphere
+ * Method:    getCenterY
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Sphere_getCenterY
+  (JNIEnv *, jclass, jlong sphereVa) {
+    const Sphere * const pSphere = reinterpret_cast<Sphere *> (sphereVa);
+    const float result = pSphere->GetCenter().GetY();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Sphere
+ * Method:    getCenterZ
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Sphere_getCenterZ
+  (JNIEnv *, jclass, jlong sphereVa) {
+    const Sphere * const pSphere = reinterpret_cast<Sphere *> (sphereVa);
+    const float result = pSphere->GetCenter().GetZ();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Sphere
+ * Method:    getRadius
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_Sphere_getRadius
+  (JNIEnv *, jclass, jlong sphereVa) {
+    const Sphere * const pSphere = reinterpret_cast<Sphere *> (sphereVa);
+    const float result = pSphere->GetRadius();
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Sphere
+ * Method:    getSupport
+ * Signature: (JFFF[F)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Sphere_getSupport
+  (JNIEnv *pEnv, jclass, jlong sphereVa, jfloat dx, jfloat dy, jfloat dz,
+  jfloatArray storeFloats) {
+    const Sphere * const pSphere = reinterpret_cast<Sphere *> (sphereVa);
+    const Vec3 direction(dx, dy, dz);
+    const Vec3 result = pSphere->GetSupport(direction);
+    jboolean isCopy;
+    jfloat * const pStoreFloats
+            = pEnv->GetFloatArrayElements(storeFloats, &isCopy);
+    pStoreFloats[0] = result.GetX();
+    pStoreFloats[1] = result.GetY();
+    pStoreFloats[2] = result.GetZ();
+    pEnv->ReleaseFloatArrayElements(storeFloats, pStoreFloats, 0);
+}
