19827351e05f944b3d0617eae7a9beb7f67784cd
stephengold <sgold@sonic.net>
2024-12-17 16:41:26 -0800

    SmokeTestAll:  re-enable the LoadSaveBinaryRigTest

--- src/test/java/testjoltjni/app/samples/SmokeTestAll.java
+++ src/test/java/testjoltjni/app/samples/SmokeTestAll.java
@@ -106,7 +106,7 @@ final public class SmokeTestAll {
         smokeTest(new CreateRigTest());
         //smokeTest(new KinematicRigTest());
         smokeTest(new LoadRigTest());
-        //smokeTest(new LoadSaveBinaryRigTest());
+        smokeTest(new LoadSaveBinaryRigTest());
         //smokeTest(new LoadSaveRigTest());
         //smokeTest(new PoweredRigTest());
         //smokeTest(new RigPileTest());
