14b98239b4a0af7f15cb3214add21fcceb62af4a
stephengold <sgold@sonic.net>
2025-01-17 22:00:41 -0800

    list-cpps.sh:  skip most of the test framework

--- bash/list-cpps.sh
+++ bash/list-cpps.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-/usr/bin/find ./src/main/native -name '*.cpp' -printf '$(N)/%P \\\n' \
+/usr/bin/find ./src/main/native/glue -name '*.cpp' -printf '$(N)/glue/%P \\\n' \
+| /usr/bin/sort
+
+/usr/bin/find ./src/main/native/Jolt -name '*.cpp' -printf '$(N)/Jolt/%P \\\n' \
 | /usr/bin/sort
 
 /usr/bin/find ./src/main/native -name '*.inl' -printf '$(N)/%P \\\n' \
 | /usr/bin/sort
+
+echo '$(N)/TestFramework/External/Perlin.cpp'
