b322f5ae889609b13a6bfa01edec5b8457266912
stephengold <sgold@sonic.net>
2025-01-26 14:13:48 -0800

    Contact.cpp:  getter should de-reference a "const" object

--- src/main/native/glue/co/Contact.cpp
+++ src/main/native/glue/co/Contact.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Stephen Gold
+Copyright (c) 2024-2025 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -80,7 +80,7 @@ JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Contact_getCharacter
   (JNIEnv *, jclass, jlong contactVa) {
     const CharacterVirtual::Contact * const pContact
             = reinterpret_cast<CharacterVirtual::Contact *> (contactVa);
-    CharacterVirtual * const pResult = pContact->mCharacterB;
+    const CharacterVirtual * const pResult = pContact->mCharacterB;
     return reinterpret_cast<jlong> (pResult);
 }
 
