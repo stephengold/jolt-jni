8b88ac9a23d9b81eb8621f8f9a6c7f7ab5f225c6
stephengold <sgold@sonic.net>
2025-02-07 22:10:43 -0800

    add the SoftBodyShape class

--- src/main/java/com/github/stephengold/joltjni/Shape.java
+++ src/main/java/com/github/stephengold/joltjni/Shape.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Stephen Gold
+Copyright (c) 2024-2025 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -112,6 +112,9 @@ abstract public class Shape extends NonCopyable
                 break;
             case Scaled:
                 result = new ScaledShape(shapeVa);
+                break;
+            case SoftBody:
+                result = new SoftBodyShape(shapeVa);
                 break;
             case Sphere:
                 result = new SphereShape(shapeVa);
