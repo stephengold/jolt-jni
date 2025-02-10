e026f5edffe8ed6d9170a821afbde5488feb59e3
stephengold <sgold@sonic.net>
2025-02-09 16:54:48 -0800

    Jolt:  update the version string

--- src/main/native/glue/j/Jolt.cpp
+++ src/main/native/glue/j/Jolt.cpp
@@ -373,7 +373,7 @@ JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Jolt_unregisterTypes
  */
 JNIEXPORT jstring JNICALL Java_com_github_stephengold_joltjni_Jolt_versionString
   (JNIEnv *pEnv, jclass) {
-    const jstring result = pEnv->NewStringUTF("0.9.5");
+    const jstring result = pEnv->NewStringUTF("0.9.6-SNAPSHOT");
     return result;
 }
 
