949acd267cc53c2f3e72c0830901fb41ec0ddf02
stephengold <sgold@sonic.net>
2025-02-19 17:53:38 -0800

    add the Skinned class

---
+++ src/main/java/com/github/stephengold/joltjni/Skinned.java
@@ -0,0 +1,127 @@
+/*
+Copyright (c) 2025 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+/**
+ * A constraint that skins a soft-body vertex. (native type:
+ * {@code SoftBodySharedSettings::Skinned})
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class Skinned extends JoltPhysicsObject {
+    // *************************************************************************
+    // constructors
+
+    /**
+     * Instantiate a default constraint.
+     */
+    public Skinned() {
+        long skinnedVa = createDefault();
+        setVirtualAddress(skinnedVa, () -> free(skinnedVa));
+    }
+
+    /**
+     * Instantiate a backstopped constraint for the specified vertex.
+     *
+     * @param vertexIndex the index of the vertex to constrain (&ge;0)
+     * @param maxDistance the maximum distance
+     * @param backstopDistance the backstop distance
+     * @param backstopRadius the backstop radius
+     */
+    public Skinned(int vertexIndex, float maxDistance, float backstopDistance,
+            float backstopRadius) {
+        long skinnedVa = createBackstopped(
+                vertexIndex, maxDistance, backstopDistance, backstopRadius);
+        setVirtualAddress(skinnedVa, () -> free(skinnedVa));
+    }
+
+    /**
+     * Instantiate a constraint with the specified container and native object.
+     *
+     * @param container the containing object, or {@code null} if none
+     * @param skinnedVa the virtual address of the native object to assign (not
+     * zero)
+     */
+    Skinned(JoltPhysicsObject container, long skinnedVa) {
+        super(container, skinnedVa);
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Access the specified weight. (native attribute: mWeight)
+     *
+     * @param index an index into the weight array (&ge;0, &lt;4)
+     * @return a new JVM object with the pre-existing native object assigned
+     */
+    public SkinWeight getWeight(int index) {
+        if (index < 0 || index >= 4) {
+            throw new IllegalArgumentException("index must be 0, 1, 2, or 3");
+        }
+
+        long skinnedVa = va();
+        long weightVa = getWeight(skinnedVa, index);
+        SkinWeight result = new SkinWeight(this, weightVa);
+
+        return result;
+    }
+
+    /**
+     * Normalize the weights so that they sum to one.
+     */
+    public void normalizeWeights() {
+        long skinnedVa = va();
+        normalizeWeights(skinnedVa);
+    }
+
+    /**
+     * Copy the specified weight into the array. (native attribute: mWeight)
+     *
+     * @param index an index into the weight array (&ge;0, &lt;4)
+     * @param weight the desired weight (alias created)
+     */
+    public void setWeight(int index, SkinWeight weight) {
+        if (index < 0 || index >= 4) {
+            throw new IllegalArgumentException("index must be 0, 1, 2, or 3");
+        }
+
+        long skinnedVa = va();
+        long weightVa = weight.va();
+        setWeight(skinnedVa, index, weightVa);
+    }
+    // *************************************************************************
+    // native private methods
+
+    native private static long createBackstopped(int vertexIndex,
+            float maxDistance, float backstopDistance, float backstopRadius);
+
+    native private static long createDefault();
+
+    native private static void free(long skinnedVa);
+
+    native private static long getWeight(long skinnedVa, int index);
+
+    native private static void normalizeWeights(long skinnedVa);
+
+    native private static void setWeight(
+            long skinnedVa, int index, long weightVa);
+}
---
+++ src/main/native/glue/s/Skinned.cpp
@@ -0,0 +1,111 @@
+/*
+Copyright (c) 2025 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+/*
+ * Author: Stephen Gold
+ */
+#include "Jolt/Jolt.h"
+#include "Jolt/Physics/SoftBody/SoftBodySharedSettings.h"
+#include "auto/com_github_stephengold_joltjni_Skinned.h"
+#include "glue/glue.h"
+
+using namespace JPH;
+
+/*
+ * Class:     com_github_stephengold_joltjni_Skinned
+ * Method:    createBackstopped
+ * Signature: (IFFF)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Skinned_createBackstopped
+  (JNIEnv *, jclass, jint vertexIndex, jfloat maxDistance,
+  jfloat backstopDistance, jfloat backstopRadius) {
+    SoftBodySharedSettings::Skinned * const pResult
+            = new SoftBodySharedSettings::Skinned(
+                    vertexIndex, maxDistance, backstopDistance, backstopRadius);
+    TRACE_NEW("SoftBodySharedSettings::Skinned", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Skinned
+ * Method:    createDefault
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Skinned_createDefault
+  (JNIEnv *, jclass) {
+    SoftBodySharedSettings::Skinned * const pResult
+            = new SoftBodySharedSettings::Skinned();
+    TRACE_NEW("SoftBodySharedSettings::Skinned", pResult)
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Skinned
+ * Method:    free
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Skinned_free
+  (JNIEnv *, jclass, jlong skinnedVa) {
+    SoftBodySharedSettings::Skinned * const pSkinned
+            = reinterpret_cast<SoftBodySharedSettings::Skinned *> (skinnedVa);
+    TRACE_DELETE("SoftBodySharedSettings::Skinned", pSkinned)
+    delete pSkinned;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Skinned
+ * Method:    getWeight
+ * Signature: (JI)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Skinned_getWeight
+  (JNIEnv *, jclass, jlong skinnedVa, jint index) {
+    SoftBodySharedSettings::Skinned * const pSkinned
+            = reinterpret_cast<SoftBodySharedSettings::Skinned *> (skinnedVa);
+    SoftBodySharedSettings::SkinWeight * pResult = &pSkinned->mWeights[index];
+    return reinterpret_cast<jlong> (pResult);
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Skinned
+ * Method:    normalizeWeights
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Skinned_normalizeWeights
+  (JNIEnv *, jclass, jlong skinnedVa) {
+    SoftBodySharedSettings::Skinned * const pSkinned
+            = reinterpret_cast<SoftBodySharedSettings::Skinned *> (skinnedVa);
+    pSkinned->NormalizeWeights();
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Skinned
+ * Method:    setWeight
+ * Signature: (JIJ)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_Skinned_setWeight
+  (JNIEnv *, jclass, jlong skinnedVa, jint index, jlong weightVa) {
+    SoftBodySharedSettings::Skinned * const pSkinned
+            = reinterpret_cast<SoftBodySharedSettings::Skinned *> (skinnedVa);
+    const SoftBodySharedSettings::SkinWeight * const pSkinWeight
+            = reinterpret_cast<SoftBodySharedSettings::SkinWeight *> (weightVa);
+    pSkinned->mWeights[index] = *pSkinWeight;
+}
