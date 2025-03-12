b200d0554c6699c54901cc9609e50187d5c7c6cd
stephengold <sgold@sonic.net>
2025-03-12 15:03:52 -0700

    add the ScaleHelpers class

---
+++ src/main/java/com/github/stephengold/joltjni/ScaleHelpers.java
@@ -0,0 +1,65 @@
+/*
+Copyright (c) 2025 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+import com.github.stephengold.joltjni.readonly.Vec3Arg;
+
+/**
+ * Utilities to test a scaling vector for various properties. (native namespace:
+ * ScaleHelpers)
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+final public class ScaleHelpers {
+    // *************************************************************************
+    // constants
+
+    /**
+     * The smalled valid scale factor.
+     */
+    final public static float cMinScale = 1e-6f;
+    /**
+     * The tolerance used when comparing components of a scale vector.
+     */
+    final public static float cScaleToleranceSq = 1e-8f;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * A private constructor to inhibit instantiation of this class.
+     */
+    private ScaleHelpers() {
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Test whether the specified scale is identity.
+     *
+     * @param scale the scale vector to test (not null, unaffected)
+     * @return {@code true} if identity, otherwise {@code false}
+     */
+    public static boolean isNotScaled(Vec3Arg scale) {
+        boolean result = scale.isClose(Vec3.sOne(), cScaleToleranceSq);
+        return result;
+    }
+}
