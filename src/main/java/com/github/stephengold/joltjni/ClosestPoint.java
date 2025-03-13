8e63f4d6dfce5a23db1f0c2f96a39fb31c5f3551
stephengold <sgold@sonic.net>
2025-03-13 11:44:47 -0700

    ClosestPoint:  re-order methods

--- src/main/java/com/github/stephengold/joltjni/ClosestPoint.java
+++ src/main/java/com/github/stephengold/joltjni/ClosestPoint.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Stephen Gold
+Copyright (c) 2024-2025 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -40,6 +40,65 @@ final public class ClosestPoint {
     }
     // *************************************************************************
     // new methods exposed
+
+    /**
+     * Calculate the barycentric coordinates of the point on the specified
+     * (infinite) line that's closest to the origin.
+     *
+     * @param a the location of a reference point on the line (not null,
+     * unaffected)
+     * @param b the location of another reference point on the line (not null,
+     * unaffected)
+     * @param storeUv storage for the coordinates (not null, length&ge;2,
+     * modified)
+     * @return {@code true} if successful, {@code false} if the points do not
+     * define a line
+     */
+    public static boolean getBaryCentricCoordinates(
+            Vec3Arg a, Vec3Arg b, float[] storeUv) {
+        float ax = a.getX();
+        float ay = a.getY();
+        float az = a.getZ();
+        float bx = b.getX();
+        float by = b.getY();
+        float bz = b.getZ();
+        boolean result
+                = getBaryCentricCoordinates2(ax, ay, az, bx, by, bz, storeUv);
+
+        return result;
+    }
+
+    /**
+     * Calculate the barycentric coordinates of the point on the specified plane
+     * that's closest to the origin.
+     *
+     * @param a the location of a reference point on the plane (not null,
+     * unaffected)
+     * @param b the location of another reference point on the plane (not null,
+     * unaffected)
+     * @param c the location of a 3rd reference point on the plane (not null,
+     * unaffected)
+     * @param storeUvw storage for the coordinates (not null, length&ge;3,
+     * modified)
+     * @return {@code true} if successful, {@code false} if the points do not
+     * define a plane
+     */
+    public static boolean getBaryCentricCoordinates(
+            Vec3Arg a, Vec3Arg b, Vec3Arg c, float[] storeUvw) {
+        float ax = a.getX();
+        float ay = a.getY();
+        float az = a.getZ();
+        float bx = b.getX();
+        float by = b.getY();
+        float bz = b.getZ();
+        float cx = c.getX();
+        float cy = c.getY();
+        float cz = c.getZ();
+        boolean result = getBaryCentricCoordinates3(
+                ax, ay, az, bx, by, bz, cx, cy, cz, storeUvw);
+
+        return result;
+    }
 
     /**
      * Locate the point on the specified line segment that's closest to the
@@ -137,67 +196,15 @@ final public class ClosestPoint {
 
         return result;
     }
-
-    /**
-     * Calculate the barycentric coordinates of the point on the specified
-     * (infinite) line that's closest to the origin.
-     *
-     * @param a the location of a reference point on the line (not null,
-     * unaffected)
-     * @param b the location of another reference point on the line (not null,
-     * unaffected)
-     * @param storeUv storage for the coordinates (not null, length&ge;2,
-     * modified)
-     * @return {@code true} if successful, {@code false} if the points do not
-     * define a line
-     */
-    public static boolean getBaryCentricCoordinates(
-            Vec3Arg a, Vec3Arg b, float[] storeUv) {
-        float ax = a.getX();
-        float ay = a.getY();
-        float az = a.getZ();
-        float bx = b.getX();
-        float by = b.getY();
-        float bz = b.getZ();
-        boolean result
-                = getBaryCentricCoordinates2(ax, ay, az, bx, by, bz, storeUv);
-
-        return result;
-    }
-
-    /**
-     * Calculate the barycentric coordinates of the point on the specified plane
-     * that's closest to the origin.
-     *
-     * @param a the location of a reference point on the plane (not null,
-     * unaffected)
-     * @param b the location of another reference point on the plane (not null,
-     * unaffected)
-     * @param c the location of a 3rd reference point on the plane (not null,
-     * unaffected)
-     * @param storeUvw storage for the coordinates (not null, length&ge;3,
-     * modified)
-     * @return {@code true} if successful, {@code false} if the points do not
-     * define a plane
-     */
-    public static boolean getBaryCentricCoordinates(
-            Vec3Arg a, Vec3Arg b, Vec3Arg c, float[] storeUvw) {
-        float ax = a.getX();
-        float ay = a.getY();
-        float az = a.getZ();
-        float bx = b.getX();
-        float by = b.getY();
-        float bz = b.getZ();
-        float cx = c.getX();
-        float cy = c.getY();
-        float cz = c.getZ();
-        boolean result = getBaryCentricCoordinates3(
-                ax, ay, az, bx, by, bz, cx, cy, cz, storeUvw);
-
-        return result;
-    }
     // *************************************************************************
     // native private methods
+
+    native private static boolean getBaryCentricCoordinates2(float ax, float ay,
+            float az, float bx, float by, float bz, float[] storeUvw);
+
+    native private static boolean getBaryCentricCoordinates3(
+            float ax, float ay, float az, float bx, float by, float bz,
+            float cx, float cy, float cz, float[] storeUvw);
 
     native private static void getClosestPointOnLine(
             float ax, float ay, float az, float bx, float by, float bz,
@@ -211,11 +218,4 @@ final public class ClosestPoint {
     native private static void getClosestPointOnTriangle(
             float ax, float ay, float az, float bx, float by, float bz,
             float cx, float cy, float cz, int[] storeSet, float[] storePoint);
-
-    native private static boolean getBaryCentricCoordinates2(float ax, float ay,
-            float az, float bx, float by, float bz, float[] storeUvw);
-
-    native private static boolean getBaryCentricCoordinates3(
-            float ax, float ay, float az, float bx, float by, float bz,
-            float cx, float cy, float cz, float[] storeUvw);
 }
