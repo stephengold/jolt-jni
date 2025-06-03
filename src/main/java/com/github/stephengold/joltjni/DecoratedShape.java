967786e7c2f4ee70566c068df51579816d2c1e34
stephengold <sgold@sonic.net>
2025-06-02 23:34:34 -0700

    make the DecoratedShape and DecoratedShapeSettings classes abstract

--- src/main/java/com/github/stephengold/joltjni/DecoratedShape.java
+++ src/main/java/com/github/stephengold/joltjni/DecoratedShape.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Stephen Gold
+Copyright (c) 2024-2025 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,7 @@ import com.github.stephengold.joltjni.readonly.ConstShape;
  *
  * @author Stephen Gold sgold@sonic.net
  */
-public class DecoratedShape extends Shape {
+abstract public class DecoratedShape extends Shape {
     // *************************************************************************
     // constructors
 
--- src/main/java/com/github/stephengold/joltjni/DecoratedShapeSettings.java
+++ src/main/java/com/github/stephengold/joltjni/DecoratedShapeSettings.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Stephen Gold
+Copyright (c) 2024-2025 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@ package com.github.stephengold.joltjni;
  *
  * @author Stephen Gold sgold@sonic.net
  */
-public class DecoratedShapeSettings extends ShapeSettings {
+abstract public class DecoratedShapeSettings extends ShapeSettings {
     // *************************************************************************
     // constructors
 
