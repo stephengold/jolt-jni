21b0d51d905b0eb6bf7c1b7b749d2a209825d68b
stephengold <sgold@sonic.net>
2025-06-15 22:42:41 -0700

    EPATest:  correct the URL in the javadoc

--- src/test/java/testjoltjni/app/samples/convexcollision/EPATest.java
+++ src/test/java/testjoltjni/app/samples/convexcollision/EPATest.java
@@ -33,7 +33,7 @@ import static testjoltjni.app.samples.DebugRendererSP.*;
  * A line-for-line Java translation of the Jolt Physics EPA test.
  * <p>
  * Compare with the original by Jorrit Rouwe at
- * https://github.com/jrouwe/JoltPhysics/blob/master/Samples/Tests/Constraints/EPATest.cpp
+ * https://github.com/jrouwe/JoltPhysics/blob/master/Samples/Tests/ConvexCollision/EPATest.cpp
  */
 public class EPATest extends Test{
 
