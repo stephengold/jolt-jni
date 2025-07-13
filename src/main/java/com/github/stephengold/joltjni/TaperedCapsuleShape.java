dd31c738a9e7d3f79426a8f89bf6f44445f9d24c
stephengold <sgold@sonic.net>
2025-07-12 17:04:17 -0700

    javadoc

--- src/main/java/com/github/stephengold/joltjni/TaperedCapsuleShape.java
+++ src/main/java/com/github/stephengold/joltjni/TaperedCapsuleShape.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Stephen Gold
+Copyright (c) 2024-2025 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,7 @@ package com.github.stephengold.joltjni;
 
 /**
  * A {@code Shape} to represent the convex hull of 2 spheres centered on the
- * axis.
+ * Y axis.
  *
  * @author Stephen Gold sgold@sonic.net
  */
--- src/main/java/com/github/stephengold/joltjni/TaperedCylinderShape.java
+++ src/main/java/com/github/stephengold/joltjni/TaperedCylinderShape.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024 Stephen Gold
+Copyright (c) 2024-2025 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -22,7 +22,7 @@ SOFTWARE.
 package com.github.stephengold.joltjni;
 
 /**
- * A {@code Shape} to represent a conical frustum.
+ * A {@code Shape} to represent a Y-axis conical frustum.
  *
  * @author Stephen Gold sgold@sonic.net
  */
