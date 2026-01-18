72b4c09a2e4b18563e54c58159640b47a87bf3ad
stephengold <sgold@sonic.net>
2026-01-17 22:49:37 -0800

    bugfix:  Geometry.create(long) not implemented

--- src/main/native/glue/g/Geometry.cpp
+++ src/main/native/glue/g/Geometry.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2025 Stephen Gold
+Copyright (c) 2025-2026 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -114,6 +114,24 @@ JNIEXPORT jint JNICALL Java_com_github_stephengold_joltjni_Geometry_countLods
             = reinterpret_cast<DebugRenderer::Geometry *> (geometryVa);
     const size_t result = pGeometry->mLODs.size();
     return result;
+#else
+    return 0;
+#endif
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_Geometry
+ * Method:    create
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_Geometry_create
+  (JNIEnv *, jclass, jlong boundsVa) {
+#ifdef JPH_DEBUG_RENDERER
+    const AABox * const pBounds = reinterpret_cast<AABox *> (boundsVa);
+    DebugRenderer::Geometry * const pResult
+            = new DebugRenderer::Geometry(*pBounds);
+    TRACE_NEW("DebugRenderer::Geometry", pResult)
+    return reinterpret_cast<jlong> (pResult);
 #else
     return 0;
 #endif
