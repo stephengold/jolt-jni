ba85362496ab5897d7064a3f282169a5f4ded7c9
stephengold <sgold@sonic.net>
2026-01-25 11:29:10 -0800

    ListClasses:  delete an exception that's never thrown

--- src/test/java/testjoltjni/ListClasses.java
+++ src/test/java/testjoltjni/ListClasses.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2025 Stephen Gold
+Copyright (c) 2025-2026 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -104,7 +104,7 @@ final public class ListClasses {
      *
      * @param stream the output stream to use (not null)
      */
-    private static void printNamesTo(PrintStream stream) throws IOException {
+    private static void printNamesTo(PrintStream stream) {
         Set<String> names = new TreeSet<>();
 
         // Enumerate Jolt JNI's core and enum public classes:
