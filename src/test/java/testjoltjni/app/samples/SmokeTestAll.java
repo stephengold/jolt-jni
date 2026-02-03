0ee9bb3cdfb5e3ad1f986ab403f10a9a7b5f9371
stephengold <sgold@sonic.net>
2026-02-02 23:01:10 -0800

    SmokeTestAll: CI hosts may only support CPU compute

--- src/test/java/testjoltjni/app/samples/SmokeTestAll.java
+++ src/test/java/testjoltjni/app/samples/SmokeTestAll.java
@@ -115,11 +115,9 @@ final public class SmokeTestAll {
         tempAllocator = new TempAllocatorImpl(numBytes);
 
         // All tests share a single ComputeSystem:
-        ComputeSystemResult csResult = ComputeSystem.createComputeSystem();
-        if (csResult.hasError()) {
-            csResult = ComputeSystem.createComputeSystemCpu();
-            assert !csResult.hasError();
-        }
+        ComputeSystemResult csResult = ComputeSystem.createComputeSystemCpu();
+        //ComputeSystemResult csResult = ComputeSystem.createComputeSystem();
+        assert !csResult.hasError();
         computeSystem = csResult.get().getPtr();
         Rtti rtti = computeSystem.getRtti();
         String systemName = rtti.getName();
