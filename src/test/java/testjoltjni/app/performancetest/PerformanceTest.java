d78f14c49a3182058b1ef77391208b8f54bca269
stephengold <sgold@sonic.net>
2026-03-16 12:49:54 -0700

    PerformanceTest:  bring the usage message up to date

--- src/test/java/testjoltjni/app/performancetest/PerformanceTest.java
+++ src/test/java/testjoltjni/app/performancetest/PerformanceTest.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024-2025 Stephen Gold
+Copyright (c) 2024-2026 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -168,7 +168,7 @@ public static void main(  String[] argv)throws IOException
 		{
 			// Print usage
 			Trace("Usage:\n"
-				 + "-s=<scene>: Select scene (Ragdoll, RagdollSinglePile, ConvexVsMesh, Pyramid)\n"
+				 + "-s=<scene>: Select scene (Ragdoll, RagdollSinglePile, ConvexVsMesh, Pyramid, LargeMesh, CharacterVirtual)\n"
 				 + "-i=<num physics steps>: Number of physics steps to simulate (default 500)\n"
 				 + "-q=<quality>: Test only with specified quality (Discrete, LinearCast)\n"
 				 + "-t=<num threads>: Test only with N threads (default is to iterate over 1 .. num hardware threads)\n"
