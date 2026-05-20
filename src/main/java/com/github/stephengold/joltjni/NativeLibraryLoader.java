e4602f20402c09fd2a72b7e1efa2d8b2139ca691
David Jake Morfe <jakemorfe@gmail.com>
2026-05-20 18:09:04 -0400

    Create NativeLibraryLoader.java (#47)
    * Create NativeLibraryLoader.java
    
    Can be used to resolve java.lang.UnsatisfiedLinkError like when used in Minecraft mod development
    
    * Update NativeLibraryLoader.java
    
    * Update NativeLibraryLoader.java

---
+++ src/main/java/com/github/stephengold/joltjni/NativeLibraryLoader.java
@@ -0,0 +1,64 @@
+/*
+Copyright (c) 2026 Stephen Gold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.stephengold.joltjni;
+
+import java.util.logging.Logger;
+
+/**
+ * Utility class to load native libraries.
+ *
+ * @author David J. Morfe
+ */
+public final class NativeLibraryLoader {
+
+    /**
+     * message logger for this class
+     */
+    final public static Logger logger
+            = Logger.getLogger(NativeLibraryLoader.class.getName());
+
+    /**
+     * A private constructor to inhibit instantiation of this class.
+     */
+    private NativeLibraryLoader() {
+    }
+
+    /**
+     * Load a Jolt native library.
+     *
+     * @param libraryPath
+     * @return true after successful load, otherwise false
+     */
+    public static boolean loadLibrary(String libraryPath) {
+        boolean success = false;
+
+        try {
+            System.load(libraryPath); // Ensures native library can successfully bind to the native context.
+            logger.info("Jolt DLL loaded");
+            success = true;
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+        return success;
+    }
+}
