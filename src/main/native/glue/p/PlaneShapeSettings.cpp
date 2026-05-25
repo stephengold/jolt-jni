712c54d08fdbe1694ab762041bac129996b2b45e
stephengold <sgold@sonic.net>
2026-05-25 13:32:35 -0700

    PlaneShapeSettings.cpp:  comments

--- src/main/native/glue/p/PlaneShapeSettings.cpp
+++ src/main/native/glue/p/PlaneShapeSettings.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2024-2025 Stephen Gold
+Copyright (c) 2024-2026 Stephen Gold
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -31,7 +31,7 @@ SOFTWARE.
 using namespace JPH;
 
 /*
- * Class:     com_github_stephengold_joltjni_BoxShapeSettings
+ * Class:     com_github_stephengold_joltjni_PlaneShapeSettings
  * Method:    createCopy
  * Signature: (J)J
  */
@@ -39,7 +39,7 @@ JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_PlaneShapeSettings_c
   BODYOF_CREATE_COPY_TARGET(PlaneShapeSettings)
 
 /*
- * Class:     com_github_stephengold_joltjni_BoxShapeSettings
+ * Class:     com_github_stephengold_joltjni_PlaneShapeSettings
  * Method:    createDefault
  * Signature: ()J
  */
