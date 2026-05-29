33d1d4fb15286d28f0215a2141f2b05c58aec26b
stephengold <sgold@sonic.net>
2026-05-28 23:22:41 -0700

    MotorCycleControllerSettings:  add getters and setters for 6 attributes

--- src/main/java/com/github/stephengold/joltjni/MotorcycleControllerSettings.java
+++ src/main/java/com/github/stephengold/joltjni/MotorcycleControllerSettings.java
@@ -67,6 +67,86 @@ public class MotorcycleControllerSettings
     // new methods exposed
 
     /**
+     * Return the lean smoothing factor. The settings are unaffected. (native
+     * attribute: mLeanSmoothingFactor)
+     *
+     * @return the factor
+     */
+    public float getLeanSmoothingFactor() {
+        long settingsVa = va();
+        float result = getLeanSmoothingFactor(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the spring constant for the lean spring. The settings are
+     * unaffected. (native attribute: mLeanSpringConstant)
+     *
+     * @return the spring constant
+     */
+    public float getLeanSpringConstant() {
+        long settingsVa = va();
+        float result = getLeanSpringConstant(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the damping constant for the lean spring. The settings are
+     * unaffected. (native attribute: mLeanSpringDamping)
+     *
+     * @return the damping constant
+     */
+    public float getLeanSpringDamping() {
+        long settingsVa = va();
+        float result = getLeanSpringDamping(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the coefficient of additional force to control the lean angle. The
+     * settings are unaffected. (native attribute:
+     * mLeanSpringIntegrationCoefficient)
+     *
+     * @return the integration coefficient
+     */
+    public float getLeanSpringIntegrationCoefficient() {
+        long settingsVa = va();
+        float result = getLeanSpringIntegrationCoefficient(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the rate of decay of the angle integral when the wheels are
+     * unsupported. The settings are unaffected. (native attribute:
+     * mLeanSpringIntegrationCoefficientDecay)
+     *
+     * @return the rate of decay (per second)
+     */
+    public float getLeanSpringIntegrationCoefficientDecay() {
+        long settingsVa = va();
+        float result = getLeanSpringIntegrationCoefficientDecay(settingsVa);
+
+        return result;
+    }
+
+    /**
+     * Return the maximum lean angle (during turns). The settings are
+     * unaffected. (native attribute: mMaxLeanAngle)
+     *
+     * @return the angle (in radians)
+     */
+    public float getMaxLeanAngle() {
+        long settingsVa = va();
+        float result = getMaxLeanAngle(settingsVa);
+
+        return result;
+    }
+
+    /**
      * Copy the argument to the current settings.
      *
      * @param source the settings to copy (not {@code null}, unaffected)
@@ -75,6 +155,71 @@ public class MotorcycleControllerSettings
         long targetVa = va();
         long sourceVa = source.va();
         assign(targetVa, sourceVa);
+    }
+
+    /**
+     * Alter the lean smoothing factor. (native attribute: mLeanSmoothingFactor)
+     *
+     * @param factor the desired factor (default=0.8)
+     */
+    public void setLeanSmoothingFactor(float factor) {
+        long settingsVa = va();
+        setLeanSmoothingFactor(settingsVa, factor);
+    }
+
+    /**
+     * Alter the spring constant for the lean spring. (native attribute:
+     * mLeanSpringConstant)
+     *
+     * @param k the desired spring constant (default=5000)
+     */
+    public void setLeanSpringConstant(float k) {
+        long settingsVa = va();
+        setLeanSpringConstant(settingsVa, k);
+    }
+
+    /**
+     * Alter the damping constant for the lean spring. (native attribute:
+     * mLeanSpringDamping)
+     *
+     * @param damping the desired damping constant (default=1000)
+     */
+    public void setLeanSpringDamping(float damping) {
+        long settingsVa = va();
+        setLeanSpringDamping(settingsVa, damping);
+    }
+
+    /**
+     * Alter the coefficient of additional force to control the lean angle.
+     * (native attribute: mLeanSpringIntegrationCoefficient)
+     *
+     * @param coefficient the desired integration coefficient (default=0)
+     */
+    public void setLeanSpringIntegrationCoefficient(float coefficient) {
+        long settingsVa = va();
+        setLeanSpringIntegrationCoefficient(settingsVa, coefficient);
+    }
+
+    /**
+     * Alter the rate of decay of the angle integral when the wheels are
+     * unsupported. (native attribute: mLeanSpringIntegrationCoefficientDecay)
+     *
+     * @param decay the desired rate of decay (per second, default=4)
+     */
+    public void setLeanSpringIntegrationCoefficientDecay(float decay) {
+        long settingsVa = va();
+        setLeanSpringIntegrationCoefficientDecay(settingsVa, decay);
+    }
+
+    /**
+     * Alter the maximum lean angle (during turns). (native attribute:
+     * mMaxLeanAngle)
+     *
+     * @param angle the desired angle (in radians, default=Pi/4)
+     */
+    public void setMaxLeanAngle(float angle) {
+        long settingsVa = va();
+        setMaxLeanAngle(settingsVa, angle);
     }
     // *************************************************************************
     // WheeledVehicleControllerSettings methods
@@ -96,4 +241,34 @@ public class MotorcycleControllerSettings
     native private static long createCopy(long originalVa);
 
     native private static long createDefault();
+
+    native private static float getLeanSmoothingFactor(long settingsVa);
+
+    native private static float getLeanSpringConstant(long settingsVa);
+
+    native private static float getLeanSpringDamping(long settingsVa);
+
+    native private static float getLeanSpringIntegrationCoefficient(
+            long settingsVa);
+
+    native private static float getLeanSpringIntegrationCoefficientDecay(
+            long settingsVa);
+
+    native private static float getMaxLeanAngle(long settingsVa);
+
+    native private static void setLeanSmoothingFactor(
+            long settingsVa, float factor);
+
+    native private static void setLeanSpringConstant(long settingsVa, float k);
+
+    native private static void setLeanSpringDamping(
+            long settingsVa, float damping);
+
+    native private static void setLeanSpringIntegrationCoefficient(
+            long settingsVa, float coefficient);
+
+    native private static void setLeanSpringIntegrationCoefficientDecay(
+            long settingsVa, float decay);
+
+    native private static void setMaxLeanAngle(long settingsVa, float angle);
 }
--- src/main/native/glue/m/MotorcycleControllerSettings.cpp
+++ src/main/native/glue/m/MotorcycleControllerSettings.cpp
@@ -53,3 +53,153 @@ JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_MotorcycleController
  */
 JNIEXPORT jlong JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_createDefault
   BODYOF_CREATE_DEFAULT_TARGET(MotorcycleControllerSettings)
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    getLeanSmoothingFactor
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_getLeanSmoothingFactor
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    const float result = pSettings->mLeanSmoothingFactor;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    getLeanSpringConstant
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_getLeanSpringConstant
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    const float result = pSettings->mLeanSpringConstant;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    getLeanSpringDamping
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_getLeanSpringDamping
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    const float result = pSettings->mLeanSpringDamping;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    getLeanSpringIntegrationCoefficient
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_getLeanSpringIntegrationCoefficient
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    const float result = pSettings->mLeanSpringIntegrationCoefficient;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    getLeanSpringIntegrationCoefficientDecay
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_getLeanSpringIntegrationCoefficientDecay
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    const float result = pSettings->mLeanSpringIntegrationCoefficientDecay;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    getMaxLeanAngle
+ * Signature: (J)F
+ */
+JNIEXPORT jfloat JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_getMaxLeanAngle
+  (JNIEnv *, jclass, jlong settingsVa) {
+    const MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    const float result = pSettings->mMaxLeanAngle;
+    return result;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    setLeanSmoothingFactor
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_setLeanSmoothingFactor
+  (JNIEnv *, jclass, jlong settingsVa, jfloat factor) {
+    MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    pSettings->mLeanSmoothingFactor = factor;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    setLeanSpringConstant
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_setLeanSpringConstant
+  (JNIEnv *, jclass, jlong settingsVa, jfloat k) {
+    MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    pSettings->mLeanSpringConstant = k;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    setLeanSpringDamping
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_setLeanSpringDamping
+  (JNIEnv *, jclass, jlong settingsVa, jfloat damping) {
+    MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    pSettings->mLeanSpringDamping = damping;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    setLeanSpringIntegrationCoefficient
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_setLeanSpringIntegrationCoefficient
+  (JNIEnv *, jclass, jlong settingsVa, jfloat coefficient) {
+    MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    pSettings->mLeanSpringIntegrationCoefficient = coefficient;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    setLeanSpringIntegrationCoefficientDecay
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_setLeanSpringIntegrationCoefficientDecay
+  (JNIEnv *, jclass, jlong settingsVa, jfloat decay) {
+    MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    pSettings->mLeanSpringIntegrationCoefficientDecay = decay;
+}
+
+/*
+ * Class:     com_github_stephengold_joltjni_MotorcycleControllerSettings
+ * Method:    setMaxLeanAngle
+ * Signature: (JF)V
+ */
+JNIEXPORT void JNICALL Java_com_github_stephengold_joltjni_MotorcycleControllerSettings_setMaxLeanAngle
+  (JNIEnv *, jclass, jlong settingsVa, jfloat angle) {
+    MotorcycleControllerSettings * const pSettings
+            = reinterpret_cast<MotorcycleControllerSettings *> (settingsVa);
+    pSettings->mMaxLeanAngle = angle;
+}
